5ac9332fea8fa949833e0bc17af4c9a70b29c774
aaearon <aaearon@gmail.com>
2013-01-01 15:12:19 -0500

    separate out update_predb so it can be ran more frequently

--- bin/cleanup_scripts.sh
+++ bin/cleanup_scripts.sh
@@ -7,6 +7,7 @@ source ../edit_these.sh
 LASTOPTIMIZE1=`date +%s`
 LASTOPTIMIZE2=`date +%s`
 LASTOPTIMIZE3=`date +%s`
+LASTOPTIMIZE4=`date +%s`
 i=1
 while [ $i -gt 0 ]
 
@@ -17,13 +18,22 @@ cd $NEWZNAB_PATH
 [ -f $NEWZNAB_PATH/update_releases.php ] && $PHP $NEWZNAB_PATH/update_releases.php
 
 CURRTIME=`date +%s`
-#every 2 hours and during first loop
+#every 15 minutes and during first loop
 DIFF=$(($CURRTIME-$LASTOPTIMIZE1))
-if [ "$DIFF" -gt 7200 ] || [ $i -eq 1 ]
+if [ "$DIFF" -gt 900 ] || [ $i -eq 1 ]
 then
         LASTOPTIMIZE1=`date +%s`
         cd $NEWZNAB_PATH
         [ -f $NEWZNAB_PATH/update_predb.php ] && $PHP $NEWZNAB_PATH/update_predb.php true
+fi
+
+
+CURRTIME=`date +%s`
+#every 2 hours and during first loop
+DIFF=$(($CURRTIME-$LASTOPTIMIZE2))
+if [ "$DIFF" -gt 7200 ] || [ $i -eq 1 ]
+then
+        LASTOPTIMIZE2=`date +%s`
         cd $TESTING_PATH
         [ -f $TESTING_PATH/update_parsing.php ] && $PHP $TESTING_PATH/update_parsing.php
         [ -f $TESTING_PATH/removespecial.php ] && $PHP $TESTING_PATH/removespecial.php
@@ -32,20 +42,20 @@ fi
 
 CURRTIME=`date +%s`
 #every 12 hours
-DIFF=$(($CURRTIME-$LASTOPTIMIZE2))
+DIFF=$(($CURRTIME-$LASTOPTIMIZE3))
 if [ "$DIFF" -gt 43200 ]
 then
-        LASTOPTIMIZE2=`date +%s`
+        LASTOPTIMIZE3=`date +%s`
         cd $NEWZNAB_PATH
         [ -f $NEWZNAB_PATH/optimise_db.php ] && $PHP $NEWZNAB_PATH/optimise_db.php
 fi
 
 CURRTIME=`date +%s`
 #every 12 hours and during 1st loop
-DIFF=$(($CURRTIME-$LASTOPTIMIZE3))
+DIFF=$(($CURRTIME-$LASTOPTIMIZE4))
 if [ "$DIFF" -gt 43200 ] || [ $i -eq 1 ]
 then
-        LASTOPTIMIZE3=`date +%s`
+        LASTOPTIMIZE4=`date +%s`
         cd $NEWZNAB_PATH
         #[ -f $NEWZNAB_PATH/optimise_db.php ] && $PHP $NEWZNAB_PATH/optimise_db.php true
         [ -f $NEWZNAB_PATH/update_tvschedule.php ] && $PHP $NEWZNAB_PATH/update_tvschedule.php
