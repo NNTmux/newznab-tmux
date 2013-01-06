badcd9c475adabfbe4778f9e492793f5663d00c5
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-06 16:28:36 -0500

    edit edit_these

--- edit_these.sh
+++ edit_these.sh
@@ -7,11 +7,11 @@ export NEWZPATH="/var/www/newznab"
 export NEWZNAB_PATH=$NEWZPATH"/misc/update_scripts"
 export TESTING_PATH=$NEWZPATH"/misc/testing"
 export ADMIN_PATH=$NEWZPATH"/www/admin"
-export USERNAME="jonnyboy" # this is the user name that will run these scripts
+export USERNAME="what is your name" # this is the user name that will run these scripts
 export NEWZNAB_IMPORT_SLEEP_TIME="60" # in seconds - this includes import_nzb backfill and current fill
 export NEWZNAB_POST_SLEEP_TIME="1" # in seconds - this is for post processing - sleep between loops
 export MAXDAYS="200"  #max days for backfill
-export NZBS="/home/jonnyboy/nzbs/batch"  #path to your nzb files to be imported
+export NZBS="/path/to/nzbs"  #path to your nzb files to be imported
 
 #Choose to run the threaded or non-threaded newznab scripts true/false
 export THREADS="true"
@@ -38,7 +38,7 @@ export USE_MYTOP="true"
 #By using this script you understand that the programmer is not responsible for any loss of data, users, or sanity.
 #You also agree that you were smart enough to make a backup of your database and files. Do you agree? yes/no
 
-export AGREED="yes"
+export AGREED="no"
 
 ##END OF EDITS##
 
