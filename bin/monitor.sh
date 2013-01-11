337e0b3e38d882e8946156ec3aff968fc67e5160
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-10 19:34:37 -0500

    added files for start.sh

---
+++ bin/monitor.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+source ../edit_these.sh
+#eval $( sed -n "/^define/ { s/.*('\([^']*\)', '*\([^']*\)'*);/export \1=\"\2\"/; p }" $NEWZPATH/www/config.php )
+eval $( sed -n "/^define/ { s/.*('\([^']*\)', '*\([^']*\)'*);/export \1=\"\2\"/; p }" "$NEWZPATH"/www/config.php )
+
+while :
+do
+
+  $PHP monitor.php
+
+done
+
