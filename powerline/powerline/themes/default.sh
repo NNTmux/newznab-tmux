e9022bc2bbe756b7f7685ac4cf467053ea832f9a
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-29 22:36:03 +0100

    change powerline display

--- powerline/powerline/themes/default.sh
+++ powerline/powerline/themes/default.sh
@@ -30,7 +30,7 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
 		"vcs_branch 29 88" \
-		#"vcs_compare 60 255" \
+		"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
 		#"vcs_modified 9 255" \
 		#"vcs_others 245 0" \
@@ -44,9 +44,9 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 33 148" \
-                "uptime 9 255" \
-		#"tmux_mem_cpu_load 33 148 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"load 33 148" \
+                #"uptime 9 255" \
+		"tmux_mem_cpu_load 33 148 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"battery 137 127" \
 		#"weather 37 255" \
 		#"xkb_layout 125 117" \
