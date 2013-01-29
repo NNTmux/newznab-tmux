047288e015f2451ce62b920455c0429f9fc8672b
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-30 00:59:46 +0100

    added memory and cpu usage to status bar

--- powerline/powerline/segments/tmux_mem_cpu_load.sh
+++ powerline/powerline/segments/tmux_mem_cpu_load.sh
@@ -1,12 +1,12 @@
 # Print out Memory, cpu and load using https://github.com/thewtex/tmux-mem-cpu-load
-
+mem_app=bin/./tmux-mem-cpu-load
 run_segment() {
-	type tmux-mem-cpu-load >/dev/null 2>&1
+	type bin/tmux-mem-cpu-load >/dev/null 2>&1
 	if [ "$?" -ne 0 ]; then
 		return
 	fi
 
-	stats=$(tmux-mem-cpu-load)
+	stats=$($mem_app)
 	if [ -n "$stats" ]; then
 		echo "$stats";
 	fi
