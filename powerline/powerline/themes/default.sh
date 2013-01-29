ba8ad5ba97ff2b71360b4ab5d63ea28f9dacf636
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-29 06:27:29 +0100

    updates to powerline config

--- powerline/powerline/themes/default.sh
+++ powerline/powerline/themes/default.sh
@@ -29,11 +29,11 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
-		#"vcs_branch 29 88" \
-		#"vcs_compare 60 255" \
-		#"vcs_staged 64 255" \
-		#"vcs_modified 9 255" \
-		#"vcs_others 245 0" \
+		"vcs_branch 29 88" \
+		"vcs_compare 60 255" \
+		"vcs_staged 64 255" \
+		"vcs_modified 9 255" \
+		"vcs_others 245 0" \
 	)
 fi
 
