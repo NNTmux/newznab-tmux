ae8f114c2202a9b88a619a1ecde152b057d8445a
jonnyboy <jonnyboy@jbmatrix.net>
2013-01-29 00:40:01 -0500

    Update edit_these.sh
    attempt to organize settings into groups

--- edit_these.sh
+++ edit_these.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#EDIT THESE#
+#########################EDIT THESE#########################
+############################################################
 
 #This is the shutdown, true/false
 #on, it runs, off and no scripts will be RESTARTED, when all panes are DEAD, killall tmux
 #if this is set to false, the script will run 1 loop and terminate
 export RUNNING="true"
 
-#paths
+############################################################
+
+#Set paths
 export NEWZPATH="/var/www/newznab"
 
+#Should not need to change
 export NEWZNAB_PATH=$NEWZPATH"/misc/update_scripts"
 export TESTING_PATH=$NEWZPATH"/misc/testing"
 export ADMIN_PATH=$NEWZPATH"/www/admin"
+
+############################################################
 
 #Post Processing Additional is the processing that downloads rar files and attempts to get info for your site
 #you are able to set the number of process to be run from 1-9
@@ -28,6 +34,8 @@ export TMUX_SESSION="Newznab"
 #Set, in seconds - how often the monitor.php (left top pane) script should update, 0 may cause errors
 export MONITOR_UPDATE="30"
 
+############################################################
+
 #Choose to run update_binaries true/false
 export BINARIES="true"
 
@@ -38,6 +46,8 @@ export BINARIES_THREADS="true"
 #Set, in seconds - how long the update_binaries should sleep between runs, 0 may cause errors
 #top right pane
 export BINARIES_SLEEP="40"
+
+############################################################
 
 #Choose to run backfill script true/false
 export BACKFILL="true"
@@ -60,6 +70,8 @@ export BACKFILL_MAX_RELEASES="0"
 #backfill will continue to run, and do no work, at that point you should disable backfill, below
 export MAXDAYS="210"
 
+############################################################
+
 #Set the path to the nzb dump you downloaded from torrents, this is the path to bulk files folder of nzbs
 #this does not recurse through subfolders
 export NZBS="/path/to/nzbs"
@@ -72,7 +84,7 @@ export IMPORT="true"
 #and 10 nzbs will be imported from each subfolder per loop.
 export NZB_THREADS="true"
 
-#How many nzbs to import per loop, if using NZBMULTI=true the per folder
+#How many nzbs to import per loop, if using NZB_THREADS=true the per folder
 export NZBCOUNT="20"
 
 #Set, in seconds - how long the nzb-import should sleep between runs, 0 may cause errors
@@ -82,6 +94,8 @@ export IMPORT_SLEEP="40"
 #Set the max amount of unprocessed releases and still allow nzb-import to run
 #set to 0 to disable
 export IMPORT_MAX_RELEASES="0"
+
+############################################################
 
 #Create releases, this is really only necessary to tuen off when you only want to postprocess
 export RELEASES="true"
@@ -94,9 +108,13 @@ export RELEASES_SLEEP="40"
 #set to 0 to disable
 export MAX_RELEASES="0"
 
+############################################################
+
 #Choose your database engine, comment the one true/false
 #you should have already converted your database to InnoDB engine, if you select true here
 export INNODB="true"
+
+############################################################
 
 #Choose to run update_cleanup.php and removespecial.php true/false
 #set to false by default, you will need to edit /misc/testing/update_cleanup.php and /misc/testing/removespecial.php
@@ -106,6 +124,8 @@ export CLEANUP="false"
 #How oftern do you want  update_cleanup.php and removespecial.php to run, in seconds
 export CLEANUP_TIMER="3600"
 
+############################################################
+
 #Choose to run update_parsing.php true/false
 #set to false by default, you will need to edit /misc/testing/update_parsing.php
 #to actually do anything, directions are in the file
@@ -114,15 +134,27 @@ export PARSING="false"
 #How oftern do you want update_parsing.php to run, in seconds
 export PARSING_TIMER="3600"
 
+############################################################
+
 #Choose to run optimise_innodb.php or optimise_mysiam.php script true/false
 #set to false by default, you should test the optimse scripts in bin first
 export OPTIMISE="false"
+
+#How often to run optimise_myisam on large tables seconds
+export MYISAM_LARGE="3600"
+
+#How often to run optimise_innodb on large tables in seconds
+export INNODB_LARGE="43200"
+
+############################################################
 
 #How often to update the PreDB in seconds
 export PREDB_TIMER="900"
 
 #How often to update the TV Schedule and the In Theaters in seconds
 export TVRAGE_TIMER="43200"
+
+############################################################
 
 #Choose to run sphinx.php script true/false
 #set to false by default, you should test the script first, php sphinx.php from the bin folder
@@ -131,9 +163,13 @@ export SPHINX="true"
 #How often to run sphinx in seconds
 export SPHINX_TIMER="3600"
 
+############################################################
+
 #Specify your SED binary
 export SED="/bin/sed"
 #export SED="/usr/local/bin/gsed"
+
+############################################################
 
 #Select some monitoring script, if they are not installed, it will not affect the running of the scripts
 #these are set to false by default, enable if you want them
@@ -145,13 +181,18 @@ export USE_MYTOP="false"
 export USE_VNSTAT="false"
 export USE_IFTOP="false"
 
-#Enable logging, this is pretty basic logging, a timpstamp and numbers from db
+############################################################
+
 #Use powerline scripts to display the status bar
 export POWERLINE="true"
+
+############################################################
 
 #By using this script you understand that the programmer is not responsible for any loss of data, users, or sanity.
 #You also agree that you were smart enough to make a backup of your database and files. Do you agree? yes/no
 export AGREED="no"
+
+############################################################
 
 ##END OF EDITS##
 
