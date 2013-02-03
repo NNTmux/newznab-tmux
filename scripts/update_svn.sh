0f1b8df29958d074b1c192112f5f50add130f3b7
jonnyboy <jonnyboy@jbmatrix.net>
2013-02-03 18:19:32 +0100

    fixed svn update command

--- scripts/update_svn.sh
+++ scripts/update_svn.sh
@@ -4,7 +4,7 @@
 export NEWZPATH="/var/www/newznab"
 export PASSWORD="password"
 
-svn export --force --username svnplus --password $PASSWORD svn://svn.newznab.com/nn/branches/nnplus $NEWZPATH/
+svn co --force --username svnplus --password $PASSWORD svn://svn.newznab.com/nn/branches/nnplus $NEWZPATH/
 
 cd $NEWZPATH"/misc/update_scripts"
 php5 update_database_version.php
