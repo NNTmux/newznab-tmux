f8f3ac07d95c2e7b11c68fc68dade17c49294b02
jonnyboy <jonnyboy@jbmatrix.net>
2013-02-19 15:46:37 +0100

    update cj's script

--- nnscripts/remove_parts_without_releases.php
+++ nnscripts/remove_parts_without_releases.php
@@ -4,14 +4,15 @@
  * 
  * The location of the script needs to be "misc/custom" or the
  * "misc/testing" directory. if used from another location,
- * change lines 30 to 32 to require the correct files.
+ * change lines 34 to 36 to require the correct files.
  *
  * @author    NN Scripts
  * @license   http://opensource.org/licenses/MIT MIT License
  * @copyright (c) 2013 - NN Scripts
  *
  * Changelog:
- * 0.1  - Initial version
+ * 0.2 - Change retrieval of parts in groups of 10000
+ * 0.1 - Initial version
  */
 
 //----------------------------------------------------------------------
@@ -24,10 +25,13 @@ define('DISPLAY', true);
 // example: 24 for 1 day old releases (based on add date)
 // false or 0 to disable
 define('LIMIT', 48);
+
+// The number of parts that should be grouped when removing
+define('QUERYLIMIT', 10000);
 //----------------------------------------------------------------------
 
 // Load the application
-require_once("config.php");
+require(dirname(__FILE__)."/../bin/config.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 require_once('nnscripts.php');
 
@@ -49,6 +53,12 @@ class removeParts
      */
     private $db;
 
+    /**
+     * The current date and time
+     * @var DateTime
+     */
+    private $now;
+
 
 
     /**
@@ -64,6 +74,14 @@ class removeParts
         
         // Set the database variable
         $this->db = $db;
+
+        // Add the "now" date
+        $this->now = new DateTime();
+        if( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT )
+        {
+            // substract the limit hours
+            $this->now->sub( new DateInterval('PT'. LIMIT .'H') );
+        }
     }
     
     
@@ -96,12 +114,40 @@ class removeParts
             "DELETE FROM parts
              WHERE parts.binaryID = 0
              %s",
-            ( ( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT ) ? sprintf("AND parts.dateadded < NOW() - INTERVAL %d HOUR", LIMIT) : '' )
+            ( ( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT ) ? sprintf("AND parts.dateadded < '%s'", $this->now->format('Y-m-d H:i:s')) : '' )
         );
         $result = $this->db->queryDirect( $sql );
         $this->nnscripts->display( ( $result ? "Done" : "Error" ) . PHP_EOL );
     }
 
+
+    /**
+     * Get the total count of parts to remove
+     * 
+     * @return int
+     */
+    protected function getTotalPartsToRemove()
+    {
+        // Init
+        $ret = 0;
+
+        // Get the total to remove
+        $sql = sprintf("SELECT count(*) AS total
+                        FROM parts AS p
+                        LEFT JOIN binaries AS b ON (b.ID = p.binaryID)
+                        WHERE b.ID IS NULL
+                        %s",
+                        ( ( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT ) ? sprintf("AND p.dateadded < '%s'", $this->now->format('Y-m-d H:i:s')) : '' ) );
+        $result = $this->db->query( $sql );
+        if( is_array($result) && 1 === count($result) )
+        {
+            $ret = $result[0]['total'];
+        }
+
+        // Return
+        return $ret;
+    }
+    
 
     /**
      * Remove parts where the binary is missing
@@ -110,35 +156,69 @@ class removeParts
      */
     private function removePartsWithMissingBinary()
     {
-        // Remove parts with missing binary
-        $sql = sprintf(
-            "SELECT DISTINCT p.ID
-             FROM parts AS p
-             LEFT JOIN binaries AS b ON (b.ID = p.binaryID)
-             WHERE b.ID IS NULL
-             %s",
-            ( ( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT ) ? sprintf("AND p.dateadded < NOW() - INTERVAL %d HOUR", LIMIT) : '' )
-        );
-        $result = $this->db->query( $sql );
-        if( is_array( $result ) && 0 < count( $result ) )
+        // The query limit
+        $queryLimit = ( defined('QUERYLIMIT') && is_int(QUERYLIMIT) && 0 < QUERYLIMIT ? QUERYLIMIT : 10000 );
+        
+        // Get the total parts to remove
+        $total = $this->getTotalPartsToRemove();
+        if( 0 < $total )
         {
-            $this->nnscripts->display( sprintf( "Removing %d parts with missing binaries: ", count( $result ) ) );
-
-            // Build the total ID list
+            $line = sprintf( "Removing %s parts with missing binaries", number_format($total, 0, '.', '') );
+            if( $total > $queryLimit )
+                $line .= sprintf( ' (in groups of %d parts)', $queryLimit );
+            $line .= ': ';
+            $this->nnscripts->display( $line );
+            
+            // Set the lastID
+            $lastId = null;
             $ids = array();
-            $counter = 0;
-            foreach( $result AS $row )
+            
+            // Loop until there are no parts to remove
+            while( 0 < $total )
             {
-                $ids[] = $row['ID'];
-                $counter++;
-
-                if( 100 === $counter )
+                // Build the query
+                $sql = sprintf("SELECT DISTINCT p.ID
+                                FROM parts AS p
+                                LEFT JOIN binaries AS b ON (b.ID = p.binaryID)
+                                WHERE b.ID IS NULL
+                                %s
+                                %s
+                                ORDER BY p.ID ASC
+                                LIMIT %d",
+                    ( null !== $lastId ? sprintf('AND p.ID > %d', $lastId) : '' ),
+                    ( ( defined('LIMIT') && is_int(LIMIT) && 0 < LIMIT ) ? sprintf("AND p.dateadded < '%s'", $this->now->format('Y-m-d H:i:s')) : '' ),
+                    $queryLimit
+                );
+                $result = $this->db->query( $sql );
+                if( is_array( $result ) && 0 < count( $result ) )
                 {
-                    $this->nnscripts->display( "." );
-                    $sql = sprintf( "DELETE FROM parts WHERE ID IN (%s)", implode(',', $ids) );
-                    $this->db->queryDirect( $sql );
+                    // Build the total ID list
                     $ids = array();
                     $counter = 0;
+                    foreach( $result AS $row )
+                    {
+                        $ids[] = $lastId = $row['ID'];
+                        $counter++;
+                        if( $queryLimit === $counter )
+                        {
+                            $this->nnscripts->display( "." );
+                            $sql = sprintf( "DELETE FROM parts WHERE ID IN (%s)", implode(',', $ids) );
+                            $this->db->queryDirect( $sql );
+                            $ids = array();
+                            $counter = 0;
+                        }
+                    }
+
+                    // Cleanup
+                    unset( $result );
+                    
+                    // Lower the total
+                    $total -= $queryLimit;
+                }
+                else
+                {
+                    // Query without result?
+                    $total = 0; 
                 }
             }
             
@@ -152,6 +232,10 @@ class removeParts
             
             $this->nnscripts->display( PHP_EOL );
         }
+        else
+        {
+            $this->nnscripts->display( 'No parts with missing binaries to remove.'. PHP_EOL );
+        }
     }
 }
 
@@ -159,7 +243,7 @@ try
 {
     // Init
     $scriptName    = 'Remove parts which are not linked to a binary';
-    $scriptVersion = '0.1';
+    $scriptVersion = '0.2';
     
     // Load the NNscript class
     $nnscripts = new NNScripts( $scriptName, $scriptVersion );
@@ -178,3 +262,4 @@ try
 } catch( Exception $e ) {
     echo $e->getMessage() . PHP_EOL;
 }
+
