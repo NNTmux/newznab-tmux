e07b446903f4c732162c5c8ea6b05dc01e139e6b
jonnyboy <jonnyboy@jbmatrix.net>
2013-02-20 19:17:07 +0100

    removed option to run processUnwanted continuously, was causing errors with tvrage

--- bin/processUnwanted.php
+++ bin/processUnwanted.php
@@ -3,26 +3,8 @@
 require(dirname(__FILE__)."/config.php");
 require(WWW_DIR.'/lib/postprocess.php');
 
-//get variables from config.sh and defaults.sh
-$varnames = shell_exec("cat ../config.sh | grep ^export | cut -d \= -f1 | awk '{print $2;}'");
-$varnames .= shell_exec("cat ../defaults.sh | grep ^export | cut -d \= -f1 | awk '{print $2;}'");
-$vardata = shell_exec('cat ../config.sh | grep ^export | cut -d \" -f2 | awk "{print $1;}"');
-$vardata .= shell_exec('cat ../defaults.sh | grep ^export | cut -d \" -f2 | awk "{print $1;}"');
-$varnames = explode("\n", $varnames);
-$vardata = explode("\n", $vardata);
-$array = array_combine($varnames, $vardata);
-unset($array['']);
+$postprocess = new PostProcess(true);
+$postprocess->processUnwanted();
 
-if ( $array['CONTINUOUS'] == "true" ) {
-    $i = 1;
-    while ( $i > 0 )
-    {
-      $postprocess = new PostProcess(true);
-      $postprocess->processUnwanted();
-    }
-} else {
-  $postprocess = new PostProcess(true);
-  $postprocess->processUnwanted();
-}
 ?>
 
--- config.sh
+++ config.sh
@@ -199,9 +199,6 @@ export CLEANUP_EDIT="false"
 #How often do you want  update_cleanup.php and removespecial.php to run, in seconds
 export CLEANUP_TIMER="3600"
 
-#processUnwanted is the process that delets audio previews and spam, you can run one cycle each loop or run contunous
-export CONTINUOUS="false"
-
 ############################################################
 
 #Choose to run update_parsing.php true/false
