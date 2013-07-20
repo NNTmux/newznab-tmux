a500440e1f4ab5ba5d14521b713d02e16991cfa2
Darko <dkrisan@gmail.com>
2013-07-20 08:25:59 +0200

    Some more changes

---
+++ hash_matching/omgwtfnzbs.php
@@ -0,0 +1,53 @@
+<?php
+require_once(dirname(__FILE__)."/../bin/config.php");
+require_once(WWW_DIR ."/lib/framework/db.php");
+require_once(WWW_DIR ."/lib/util.php");
+require_once ("hashcompare.php");
+
+//This script is adapted from nZEDb predb.php script
+
+        $db = new DB();
+		$newnames = 0;
+
+		$buffer = getUrl("http://rss.omgwtfnzbs.org/rss-info.php");
+        echo "Requesting pre info from omgwtfnzbs.org rss feed ...\n";
+		if ($buffer !== false && strlen($buffer))
+		{
+			if (preg_match_all('/<item>.+?<\/item>/s', $buffer, $matches))
+			{
+				foreach ($matches as $match)
+				{
+					foreach ($match as $m)
+					{
+						if (preg_match('/<title>(?P<title>.+?)<\/title.+?pubDate>(?P<date>.+?)<\/pubDate.+?gory:<\/b> (?P<category>.+?)<br \/.+?<\/b> (?P<size1>.+?) (?P<size2>[a-zA-Z]+)<b/s', $m, $matches2))
+						{
+							$oldname = $db->queryOneRow(sprintf("SELECT title, source, ID FROM prehash WHERE title = %s", $db->escapeString($matches2["title"])));
+							if ($oldname["title"] == $matches2["title"])
+							{
+								if ($oldname["source"] == "womble")
+								{
+									continue;
+								}
+								else
+								{
+									$size = $db->escapeString(round($matches2["size1"]).$matches2["size2"]);
+									$db->query(sprintf("UPDATE prehash SET size = %s, category = %s, predate = FROM_UNIXTIME(".strtotime($matches2["date"])."), adddate = now(), source = %s where ID = %d", $size, $db->escapeString($matches2["category"]), $db->escapeString("omgwtfnzbs"), $oldname["ID"]));
+									$newnames++;
+								}
+							}
+							else
+							{
+								$size = $db->escapeString(round($matches2["size1"]).$matches2["size2"]);
+                                $title = preg_replace("/  - omgwtfnzbs.org/", "", $matches2["title"]);
++                               $db->query(sprintf("INSERT IGNORE INTO predb (title, size, category, predate, adddate, source, md5) VALUES (%s, %s, %s, FROM_UNIXTIME(".strtotime($matches2["date"])."), now(), %s, %s)", $db->escapeString($title), $size, $db->escapeString($matches2["category"]), $db->escapeString("omgwtfnzbs"), $db->escapeString(md5($matches2["title"]))));
+								$newnames++;
+							}
+						}
+					}
+				}
+			}
+		}
+		return $newnames;
+
+
+        ?>
---
+++ hash_matching/predbme.php
@@ -0,0 +1,37 @@
+<?php
+require_once(dirname(__FILE__)."/../bin/config.php");
+require_once(WWW_DIR ."/lib/framework/db.php");
+require_once(WWW_DIR ."/lib/util.php");
+require_once ("hashcompare.php");
+
+//This script is adapted from nZEDb predb.php script
+
+$db = new DB();
+		$newnames = 0;
+		$arr = array("http://predb.me/?cats=movies-sd&rss=1", "http://predb.me/?cats=movies-hd&rss=1", "http://predb.me/?cats=movies-discs&rss=1", "http://predb.me/?cats=tv-sd&rss=1", "http://predb.me/?cats=tv-hd&rss=1", "http://predb.me/?cats=tv-discs&rss=1", "http://predb.me/?cats=music-audio&rss=1", "http://predb.me/?cats=music-video&rss=1", "http://predb.me/?cats=music-discs&rss=1", "http://predb.me/?cats=games-pc&rss=1", "http://predb.me/?cats=games-xbox&rss=1", "http://predb.me/?cats=games-playstation&rss=1", "http://predb.me/?cats=games-nintendo&rss=1", "http://predb.me/?cats=apps-windows&rss=1", "http://predb.me/?cats=apps-linux&rss=1", "http://predb.me/?cats=apps-mac&rss=1", "http://predb.me/?cats=apps-mobile&rss=1", "http://predb.me/?cats=books-ebooks&rss=1", "http://predb.me/?cats=books-audio-books&rss=1", "http://predb.me/?cats=xxx-videos&rss=1", "http://predb.me/?cats=xxx-images&rss=1", "http://predb.me/?cats=dox&rss=1", "http://predb.me/?cats=unknown&rss=1");
+
+        echo "Requesting pre info from predb.me ...\n";
+
+		foreach ($arr as &$value)
+		{
+			$releases = @simplexml_load_file($value);
+			if ($releases !== false)
+			{
+				foreach ($releases->channel->item as $release)
+				{
+					$oldname = $db->queryOneRow(sprintf("SELECT title FROM prehash WHERE title = %s", $db->escapeString($release->title)));
+					if ($oldname["title"] == $release->title)
+						continue;
+					else
+					{
+						$db->query(sprintf("INSERT IGNORE INTO prehash (title, predate, adddate, source, hash) VALUES (%s, now(), now(), %s, %s)", $db->escapeString($release->title), $db->escapeString("predbme"), $db->escapeString(md5($release->title))));
+						$newnames++;
+					}
+				}
+			}
+		}
+		return $newnames;
+
+
+
+?>
