a963dcf823400545748f8143b2ceedb146a5524a
Darko <dkrisan@gmail.com>
2013-07-21 12:40:07 +0200

    Updated sql

--- hash_matching/db_updates/prehash_table_update_18.07.2013.sql
+++ hash_matching/db_updates/prehash_table_update_18.07.2013.sql
@@ -1,7 +1,7 @@
-ALTER TABLE  `prehash` ADD  `source` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  '' AFTER  `predate`
-ALTER TABLE  `prehash` ADD  `nfo` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `releasename`
-ALTER TABLE  `prehash` ADD  `size` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `nfo`
-ALTER TABLE  `prehash` ADD  `category` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `size`
-ALTER TABLE  `prehash` CHANGE  `hash`  `hash` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  '0'
-ALTER TABLE  `prehash` ADD  `releaseID` INT( 11 ) NULL DEFAULT NULL AFTER  `source`
-ALTER TABLE  `prehash` ADD  `adddate` DATETIME NULL DEFAULT NULL AFTER  `predate`
+ALTER TABLE  `prehash` ADD  `source` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  '' AFTER  `predate`;
+ALTER TABLE  `prehash` ADD  `nfo` VARCHAR( 500 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `releasename`;
+ALTER TABLE  `prehash` ADD  `size` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `nfo`;
+ALTER TABLE  `prehash` ADD  `category` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL AFTER  `size`;
+ALTER TABLE  `prehash` CHANGE  `hash`  `hash` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  '0';
+ALTER TABLE  `prehash` ADD  `releaseID` INT( 11 ) NULL DEFAULT NULL AFTER  `source`;
+ALTER TABLE  `prehash` ADD  `adddate` DATETIME NULL DEFAULT NULL AFTER  `predate`;
