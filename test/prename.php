432b1afa4c7b45416899fdb3522ace3f3473a473
Darko <dkrisan@gmail.com>
2013-09-16 14:46:41 +0200

    finaly something is fixed

--- test/prename.php
+++ test/prename.php
@@ -23,16 +23,16 @@ function preName($argv)
         $counter=1;
         $n = "\n";
         echo "Resetting blank searchnames\n";
-        $bad = $db->queryDirect("UPDATE releases SET searchname = name WHERE searchname = ''");
+        $bad = $db->query("UPDATE releases SET searchname = name WHERE searchname = ''");
         $row = mysqli_fetch_array($bad);
         $tot = $row [0];
         if ($tot > 0)
                 echo $tot." Releases had no searchname\n";
         echo "Getting work\n";
         if (isset($argv[1]) && $argv[1]=="full")
-                $res = $db->queryDirect("SELECT ID, name, searchname, groupID, categoryID from releases WHERE relnamestatus IN (0, 1, 7, 20, 21, 22) or categoryID BETWEEN 8000 and 8999");
+                $res = $db->queryDirect("SELECT ID, name, searchname, groupID, categoryID from releases WHERE relnamestatus IN (0, 1, 7, 20, 21, 22) and categoryID BETWEEN 8000 and 8999");
         else
-                $res = $db->queryDirect("SELECT ID, name, searchname, groupID, categoryID FROM releases WHERE relnamestatus IN (0, 1, 7, 20, 21, 22) or categoryID between 8000 and 8999 and adddate > NOW() - INTERVAL 4 HOUR");
+                $res = $db->queryDirect("SELECT ID, name, searchname, groupID, categoryID FROM releases WHERE relnamestatus IN (0, 1, 7, 20, 21, 22) and categoryID between 8000 and 8999 and adddate > NOW() - INTERVAL 4 HOUR");
         $row = mysqli_fetch_array($res);
         $total = $row [0];
         if ($total > 0)
