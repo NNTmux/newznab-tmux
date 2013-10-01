81f4c3cda52437205af3b7587c4ac8bdc8a374fe
Darko <dkrisan@gmail.com>
2013-10-01 15:54:32 +0200

    Changes in namefixer.

--- test/namefixer.php
+++ test/namefixer.php
@@ -201,7 +201,7 @@ class Namefixer
 
 				$category = new Category();
                 $functions = new Functions ();
-				$determinedcat = $category->determineCategory($newname, $release["groupID"]);
+				$determinedcat = $category->determineCategory($groupname, $newname);
 
 				if ($type === "PAR2, ")
 					$newname = ucwords($newname);
@@ -236,15 +236,18 @@ class Namefixer
 					if ($namestatus == 1)
 					{
 					   if ($type == "NFO, ")
-							$status = 8;
+                            //$status = 8
+							$db->queryDirect(sprintf("UPDATE releases set searchname = %s, relnamestatus = 8, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $determinedcat, $release["releaseID"]));
 						else if ($type == "PAR2, ")
-							$status = 7;
+                            //$status = 7
+							$db->query(sprintf("UPDATE releases set searchname = %s, relnamestatus = 7, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $determinedcat, $release["releaseID"]));
 						else if ($type == "Filenames, ")
-							$status = 9;
-						$db->queryDirect(sprintf("UPDATE releases set searchname = %s, relnamestatus = %d, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $status, $determinedcat, $release["releaseID"]));
+                            //$status = 9
+							$db->query(sprintf("UPDATE releases set searchname = %s, relnamestatus = 9, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $determinedcat, $release["releaseID"]));
+						//$db->query(sprintf("UPDATE releases set searchname = %s, relnamestatus = %d, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $status, $determinedcat, $release["releaseID"]));
 					}
 					else
-						$db->queryDirect(sprintf("UPDATE releases set searchname = %s, categoryID = %d where ID = %d", $db->escapeString($newname), $determinedcat, $release["releaseID"]));
+						$db->query(sprintf("UPDATE releases set searchname = %s, categoryID = %d where ID = %d", $db->escapeString($newname), $determinedcat, $release["releaseID"]));
 				}
 			}
 		}
