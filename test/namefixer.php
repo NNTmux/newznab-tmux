8e746d299ed8e929e1f134e3412bbb773bc621a6
Darko <dkrisan@gmail.com>
2013-10-01 18:58:08 +0200

    Reverted to an old behaviour, hopefuly this will fix missing relnamestatus updates in db.

--- test/namefixer.php
+++ test/namefixer.php
@@ -242,7 +242,7 @@ class Namefixer
                             $status = 7;
 					   else if ($type == "Filenames, ")
                             $status = 9;
-						$db->queryDirect(sprintf("UPDATE releases set searchname = %s, relnamestatus = %d, categoryID = %d where ID = %d", $db->escapeString(substr($newname, 0, 255)), $status, $determinedcat, $release["releaseID"]));
+						$db->queryDirect(sprintf("UPDATE releases set searchname = %s, relnamestatus = %d, categoryID = %d where ID = %d", $db->escapeString($newname), $status, $determinedcat, $release["releaseID"]));
 					}
 					else
 						$db->query(sprintf("UPDATE releases set searchname = %s, categoryID = %d where ID = %d", $db->escapeString($newname), $determinedcat, $release["releaseID"]));
