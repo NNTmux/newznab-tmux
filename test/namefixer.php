ef4d0748878b2f6062c602a1bdc9f0650afff055
DariusIII <dkrisan@gmail.com>
2013-11-19 21:33:34 +0100

    Another try to fix the shown count

--- test/namefixer.php
+++ test/namefixer.php
@@ -173,11 +173,11 @@ class Namefixer
 		if ($time == 2 && $cats == 2)
 			$relres = $db->queryDirect($query.$this->fullother);
 
-        $row = mysqli_fetch_array($relres);
-        $total = $row[0];
-        if ($total > 0)
+        $rowcount = $db->getAffectedRows();
+
+        if (count ($relres) > 0)
 		    {
-            echo $total." release(s) to process.\n";
+            echo $rowcount." release(s) to process.\n";
 		    $db = $this->db;
 			$nzbcontents = new NZBcontents($this->echooutput);
             $pp = new Functions ($this->echooutput);
