503981433df611f53b7605a5de0a5f70267ebf83
Darko <dkrisan@gmail.com>
2013-12-10 12:56:32 +0100

    Another typo. Greetings to afly

--- test/jb_fix_names.php
+++ test/jb_fix_names.php
@@ -84,7 +84,7 @@ function preName($argv)
 				if ( $cleanerName != $row['name'] )
 				{
 					$determinedcat = $category->determineCategory($row["groupID"], $cleanName);
-					$run = $db->exec(sprintf("UPDATE releases set relnamestatus = 16, searchname = %s, categoryID = %d where ID = %d", $db->escapeString($cleanerName), $db->escapeString($determinedcat), $db->escapeString($row['ID'])));
+					$run = $db->exec(sprintf("UPDATE releases set relnamestatus = 16, searchname = %s, categoryID = %d where ID = %d", $db->escapeString($cleanName), $db->escapeString($determinedcat), $db->escapeString($row['ID'])));
 					$groupname = $functions->getByNameByID($row["groupID"]);
 					$oldcatname = $functions->getNameByID($row["categoryID"]);
 					$newcatname = $functions->getNameByID($determinedcat);
