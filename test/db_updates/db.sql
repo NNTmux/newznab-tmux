7708e4f2f9a8a02354f2add6a6e8759d784588aa
DariusIII <dkrisan@gmail.com>
2013-12-11 08:56:41 +0100

    Added reqidstatus to releases table for future use

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -4,11 +4,13 @@ ALTER TABLE  `releases` ADD  `dehashstatus` TINYINT( 1 ) NOT NULL DEFAULT  '0' A
 ALTER TABLE  `releases` ADD  `nfostatus` TINYINT NOT NULL DEFAULT 0 after `dehashstatus`;
 ALTER TABLE  `releases` ADD  `relnamestatus` TINYINT NOT NULL DEFAULT 1 after `nfostatus`;
 ALTER TABLE	 `releases` ADD  `hashed` BOOL DEFAULT FALSE after `relnamestatus`; 
-ALTER TABLE	 `releases` ADD  `nzbstatus` TINYINT NOT NULL DEFAULT 0 after `hashed`; 
+ALTER TABLE	 `releases` ADD  `nzbstatus` TINYINT NOT NULL DEFAULT 0 after `hashed`;
+ALTER TABLE  `releases` ADD  `reqidstatus` TINYINT(1) NOT NULL DEFAULT '0' AFTER `relnamestatus`;
 CREATE INDEX ix_releases_hashed on releases(hashed);
 CREATE INDEX ix_releases_mergedreleases on releases(dehashstatus, relnamestatus, passwordstatus);
 CREATE INDEX ix_releases_nzbstatus ON releases(nzbstatus);
 CREATE INDEX ix_releases_nfostatus ON releases (nfostatus ASC) USING HASH;
+CREATE INDEX ix_releases_reqidstatus ON releases(reqidstatus ASC) USING HASH;
 UPDATE releases SET hashed = true WHERE searchname REGEXP '[a-fA-F0-9]{32}' OR name REGEXP '[a-fA-F0-9]{32}'; 
 UPDATE releases SET nzbstatus = 1;
 delimiter //
