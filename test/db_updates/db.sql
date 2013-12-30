2feff25979194760ba1efa64e9327c934bdb3d98
Darko <dkrisan@gmail.com>
2013-12-30 09:44:00 +0100

    Small changes in scripts, probable bugfix for dead monitor on start

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -3,7 +3,7 @@ ALTER TABLE  `predb` ADD INDEX (  `hash` ( 32 ) );
 ALTER TABLE  `releases` ADD  `dehashstatus` TINYINT( 1 ) NOT NULL DEFAULT  '0' AFTER  `haspreview`;
 ALTER TABLE  `releases` ADD  `nfostatus` TINYINT NOT NULL DEFAULT 0 after `dehashstatus`;
 ALTER TABLE  `releases` ADD  `relnamestatus` TINYINT NOT NULL DEFAULT 1 after `nfostatus`;
-ALTER TABLE	 `releases` ADD  `hashed` BOOL DEFAULT FALSE after `relnamestatus`; 
+ALTER TABLE	 `releases` ADD  `hashed` BOOL DEFAULT FALSE after `relnamestatus`;
 ALTER TABLE	 `releases` ADD  `nzbstatus` TINYINT NOT NULL DEFAULT 0 after `hashed`;
 ALTER TABLE  `releases` ADD  `reqidstatus` TINYINT(1) NOT NULL DEFAULT '0' AFTER `relnamestatus`;
 CREATE INDEX ix_releases_hashed on releases(hashed);
@@ -16,10 +16,12 @@ UPDATE releases SET nzbstatus = 1;
 UPDATE releases SET reqidstatus = -1 WHERE reqidstatus = 0 AND nzbstatus = 1 AND relnamestatus IN (0, 1) AND name REGEXP '^\\[[[:digit:]]+\\]' = 0;
 delimiter //
 CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW BEGIN IF NEW.name REGEXP '^\\[[[:digit:]]+\\]' = 0 THEN SET NEW.reqidstatus = -1; ELSEIF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.hashed = true; END IF; END;
+//
 delimiter ;
 delimiter //
 CREATE TRIGGER check_update BEFORE UPDATE ON releases FOR EACH ROW BEGIN IF NEW.name REGEXP '^\\[[[:digit:]]+\\]' = 0 THEN SET NEW.reqidstatus = -1; ELSEIF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.hashed = true; END IF; END;
-delimiter;
+//
+delimiter ;
 		
 
 
