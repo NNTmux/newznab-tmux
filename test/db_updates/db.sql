681657aec75a44ac5a1cc91cfeaa25b1942a5093
Darko <dkrisan@gmail.com>
2014-01-16 15:41:05 +0100

    Wrong stuff, that is what you get when you use automatic name replacement.

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -6,8 +6,8 @@ ALTER TABLE  `releases`
   ADD `nfostatus` TINYINT NOT NULL DEFAULT 0 after `dehashstatus`,
   ADD `reqidstatus` TINYINT(1) NOT NULL DEFAULT '0' AFTER `dehashstatus`,
   ADD `bitwise` SMALLINT UNSIGNED NOT NULL DEFAULT 0 AFTER `reqidstatus`,
-  ADD INDEX `ix_releases_nfostatus` (`nfostatus` ASC) USING md5,
-  ADD INDEX `ix_releases_reqidstatus` (`reqidstatus` ASC) USING md5,
+  ADD INDEX `ix_releases_nfostatus` (`nfostatus` ASC) USING HASH,
+  ADD INDEX `ix_releases_reqidstatus` (`reqidstatus` ASC) USING HASH,
   ADD INDEX `ix_releases_status` (`ID`, `nfostatus`, `bitwise`, `passwordstatus`, `dehashstatus`, `reqidstatus`, `musicinfoID`, `consoleinfoID`, `bookinfoID`, `haspreview`, `categoryID`);
 
 DELIMITER $$
