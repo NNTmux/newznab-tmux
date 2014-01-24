7b2681373dcd3e0b4122229cc3758a5bd78774e1
Darko <dkrisan@gmail.com>
2014-01-24 09:08:45 +0100

    Update, rearrangement of items

--- test/resetRelnameStatus.php
+++ test/resetRelnameStatus.php
@@ -11,10 +11,11 @@ require_once ("ColorCLI.php");
 
 $c = new ColorCLI();
 
-
 if (!isset($argv[1])) {
-    passthru('clear');
-    exit($c->error("\nThis script will set bitwise = 0 or all rename bits to unchecked or just specific bits.\nTo reset all releases to 0, run:\nphp $argv[0] true\n\nTo reset all rename bits run:\nphp $argv[0] rename\n\nTo run on specific bit, run:\nphp $argv[0] 512\n\n"));
+	exit($c->error("\nThis script will set bitwise = 0 or all rename bits to unchecked or just specific bits.\n\n"
+					. "php $argv[0] true           ...: To reset bitwise on all releases to 0.\n"
+					. "php $argv[0] rename         ...: To reset bitwise on all releases for just rename bits (4, 8, 16, 32, 64, 128).\n"
+					. "php $argv[0] 512            ...: To reset a specific bit.\n"));
 }
 
 $db = new DB();
