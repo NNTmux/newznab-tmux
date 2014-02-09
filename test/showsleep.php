117988cee39370c41e4f404b047182f515c49219
DariusIII <dkrisan@gmail.com>
2014-02-09 22:25:11 +0100

    This file is needed

---
+++ test/showsleep.php
@@ -0,0 +1,11 @@
+<?php
+//This script is ported from nZEDb and adapted for newznab
+require_once(dirname(__FILE__)."/../bin/config.php");
+require_once("consoletools.php");
+
+// This script is simply so I can show sleep progress in bash script
+$consoletools = new ConsoleTools();
+if (isset($argv[1]) && is_numeric($argv[1]))
+{
+	$consoletools->showsleep($argv[1]);
+}
