f7c2360e656b9f24d8965b109684b5c59c7413c7
DariusIII <dkrisan@gmail.com>
2014-02-14 21:54:10 +0100

    Rearrange some items

--- test/db_updates/db_update_13_02_2014.sql
+++ test/db_updates/db_update_13_02_2014.sql
@@ -1,8 +1,8 @@
+DROP INDEX `ix_releases_status` on releases;
 CREATE INDEX ix_releases_bitwise on releases(bitwise);
 CREATE INDEX ix_releases_passwordstatus on releases(passwordstatus);
 CREATE INDEX ix_releases_dehashstatus ON releases(dehashstatus);
 CREATE INDEX ix_releases_haspreview ON releases (haspreview ASC) USING HASH;
 CREATE INDEX ix_releases_postdate_name ON releases (postdate, name);
-DROP INDEX `ix_releases_status` on releases;
 CREATE INDEX ix_releases_status ON releases (ID, nfostatus, bitwise, passwordstatus, dehashstatus, reqidstatus, musicinfoID, consoleinfoID, bookinfoID, haspreview, categoryID, imdbID, rageID, groupID);
 
