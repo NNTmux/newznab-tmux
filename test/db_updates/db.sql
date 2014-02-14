45b2844328a3a43b1384c64e561d2a799365c572
DariusIII <dkrisan@gmail.com>
2014-02-14 23:22:51 +0100

    Typos

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -8,11 +8,11 @@ ALTER TABLE  `releases`
   ADD `bitwise` SMALLINT UNSIGNED NOT NULL DEFAULT 0 AFTER `reqidstatus`,
   ADD INDEX `ix_releases_nfostatus` (`nfostatus` ASC) USING HASH,
   ADD INDEX `ix_releases_reqidstatus` (`reqidstatus` ASC) USING HASH,
-  CREATE INDEX ix_releases_bitwise on releases(bitwise);
-  CREATE INDEX ix_releases_passwordstatus on releases(passwordstatus);
-  CREATE INDEX ix_releases_dehashstatus ON releases(dehashstatus);
-  CREATE INDEX ix_releases_haspreview ON releases (haspreview ASC) USING HASH;
-  CREATE INDEX ix_releases_postdate_name ON releases (postdate, name);
+  CREATE INDEX ix_releases_bitwise on releases(bitwise),
+  CREATE INDEX ix_releases_passwordstatus on releases(passwordstatus),
+  CREATE INDEX ix_releases_dehashstatus ON releases(dehashstatus),
+  CREATE INDEX ix_releases_haspreview ON releases (haspreview ASC) USING HASH,
+  CREATE INDEX ix_releases_postdate_name ON releases (postdate, name),
   CREATE INDEX ix_releases_status ON releases (ID, nfostatus, bitwise, passwordstatus, dehashstatus, reqidstatus, musicinfoID, consoleinfoID, bookinfoID, haspreview, categoryID, imdbID, rageID, groupID);
 
 DELIMITER $$
