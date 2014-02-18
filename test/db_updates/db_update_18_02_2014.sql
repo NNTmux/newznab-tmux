b518c040953f423ee4399fdbde7f7c3ca475e96c
Darko <dkrisan@gmail.com>
2014-02-18 14:30:46 +0100

    ooopsie

--- test/db_updates/db_update_18_02_2014.sql
+++ test/db_updates/db_update_18_02_2014.sql
@@ -18,5 +18,3 @@ DROP TRIGGER IF EXISTS check_update;
 
 CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;
 CREATE TRIGGER check_update BEFORE UPDATE ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;
-
-UPDATE tmux SET value = '' WHERE setting = 'sqlpatch';
