f3ba3f99c982820f8892bef44e9a36db8464aa63
Darko <dkrisan@gmail.com>
2014-02-20 14:56:49 +0100

    Fixes

--- bin/postprocess_nfo.php
+++ bin/postprocess_nfo.php
@@ -4,7 +4,7 @@ require(dirname(__FILE__)."/config.php");
 require(dirname(__FILE__)."/temp/postprocess1.php");
 
 $postprocess = new PostProcess1(true);
-$postprocess->processNfos1();
+$postprocess->processNfos();
 
 ?>
 
