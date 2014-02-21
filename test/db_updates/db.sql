8d32e6daf0b09b91ddf93c3430ef81545cd3c023
DariusIII <dkrisan@gmail.com>
2014-02-21 23:33:36 +0100

    Updated db.sql

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -5,24 +5,30 @@ ALTER TABLE  `releases`
   ADD `dehashstatus` TINYINT( 1 ) NOT NULL DEFAULT  '0' AFTER  `haspreview`,
   ADD `nfostatus` TINYINT NOT NULL DEFAULT 0 after `dehashstatus`,
   ADD `reqidstatus` TINYINT(1) NOT NULL DEFAULT '0' AFTER `nfostatus`,
-  ADD `bitwise` SMALLINT UNSIGNED NOT NULL DEFAULT 0 AFTER `reqidstatus`,
+  ADD COLUMN `nzbstatus` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `iscategorized` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `isrenamed` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `ishashed` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `isrequestid` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `proc_par2` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `proc_nfo` BIT NOT NULL DEFAULT 0,
+  ADD COLUMN `proc_files` BIT NOT NULL DEFAULT 0,
   ADD INDEX `ix_releases_nfostatus` (`nfostatus` ASC) USING HASH,
   ADD INDEX `ix_releases_reqidstatus` (`reqidstatus` ASC) USING HASH,
-  ADD INDEX `ix_releases_bitwise` (`bitwise`),
   ADD INDEX `ix_releases_passwordstatus` (`passwordstatus`),
   ADD INDEX `ix_releases_dehashstatus` (`dehashstatus`),
   ADD INDEX `ix_releases_haspreview` (`haspreview` ASC) USING HASH,
   ADD INDEX `ix_releases_postdate_name` (`postdate`, `name`),
-  ADD INDEX `ix_releases_status` (`ID`, `nfostatus`, `bitwise`, `passwordstatus`, `dehashstatus`, `reqidstatus`, `musicinfoID`, `consoleinfoID`, `bookinfoID`, `haspreview`, `categoryID`, `imdbID`, `rageID`, `groupID`);
+  ADD INDEX `ix_releases_status` (`iscategorized`, `isrenamed`, `nfostatus`, `ishashed`, `passwordstatus`, `dehashstatus`, `releasenfoID`, `musicinfoID`, `consoleinfoID`, `bookinfoID`, `haspreview`, `categoryID`, `imdbID`, `rageID`);
+
+DROP TRIGGER IF EXISTS check_insert;
+DROP TRIGGER IF EXISTS check_update;
 
 DELIMITER $$
-CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.bitwise = ((NEW.bitwise & ~512)|512);ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.bitwise = ((NEW.bitwise & ~1024)|1024); END IF; END; $$
-CREATE TRIGGER check_update BEFORE UPDATE ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.bitwise = ((NEW.bitwise & ~512)|512);ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.bitwise = ((NEW.bitwise & ~1024)|1024); END IF; END; $$
+CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;$$
+CREATE TRIGGER check_update BEFORE UPDATE ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;$$
 DELIMITER ;
 
-UPDATE releases set bitwise = 0;
-UPDATE releases SET bitwise = ((bitwise & ~512)|512) WHERE searchname REGEXP '[a-fA-F0-9]{32}' OR name REGEXP '[a-fA-F0-9]{32}';
-UPDATE releases SET bitwise = ((bitwise & ~1024)|1024) WHERE name REGEXP '^\\[[[:digit:]]+\\]';
 
 
 
