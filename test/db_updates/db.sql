b5965a565683687f6f8ad5cd7d7aaf93a37176d0
Darko <dkrisan@gmail.com>
2014-03-06 11:53:11 +0100

    Integrated prehash table creation into db.sql

--- test/db_updates/db.sql
+++ test/db_updates/db.sql
@@ -21,6 +21,30 @@ ALTER TABLE  `releases`
   ADD INDEX `ix_releases_postdate_name` (`postdate`, `name`),
   ADD INDEX `ix_releases_status` (`iscategorized`, `isrenamed`, `nfostatus`, `ishashed`, `passwordstatus`, `dehashstatus`, `releasenfoID`, `musicinfoID`, `consoleinfoID`, `bookinfoID`, `haspreview`, `categoryID`, `imdbID`, `rageID`);
 
+DROP TABLE IF EXISTS prehash;
+CREATE TABLE prehash (
+	ID INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	title VARCHAR(255) NOT NULL DEFAULT '',
+	nfo VARCHAR(255) NULL,
+	size VARCHAR(50) NULL,
+	category VARCHAR(255) NULL,
+	predate DATETIME DEFAULT NULL,
+	adddate DATETIME DEFAULT NULL,
+	source VARCHAR(50) NOT NULL DEFAULT '',
+	md5 VARCHAR(255) NOT NULL DEFAULT '0',
+	requestID INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	groupID INT(10) UNSIGNED NOT NULL DEFAULT '0',
+	PRIMARY KEY (ID)
+) ENGINE=INNODB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1;
+
+CREATE INDEX ix_prehash_title ON prehash(title);
+CREATE INDEX ix_prehash_nfo ON prehash(nfo);
+CREATE INDEX ix_prehash_predate ON prehash(predate);
+CREATE INDEX ix_prehash_adddate ON prehash(adddate);
+CREATE INDEX ix_prehash_source ON prehash(source);
+CREATE INDEX ix_prehash_requestid on prehash(requestID, groupID);
+CREATE UNIQUE INDEX ix_prehash_md5 ON prehash(md5);
+
 DROP TRIGGER IF EXISTS check_insert;
 DROP TRIGGER IF EXISTS check_update;
 
