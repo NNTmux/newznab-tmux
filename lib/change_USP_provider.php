23910eef5c59786d06ed8f4b4b5884a850abf47c
Darko <dkrisan@gmail.com>
2014-03-26 13:38:27 +0100

    Missed it

--- lib/change_USP_provider.php
+++ lib/change_USP_provider.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__)."/../bin/config.php");
+require_once(WWW_DIR. "lib/framework/db.php");  
 require_once(WWW_DIR."/lib/nntp.php");
 require_once(WWW_DIR."/lib/backfill.php");
 require_once("ColorCLI.php");
--- lib/reset_postprocessing.php
+++ lib/reset_postprocessing.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__)."/../bin/config.php");
+require_once(WWW_DIR. "lib/framework/db.php");
 require_once("ColorCLI.php");
 require_once("consoletools.php");
 
