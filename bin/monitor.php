e0a438f5f5d04ff9957a1259ed3b85af0dd648bc
DariusIII <dkrisan@gmail.com>
2014-03-28 21:26:43 +0100

    Changed the query in ReleaseRemover

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__)."/../lib/showsleep.php");
 require_once(dirname(__FILE__)."/../lib/functions.php");
 
 
-$version="0.3r1037";
+$version="0.3r1038";
 
 $db = new DB();
 $functions = new Functions();
