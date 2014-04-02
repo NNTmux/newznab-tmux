ab589217c398c52ae159325da8e3d48873b5acf2
Darko <dkrisan@gmail.com>
2014-04-02 10:01:11 +0200

    updated the description, no change in patch

--- lib/DB/patches/db_update_02_04_2014_prehash.sql
+++ lib/DB/patches/db_update_02_04_2014_prehash.sql
@@ -4,7 +4,7 @@ ALTER TABLE prehash DROP INDEX ix_prehash_adddate;
 /* Drop the adddate column */
 ALTER TABLE prehash DROP COLUMN adddate;
 
-/* Use the site to keep the last pre time (unixtime) */
+/* Use tmux table to keep the last pre time (unixtime) */
 INSERT INTO tmux (setting, value) VALUES ('lastpretime', '0');
 
 UPDATE tmux SET value = '14' WHERE setting = 'sqlpatch';
