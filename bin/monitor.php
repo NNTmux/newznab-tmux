fbbed6c71797f554c45173120b038d3844be7e17
DariusIII <dkrisan@gmail.com>
2014-04-06 23:11:27 +0200

    Change in nntp library used

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__)."/../lib/showsleep.php");
 require_once(dirname(__FILE__)."/../lib/functions.php");
 
 
-$version="0.3r1066";
+$version="0.3r1067";
 
 $db = new DB();
 $functions = new Functions();
