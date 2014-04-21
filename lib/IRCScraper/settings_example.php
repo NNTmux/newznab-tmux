b2497df28060129d025a81684708c75a73def6b6
DariusIII <dkrisan@gmail.com>
2014-04-21 13:42:56 +0200

    Reverted changes to IRCScraper for now.

--- lib/IRCScraper/settings_example.php
+++ lib/IRCScraper/settings_example.php
@@ -5,12 +5,11 @@ $username = '';
 
 // EFNET server details.
 define('SCRAPE_IRC_EFNET_SERVER', 'irc.Prison.NET'); // Efnet server address, change if you have issues connecting.
-define('SCRAPE_IRC_EFNET_PORT', '6667');             // Port for efnet server.
-define('SCRAPE_IRC_EFNET_NICKNAME', "$username");    // Nick name (this is the nickname everyone sees in the channel)
-define('SCRAPE_IRC_EFNET_REALNAME', "$username");    // This is a name that people see in /whois, you can set this to your nickname.
-define('SCRAPE_IRC_EFNET_USERNAME', "$username");    // This is part of your hostname, you can set this the same as nickname. This is also used to log in to ZNC.
-define('SCRAPE_IRC_EFNET_PASSWORD', false);          // This is used for bouncers like ZNC, set this false or '' if you don't have a bouncer.
-define('SCRAPE_IRC_EFNET_ENCRYPTION', false);        // Set to true to use TLS encryption (make sure you change the port to a SSL one).
+define('SCRAPE_IRC_EFNET_PORT', '6667'); // Port for efnet server.
+define('SCRAPE_IRC_EFNET_NICKNAME', "$username"); // Nick name (this is the nickname everyone sees in the channel)
+define('SCRAPE_IRC_EFNET_REALNAME', "$username"); // This is a name that people see in /whois, you can set this to your nickname.
+define('SCRAPE_IRC_EFNET_USERNAME', "$username"); // This is part of your hostname, you can set this the same as nickname. This is also used to log in to ZNC.
+define('SCRAPE_IRC_EFNET_PASSWORD', false); // This is used for bouncers like ZNC, set this false or '' if you don't have a bouncer.
 // List of ignored channels, separated by commas. ie '#alt.binaries.teevee,#alt.binaries.moovee' for a single channel : '#alt.binaries.teevee'
 define('SCRAPE_IRC_EFNET_IGNORED_CHANNELS', '');
 
@@ -23,7 +22,6 @@ define('SCRAPE_IRC_CORRUPT_NICKNAME', "$username");
 define('SCRAPE_IRC_CORRUPT_REALNAME', "$username");
 define('SCRAPE_IRC_CORRUPT_USERNAME', "$username");
 define('SCRAPE_IRC_CORRUPT_PASSWORD', false);
-define('SCRAPE_IRC_CORRUPT_ENCRYPTION', false);
 
 // Zenet server details.
 define('SCRAPE_IRC_ZENET_SERVER', 'irc.zenet.org');
@@ -32,4 +30,3 @@ define('SCRAPE_IRC_ZENET_NICKNAME', "$username");
 define('SCRAPE_IRC_ZENET_REALNAME', "$username");
 define('SCRAPE_IRC_ZENET_USERNAME', "$username");
 define('SCRAPE_IRC_ZENET_PASSWORD', false);
-define('SCRAPE_IRC_ZENET_ENCRYPTION', false);
