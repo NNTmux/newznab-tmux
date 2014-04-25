76f89a020bbf1947f83fe0e9a4160fdd34666cc3
DariusIII <dkrisan@gmail.com>
2014-04-26 00:22:18 +0200

    Removed encryption requirement.

--- lib/IRCScraper/scrape.php
+++ lib/IRCScraper/scrape.php
@@ -26,15 +26,6 @@ if (!in_array($argv[1], array('efnet', 'cz'))) {
 	exit('Error, must be efnet or cz, you typed: ' . $argv[1] . PHP_EOL);
 }
 
-require_once('/var/www/newznab/misc/update_scripts/nix_scripts/tmux/lib/IRCScraper/settings.php');
-
-if (!defined('SCRAPE_IRC_EFNET_ENCRYPTION') ||
-	!defined('SCRAPE_IRC_CORRUPT_ENCRYPTION') ||
-	!defined('SCRAPE_IRC_ZENET_ENCRYPTION')
-) {
-	exit ('ERROR! You must update your settings.php using settings_example.php (the encryption setting was added)' . PHP_EOL);
-}
-
 if (SCRAPE_IRC_EFNET_NICKNAME == '' || SCRAPE_IRC_CORRUPT_NICKNAME == '' || SCRAPE_IRC_ZENET_NICKNAME == '') {
 	exit("ERROR! You must put a username in settings.php" . PHP_EOL);
 }
