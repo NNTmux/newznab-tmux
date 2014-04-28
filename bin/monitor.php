2e304e91c8a7347678ed4d20ae93de3c2022a248
Darko <dkrisan@gmail.com>
2014-04-28 14:01:35 +0200

    Updated scripts, some removed and added scripzs to pupulate nzb_guid

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . "/../lib/showsleep.php");
 require_once(dirname(__FILE__) . "/../lib/functions.php");
 
 
-$version = "0.3r1120";
+$version = "0.3r1121";
 
 $db = new DB();
 $functions = new Functions();
