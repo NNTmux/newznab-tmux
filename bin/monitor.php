6d498f1f0394f2891026e4eba3cf2dfc6f599ae7
Darko <dkrisan@gmail.com>
2014-05-06 09:58:24 +0200

    Updated the threaded requestID to use the new server

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . "/../lib/showsleep.php");
 require_once(dirname(__FILE__) . "/../lib/functions.php");
 
 
-$version = "0.3r1139";
+$version = "0.3r1140";
 
 $db = new DB();
 $functions = new Functions();
