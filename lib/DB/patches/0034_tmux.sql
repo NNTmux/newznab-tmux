477318bbe6409f0be379e16f18e76d00e1663033
DariusIII <dkrisan@gmail.com>
2014-05-06 22:44:35 +0200

    Updated panes for IRCScraper.

--- lib/DB/patches/0034_tmux.sql
+++ lib/DB/patches/0034_tmux.sql
@@ -1,9 +1,5 @@
-DELETE FROM `tmux`
-WHERE `setting` = 'scrape_cz';
-DELETE FROM `tmux`
-WHERE `setting` = 'scrape_efnet';
+DELETE FROM `tmux` WHERE `setting` = 'scrape_cz';
+DELETE FROM `tmux` WHERE `setting` = 'scrape_efnet';
 INSERT IGNORE INTO `tmux` (setting, value) VALUE ('scrape', '0');
 
-UPDATE tmux
-SET value = '34'
-WHERE setting = 'sqlpatch';
+UPDATE `tmux` SET value = '34' WHERE `setting` = 'sqlpatch';
