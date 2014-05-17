ff06311d402c02dc3f2f4802a71941370ae8fcbd
DariusIII <dkrisan@gmail.com>
2014-05-17 23:03:25 +0200

    prehash changes, step 1.

--- lib/DB/db.sql
+++ lib/DB/db.sql
@@ -18,7 +18,7 @@ ADD `proc_par2` BIT NOT NULL DEFAULT 0,
 ADD `proc_nfo` BIT NOT NULL DEFAULT 0,
 ADD `proc_files` BIT NOT NULL DEFAULT 0,
 ADD `proc_filenames` BIT NOT NULL DEFAULT 0,
-ADD `nzbstatus` TINYINT(1) NOT NULL DEFAULT 0,
+ADD `nzbstatus` TINYINT(1) NOT NULL DEFAULT 1,
 ADD INDEX `ix_releases_nfostatus` (`nfostatus` ASC) USING HASH,
 ADD INDEX `ix_releases_reqidstatus` (`reqidstatus` ASC) USING HASH,
 ADD INDEX `ix_releases_passwordstatus` (`passwordstatus`),
@@ -542,6 +542,53 @@ ALTER TABLE releasecomment ADD COLUMN nzb_guid VARCHAR(32) NOT NULL DEFAULT '';
 ALTER TABLE `releasefiles` ADD COLUMN `ishashed` TINYINT(1) NOT NULL DEFAULT '0'
 AFTER `size`;
 ALTER TABLE `releasefiles` ADD INDEX `ix_releasefiles_ishashed` (`ishashed`);
+
+DROP TABLE IF EXISTS predbhash;
+CREATE TABLE predbhash (
+  pre_id INT(11) UNSIGNED NOT NULL DEFAULT 0,
+  hashes VARCHAR(512)     NOT NULL DEFAULT '',
+  PRIMARY KEY (pre_id)
+)
+  ENGINE =MYISAM
+  ROW_FORMAT = DYNAMIC
+  DEFAULT CHARSET =utf8mb4
+  COLLATE =utf8mb4_unicode_ci;
+
+INSERT INTO predbhash (pre_id, hashes) (SELECT
+                                          ID,
+                                          CONCAT_WS(',', MD5(title), MD5(MD5(title)), SHA1(title))
+                                        FROM prehash);
+
+CREATE FULLTEXT INDEX ix_predbhash_hashes_ft ON predbhash (hashes);
+ALTER IGNORE TABLE predbhash ADD UNIQUE INDEX ix_predbhash_hashes (hashes(32));
+
+DROP TRIGGER IF EXISTS insert_hashes;
+
+DELIMITER $$
+CREATE TRIGGER insert_hashes AFTER INSERT ON prehash FOR EACH ROW BEGIN INSERT INTO predbhash (pre_id, hashes)
+VALUES (NEW.ID, CONCAT_WS(',', MD5(NEW.title), MD5(MD5(NEW.title)), SHA1(NEW.title)));
+END;
+$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS update_hashes;
+
+DELIMITER $$
+CREATE TRIGGER update_hashes AFTER UPDATE ON prehash FOR EACH ROW BEGIN IF NEW.title != OLD.title
+THEN UPDATE predbhash
+SET hashes = CONCAT_WS(',', MD5(NEW.title), MD5(MD5(NEW.title)), SHA1(NEW.title)); END IF;
+END;
+$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS delete_hashes;
+
+DELIMITER $$
+CREATE TRIGGER delete_hashes AFTER DELETE ON prehash FOR EACH ROW BEGIN DELETE FROM predbhash
+WHERE pre_id = OLD.ID;
+END;
+$$
+DELIMITER ;
 
 DROP TRIGGER IF EXISTS check_rfinsert;
 DROP TRIGGER IF EXISTS check_rfupdate;
