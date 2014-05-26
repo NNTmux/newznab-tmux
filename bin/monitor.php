c29cb8a0b57136945de0b51129fbe087e4ecc37f
Darko <dkrisan@gmail.com>
2014-05-26 12:42:56 +0200

    Updated namecleaner.

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . "/../lib/showsleep.php");
 require_once(dirname(__FILE__) . "/../lib/functions.php");
 
 
-$version = "0.3r1170";
+$version = "0.3r1171";
 
 $db = new DB();
 $functions = new Functions();
