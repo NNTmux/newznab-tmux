744230d696d9b37ed6ddfaff8f67e8b891f7bae9
Darko <dkrisan@gmail.com>
2014-05-27 15:05:09 +0200

    Fixed the patch.

--- lib/DB/patches/0044_releasecomment.sql
+++ lib/DB/patches/0044_releasecomment.sql
@@ -1,4 +1,5 @@
 ALTER TABLE releasecomment MODIFY text varchar(255);
-ALTER IGNORE TABLE releasecomment ADD UNIQUE INDEX ix_releasecomment_text_releaseID (text, releaseID);
+DROP INDEX ix_releasecomment_text_releaseID ON releasecomment;
+CREATE UNIQUE INDEX ix_releasecomment_text_releaseID ON releasecomment (text, releaseID);
 UPDATE `tmux` SET value = '44' WHERE `setting` = 'sqlpatch';
 
