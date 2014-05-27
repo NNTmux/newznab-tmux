c16dac3cb07fb86fafaacdd567b38859f8dd0fa3
Darko <dkrisan@gmail.com>
2014-05-27 11:30:23 +0200

    Fix typo in db.sql

--- lib/DB/db.sql
+++ lib/DB/db.sql
@@ -571,7 +571,7 @@ DELIMITER ;
 DROP TRIGGER IF EXISTS update_hashes;
 
 DELIMITER $$
-CREATE TRIGGER update_hashes AFTER UPDATE ON predhash FOR EACH ROW BEGIN IF NEW.title != OLD.title
+CREATE TRIGGER update_hashes AFTER UPDATE ON prehash FOR EACH ROW BEGIN IF NEW.title != OLD.title
 THEN UPDATE predbhash
 SET hashes = CONCAT_WS(',', MD5(NEW.title), MD5(MD5(NEW.title)), SHA1(NEW.title))
 WHERE pre_id = OLD.ID; END IF;
