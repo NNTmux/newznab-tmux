63d50822cda242039ad93c22b382e9755cd5e0aa
Darko <dkrisan@gmail.com>
2014-06-05 11:18:32 +0200

    Add changes into tmux edit page.

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . "/../lib/showsleep.php");
 require_once(dirname(__FILE__) . "/../lib/functions.php");
 
 
-$version = "0.3r1183";
+$version = "0.3r1184";
 
 $db = new DB();
 $functions = new Functions();
