f44765c3a809b2bd31612f20b75bb94599cc3c9c
Darko <dkrisan@gmail.com>
2014-06-18 09:39:23 +0200

    Removed web scraping from prehash

--- bin/monitor.php
+++ bin/monitor.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__) . "/../lib/showsleep.php");
 require_once(dirname(__FILE__) . "/../lib/functions.php");
 
 
-$version = "0.3r1191";
+$version = "0.3r1192";
 
 $db = new DB();
 $functions = new Functions();
