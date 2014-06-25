19c8bc45c18a3681c64fc16c5a1c2ed111f11a4a
DariusIII <dkrisan@gmail.com>
2014-06-25 22:40:24 +0200

    Test.

--- lib/copy_this/www/lib/Categorize.php
+++ lib/copy_this/www/lib/Categorize.php
@@ -46,7 +46,7 @@ class Categorize extends Category
 		$site = $s->get();
 		$this->categorizeForeign = ($site->categorizeforeign == "0") ? false : true;
 		$this->catWebDL = ($site->catwebdl == "0") ? false : true;
-		parent::__construct();
+		//parent::__construct();
 	}
 
 	/**
