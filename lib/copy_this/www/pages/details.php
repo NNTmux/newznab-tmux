3caa60083fd9ead6cea3a4fce0a6a366b5fb187e
DariusIII <dkrisan@gmail.com>
2014-08-20 01:00:57 +0200

    Updated details page

--- lib/copy_this/www/pages/details.php
+++ lib/copy_this/www/pages/details.php
@@ -7,8 +7,8 @@ require_once(WWW_DIR."/lib/tvrage.php");
 require_once(WWW_DIR."/lib/anidb.php");
 require_once(WWW_DIR."/lib/predb.php");
 require_once(WWW_DIR."/lib/episode.php");
-require_once(WWW_DIR."/../misc/update_scripts/nix_scripts/tmux/lib/prehash.php");
-
+require_once(WWW_DIR . "/lib/XXX.php");
+require_once(WWW_DIR . "/lib/Games.php");
 
 
 if (!$users->isLoggedIn())
@@ -25,14 +25,14 @@ if (isset($_GET["id"]))
 		$page->show404();
 
 	if ($page->isPostBack())
-			$rc->addComment($data["ID"], $data["gid"], $_POST["txtAddComment"], $users->currentUserId(), $_SERVER['REMOTE_ADDR']); 
-	
+		$rc->addComment($data["ID"], $data["gid"], $_POST["txtAddComment"], $users->currentUserId(), $_SERVER['REMOTE_ADDR']);
+
 	$nfo = $releases->getReleaseNfo($data["ID"], false);
 	$reVideo = $re->getVideo($data["ID"]);
 	$reAudio = $re->getAudio($data["ID"]);
 	$reSubs = $re->getSubs($data["ID"]);
 	$comments = $rc->getCommentsByGid($data["gid"]);
-	
+
 	$rage = '';
 	if ($data["rageID"] != '')
 	{
@@ -47,29 +47,29 @@ if (isset($_GET["id"]))
 				$seriesnames[] = $r['releasetitle'];
 				if (!empty($r['description']))
 					$seriesdescription[] = $r['description'];
-				
+
 				if (!empty($r['country']))
 					$seriescountry[] = $r['country'];
-					
+
 				if (!empty($r['genre']))
 					$seriesgenre[] = $r['genre'];
-					
+
 				if (!empty($r['imgdata'])) {
 					$seriesimg[] = $r['imgdata'];
 					$seriesid[] = $r['ID'];
 				}
 			}
 			$rage = array(
-				'releasetitle' => array_shift($seriesnames), 
-				'description' => array_shift($seriesdescription), 
-				'country' => array_shift($seriescountry), 
-				'genre' => array_shift($seriesgenre), 
-				'imgdata' => array_shift($seriesimg), 
+				'releasetitle' => array_shift($seriesnames),
+				'description'  => array_shift($seriesdescription),
+				'country'      => array_shift($seriescountry),
+				'genre'        => array_shift($seriesgenre),
+				'imgdata'      => array_shift($seriesimg),
 				'ID'=>array_shift($seriesid)
 			);
 		}
 	}
-	
+
 	$episodeArray = '';
 	if ($data['episodeinfoID'] > 0)
 	{
@@ -82,34 +82,59 @@ if (isset($_GET["id"]))
 		require_once(WWW_DIR."/lib/movie.php");
 		$movie = new Movie();
 		$mov = $movie->getMovieInfo($data['imdbID']);
-		
+
 		if ($mov) {
 			$mov['actors'] = $movie->makeFieldLinks($mov, 'actors');
 			$mov['genre'] = $movie->makeFieldLinks($mov, 'genre');
 			$mov['director'] = $movie->makeFieldLinks($mov, 'director');
 		}
 	}
-	
+
+	$xxx = '';
+	if ($data['xxxinfo_id'] != '' && $data['xxxinfo_id'] != 0) {
+		$x = new XXX();
+		$xxx = $x->getXXXInfo($data['xxxinfo_id']);
+
+		if (isset($xxx['trailers'])) {
+			$xxx['trailers'] = $x->insertswf($xxx['classused'], $xxx['trailers']);
+		}
+
+		if ($xxx && isset($xxx['title'])) {
+			$xxx['title'] = str_replace(array('/', '\\'), '', $xxx['title']);
+			$xxx['actors'] = $x->makeFieldLinks($xxx, 'actors');
+			$xxx['genre'] = $x->makeFieldLinks($xxx, 'genre');
+			$xxx['director'] = $x->makeFieldLinks($xxx, 'director');
+		} else {
+			$xxx = false;
+		}
+	}
+
+	$game = '';
+	if ($data['gamesinfo_id'] != '') {
+		$g = new Games();
+		$game = $g->getgamesInfo($data['gamesinfo_id']);
+	}
+
 	$mus = '';
 	if ($data['musicinfoID'] != '') {
 		require_once(WWW_DIR."/lib/music.php");
 		$music = new Music();
 		$mus = $music->getMusicInfo($data['musicinfoID']);
-	}	
-	
+	}
+
 	$book = '';
 	if ($data['bookinfoID'] != '') {
 		require_once(WWW_DIR."/lib/book.php");
 		$b = new Book();
 		$book = $b->getBookInfo($data['bookinfoID']);
-	}		
-	
+	}
+
 	$con = '';
 	if ($data['consoleinfoID'] != '') {
 		require_once(WWW_DIR."/lib/console.php");
 		$c = new Console();
 		$con = $c->getConsoleInfo($data['consoleinfoID']);
-	}		
+	}
 
 	$AniDBAPIArray = '';
 	if ($data["anidbID"] > 0)
@@ -125,13 +150,12 @@ if (isset($_GET["id"]))
 		$predbQuery = $PreDB->getByID($data["preID"]);
 	}
 
-
-    $prehash = new PreHash();
+	$prehash = new PreHash();
 	$pre = $prehash->getForRelease($data["prehashID"]);
 
 	$rf = new ReleaseFiles;
 	$releasefiles = $rf->get($data["ID"]);
-	
+
 	$page->smarty->assign('releasefiles',$releasefiles);
 	$page->smarty->assign('release',$data);
 	$page->smarty->assign('reVideo',$reVideo);
@@ -140,20 +164,22 @@ if (isset($_GET["id"]))
 	$page->smarty->assign('nfo',$nfo);
 	$page->smarty->assign('rage',$rage);
 	$page->smarty->assign('movie',$mov);
+	$page->smarty->assign('xxx', $xxx);
 	$page->smarty->assign('episode',$episodeArray);
 	$page->smarty->assign('anidb',$AniDBAPIArray);
 	$page->smarty->assign('music',$mus);
 	$page->smarty->assign('con',$con);
+	$page->smarty->assign('game', $game);
 	$page->smarty->assign('book',$book);
 	$page->smarty->assign('predb',$predbQuery);
-    $page->smarty->assign('prehash',$pre);
+	$page->smarty->assign('prehash', $pre);
 	$page->smarty->assign('comments',$comments);
 	$page->smarty->assign('searchname',$releases->getSimilarName($data['searchname']));
 
 	$page->meta_title = "View NZB";
 	$page->meta_keywords = "view,nzb,description,details";
 	$page->meta_description = "View NZB for".$data["searchname"] ;
-	
+
 	$page->content = $page->smarty->fetch('viewnzb.tpl');
 	$page->render();
 }
