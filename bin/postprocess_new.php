78a7d5e346496c5045dedd623c8fbfd67e207c37
Darko <dkrisan@gmail.com>
2014-09-01 11:30:00 +0200

    It was already there

--- bin/postprocess_new.php
+++ bin/postprocess_new.php
@@ -1,14 +1,14 @@
 <?php
 
-require_once(dirname(__FILE__).'/config.php');
-require_once(WWW_DIR.'/lib/postprocess.php');
-require_once(WWW_DIR.'/lib/nntp.php');
-require_once(WWW_DIR.'/lib/site.php');
-require_once(WWW_DIR.'/lib/anidb.php');
-require_once(WWW_DIR.'/lib/thetvdb.php');
-require_once(dirname(__FILE__).'/../lib/ColorCLI.php');
-require_once(dirname(__FILE__).'/../lib/TvAnger.php');
-require_once(dirname(__FILE__).'/../lib/Pprocess.php');
+require_once(dirname(__FILE__) . '/config.php');
+require_once(WWW_DIR . '/lib/postprocess.php');
+require_once(WWW_DIR . '/lib/nntp.php');
+require_once(WWW_DIR . '/lib/site.php');
+require_once(WWW_DIR . '/lib/anidb.php');
+require_once(WWW_DIR . '/lib/thetvdb.php');
+require_once(dirname(__FILE__) . '/../lib/ColorCLI.php');
+require_once(dirname(__FILE__) . '/../lib/TvAnger.php');
+require_once(dirname(__FILE__) . '/../lib/Pprocess.php');
 require_once(dirname(__FILE__) . '/../lib/Info.php');
 
 $c = new ColorCLI();
@@ -22,31 +22,31 @@ $pprocess = new PProcess(true);
 if (isset($argv[1]) && $argv[1] === "additional") {
 	// Create the connection here and pass, this is for post processing
 	$nntp = new NNTP();
-    if ($nntp->doConnect() === false)
-    {
-	$c = new ColorCLI();
-	echo $c->error("Unable to connect to usenet.\n");
-	return;
-    }
+	if ($nntp->doConnect() === false) {
+		$c = new ColorCLI();
+		echo $c->error("Unable to connect to usenet.\n");
+
+		return;
+	}
 	$pprocess->processAdditional($nntp);
-    $nntp->doQuit();
-} else if (isset($argv[1]) && $argv[1] === "nfo"){
-    // Create the connection here and pass, this is for post processing
+	$nntp->doQuit();
+} else if (isset($argv[1]) && $argv[1] === "nfo") {
+	// Create the connection here and pass, this is for post processing
 	$nntp = new NNTP();
-    if ($nntp->doConnect() === false)
-    {
-	$c = new ColorCLI();
-	echo $c->error("Unable to connect to usenet.\n");
-	return;
-    }
+	if ($nntp->doConnect() === false) {
+		$c = new ColorCLI();
+		echo $c->error("Unable to connect to usenet.\n");
+
+		return;
+	}
 	$postprocess->processNfos();
-    $nntp->doQuit();
-} else if (isset($argv[1]) && $argv[1] === "movie"){
-	    $pprocess->processMovies();
+	$nntp->doQuit();
+} else if (isset($argv[1]) && $argv[1] === "movie") {
+	$pprocess->processMovies();
 	echo '.';
-} else if (isset($argv[1]) && $argv[1] === "tv"){
-			$tvrage = new TvAnger(true);
-			$thetvdb = new TheTVDB(true);
+} else if (isset($argv[1]) && $argv[1] === "tv") {
+	$tvrage = new TvAnger(true);
+	$thetvdb = new TheTVDB(true);
 	$tvrage->processTvReleases(($site->lookuptvrage == 1));
 	$thetvdb->processReleases();
 } else if (isset($argv[1]) && $argv[1] === "games") {
@@ -58,17 +58,15 @@ if (isset($argv[1]) && $argv[1] === "additional") {
 } else if (isset($argv[1]) && $argv[1] === "ebook") {
 	$postprocess->processBooks();
 } else if (isset($argv[1]) && $argv[1] === "music") {
-                $postprocess -> processMusic();
+	$postprocess->processMusic();
 } else if (isset($argv[1]) && $argv[1] === "anime") {
 	$pprocess->processAnime();
 } else if (isset($argv[1]) && $argv[1] === "spotnab") {
-                $postprocess -> processSpotnab();
+	$postprocess->processSpotnab();
 } else if (isset($argv[1]) && $argv[1] === "unwanted") {
-                $postprocess -> processUnwanted();
+	$postprocess->processUnwanted();
 } else if (isset($argv[1]) && $argv[1] === "other") {
-                $postprocess -> processOtherMiscCategory();
+	$postprocess->processOtherMiscCategory();
 } else if (isset($argv[1]) && $argv[1] === "sharing") {
-                $pprocess -> processSharing($nntp);
-} else if (isset($argv[1]) && $argv[1] === "additional") {
-	$pprocess->processAdditional($nntp);
+	$pprocess->processSharing($nntp);
 }
