aec195db982fa90a02885ac3a080e9667a1bf75a
DariusIII <dkrisan@gmail.com>
2014-09-09 19:41:58 +0200

    Forgot this.

--- bin/postprocess_new.php
+++ bin/postprocess_new.php
@@ -2,6 +2,7 @@
 
 require_once(dirname(__FILE__) . '/config.php');
 require_once(WWW_DIR . '/lib/postprocess.php');
+require_once(WWW_DIR . '/lib/framework/Settings.php');
 require_once(WWW_DIR . '/lib/nntp.php');
 require_once(WWW_DIR . '/lib/site.php');
 require_once(WWW_DIR . '/lib/anidb.php');
@@ -11,6 +12,7 @@ require_once(dirname(__FILE__) . '/../lib/TvAnger.php');
 require_once(dirname(__FILE__) . '/../lib/Pprocess.php');
 require_once(dirname(__FILE__) . '/../lib/Info.php');
 
+$pdo = new Settings();
 /**
 Array with possible arguments for run and
 whether or not those methods of operation require NNTP
@@ -65,7 +67,7 @@ if (!isset($argv[1]) || !in_array($argv[1], $args) || !isset($argv[2]) || !in_ar
 
 $nntp = null;
 if ($args[$argv[1]] === true) {
-	$nntp = new \NNTP(['Settings' => $pdo]);
+	$nntp = new NNTP(['Settings' => $pdo]);
 	if (($pdo->getSetting('alternate_nntp') == 1 ? $nntp->doConnect(true, true) : $nntp->doConnect()) !== true) {
 		exit($pdo->log->error("Unable to connect to usenet." . PHP_EOL));
 	}
