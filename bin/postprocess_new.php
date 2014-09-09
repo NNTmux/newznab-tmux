0ef8d6b01ba1c86912d959384ba2d28c1bb1a4ea
Darko <dkrisan@gmail.com>
2014-09-09 16:00:56 +0200

    Updated postprocess_new.

--- bin/postprocess_new.php
+++ bin/postprocess_new.php
@@ -11,62 +11,125 @@ require_once(dirname(__FILE__) . '/../lib/TvAnger.php');
 require_once(dirname(__FILE__) . '/../lib/Pprocess.php');
 require_once(dirname(__FILE__) . '/../lib/Info.php');
 
-$c = new ColorCLI();
-$s = new Sites();
-$site = $s->get();
-if (!isset($argv[1])) {
-	exit($c->error("You need to set an argument [additional, nfo, movie, tv, games, xxx, ebook, music, anime, unwanted, others, spotnab, sharing]."));
+/**
+Array with possible arguments for run and
+whether or not those methods of operation require NNTP
+ **/
+
+$args = array(
+	'additional' => true,
+	'all'        => true,
+	'allinf'     => true,
+	'amazon'     => false,
+	'anime'      => false,
+	'book'       => false,
+	'console'    => false,
+	'games'      => false,
+	'movies'     => false,
+	'music'      => false,
+	'nfo'        => true,
+	'pre'        => true,
+	'sharing'    => true,
+	'tv'         => false,
+	'xxx'        => false,
+);
+
+$bool = array(
+	'true',
+	'false'
+);
+
+if (!isset($argv[1]) || !in_array($argv[1], $args) || !isset($argv[2]) || !in_array($argv[2], $bool)) {
+	exit(
+	$pdo->log->error(
+		"\nIncorrect arguments.\n"
+		. "The second argument (true/false) determines wether to echo or not.\n\n"
+		. "php postprocess_new.php all true         ...: Does all the types of post processing.\n"
+		. "php postprocess_new.php pre true         ...: Processes all Predb sites.\n"
+		. "php postprocess_new.php nfo true         ...: Processes NFO files.\n"
+		. "php postprocess_new.php movies true      ...: Processes movies.\n"
+		. "php postprocess_new.php music true       ...: Processes music.\n"
+		. "php postprocess_new.php console true     ...: Processes console games.\n"
+		. "php postprocess_new.php games true       ...: Processes games.\n"
+		. "php postprocess_new.php book true        ...: Processes books.\n"
+		. "php postprocess_new.php anime true       ...: Processes anime.\n"
+		. "php postprocess_new.php tv true          ...: Processes tv.\n"
+		. "php postprocess_new.php xxx true         ...: Processes xxx.\n"
+		. "php postprocess_new.php additional true  ...: Processes previews/mediainfo/etc...\n"
+		. "php postprocess_new.php sharing true     ...: Processes uploading/downloading comments.\n"
+		. "php postprocess_new.php allinf true      ...: Does all the types of post processing on a loop, sleeping 15 seconds between.\n"
+		. "php postprocess_new.php amazon true      ...: Does all the amazon (books/console/games/music/xxx).\n"
+	)
+	);
 }
-$postprocess = new PostProcess(true);
-$pprocess = new PProcess(true);
-if (isset($argv[1]) && $argv[1] === "additional") {
-	// Create the connection here and pass, this is for post processing
-	$nntp = new NNTP();
-	if ($nntp->doConnect() === false) {
-		$c = new ColorCLI();
-		echo $c->error("Unable to connect to usenet.\n");
 
-		return;
+$nntp = null;
+if ($args[$argv[1]] === true) {
+	$nntp = new \NNTP(['Settings' => $pdo]);
+	if (($pdo->getSetting('alternate_nntp') == 1 ? $nntp->doConnect(true, true) : $nntp->doConnect()) !== true) {
+		exit($pdo->log->error("Unable to connect to usenet." . PHP_EOL));
 	}
-	$pprocess->processAdditional($nntp);
-	$nntp->doQuit();
-} else if (isset($argv[1]) && $argv[1] === "nfo") {
-	// Create the connection here and pass, this is for post processing
-	$nntp = new NNTP();
-	if ($nntp->doConnect() === false) {
-		$c = new ColorCLI();
-		echo $c->error("Unable to connect to usenet.\n");
+}
 
-		return;
-	}
-	$postprocess->processNfos();
-	$nntp->doQuit();
-} else if (isset($argv[1]) && $argv[1] === "movie") {
-	$pprocess->processMovies();
-	echo '.';
-} else if (isset($argv[1]) && $argv[1] === "tv") {
-	$tvrage = new TvAnger(true);
-	$thetvdb = new TheTVDB(true);
-	$tvrage->processTvReleases(($site->lookuptvrage == 1));
-	$thetvdb->processReleases();
-} else if (isset($argv[1]) && $argv[1] === "games") {
-	$pprocess->processGames();
-} else if (isset($argv[1]) && $argv[1] === "xxx") {
-	$pprocess->processXXX();
-} else if (isset($argv[1]) && $argv[1] === "console") {
-	$pprocess->processConsoleGames();
-} else if (isset($argv[1]) && $argv[1] === "ebook") {
-	$postprocess->processBooks();
-} else if (isset($argv[1]) && $argv[1] === "music") {
-	$postprocess->processMusic();
-} else if (isset($argv[1]) && $argv[1] === "anime") {
-	$pprocess->processAnime();
-} else if (isset($argv[1]) && $argv[1] === "spotnab") {
-	$postprocess->processSpotnab();
-} else if (isset($argv[1]) && $argv[1] === "unwanted") {
-	$postprocess->processUnwanted();
-} else if (isset($argv[1]) && $argv[1] === "other") {
-	$postprocess->processOtherMiscCategory();
-} else if (isset($argv[1]) && $argv[1] === "sharing") {
-	$pprocess->processSharing($nntp);
+$postProcess = new PProcess(['Settings' => $pdo, 'Echo' => ($argv[2] === 'true' ? true : false)]);
+
+$charArray = ['a','b','c','d','e','f','0','1','2','3','4','5','6','7','8','9'];
+
+switch ($argv[1]) {
+
+	case 'all':
+		$postProcess->processAll($nntp);
+		break;
+	case 'allinf':
+		$i = 1;
+		while ($i = 1) {
+			$postProcess->processAll($nntp);
+			sleep(15);
+		}
+		break;
+	case 'additional':
+		$postProcess->processAdditional($nntp, '', (isset($argv[3]) && in_array($argv[3], $charArray) ? $argv[3] : ''));
+		break;
+	case 'amazon':
+		$postProcess->processBooks();
+		$postProcess->processConsoles();
+		$postProcess->processGames();
+		$postProcess->processMusic();
+		$postProcess->processXXX();
+		break;
+	case 'anime':
+		exit;
+	//$postProcess->processAnime();
+	//break;
+	case 'book':
+		$postProcess->processBooks();
+		break;
+	case 'console':
+		$postProcess->processConsoles();
+		break;
+	case 'games':
+		$postProcess->processGames();
+		break;
+	case 'nfo':
+		$postProcess->processNfos($nntp, '', (isset($argv[3]) && in_array($argv[3], $charArray) ? $argv[3] : ''));
+		break;
+	case 'movies':
+		$postProcess->processMovies('', (isset($argv[3]) && in_array($argv[3], $charArray) ? $argv[3] : ''));
+		break;
+	case 'music':
+		$postProcess->processMusic();
+		break;
+	case 'pre':
+		break;
+	case 'sharing':
+		$postProcess->processSharing($nntp);
+		break;
+	case 'tv':
+		$postProcess->processTV('', (isset($argv[3]) && in_array($argv[3], $charArray) ? $argv[3] : ''));
+		break;
+	case 'xxx':
+		$postProcess->processXXX();
+		break;
+	default:
+		exit;
 }
