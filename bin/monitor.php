9be64d48f44e07a7c9785a9043e58ca9e90b0754
Darko <dkrisan@gmail.com>
2014-09-09 12:24:15 +0200

    Change in monitor.Again.

--- bin/monitor.php
+++ bin/monitor.php
@@ -1,6 +1,6 @@
 <?php
 require_once(dirname(__FILE__) . "/config.php");
-require_once(WWW_DIR . "lib/framework/db.php");
+require_once(WWW_DIR . "lib/framework/Settings.php");
 require_once(WWW_DIR . '/lib/site.php');
 require_once(WWW_DIR . '/lib/Tmux.php');
 require_once(WWW_DIR . "/lib/ColorCLI.php");
