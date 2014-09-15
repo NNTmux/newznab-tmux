9a79bc8d4b3ece8f5b6ee6ba9c0796a191f285f0
Darko <dkrisan@gmail.com>
2014-09-15 15:40:01 +0200

    Test site.php

--- lib/copy_this/www/lib/site.php
+++ lib/copy_this/www/lib/site.php
@@ -8,93 +8,155 @@ class Sites
 	const REGISTER_STATUS_INVITE = 1;
 	const REGISTER_STATUS_CLOSED = 2;
 	const REGISTER_STATUS_API_ONLY = 3;
-
 	const ERR_BADUNRARPATH = -1;
 	const ERR_BADFFMPEGPATH = -2;
 	const ERR_BADMEDIAINFOPATH = -3;
 	const ERR_BADNZBPATH = -4;
 	const ERR_DEEPNOUNRAR = -5;
 	const ERR_BADTMPUNRARPATH = -6;
-	const ERR_BADLAMEPATH = -7;
-	const ERR_SABCOMPLETEPATH = -8;
+	const ERR_BADNZBPATH_UNREADABLE = -7;
+	const ERR_BADNZBPATH_UNSET = -8;
+	const ERR_BAD_COVERS_PATH = -9;
+	const ERR_BAD_YYDECODER_PATH = -10;
+
+	/**
+	 * @var Settings
+	 */
+	protected $_db;
+
+	/**
+	 * @param array $options Class instances.
+	 */
+	public function __construct(array $options = array())
+	{
+		$defaults = [
+			'Settings' => null,
+		];
+		$options += $defaults;
+
+		$this->_db = ($options['Settings'] instanceof Settings ? $options['Settings'] : new Settings());
+
+	}
 
 	public function version()
 	{
-		return "0.2.3p";
+		return ($this->getSetting('dbversion'));
 	}
 
 	public function update($form)
 	{
-		$db = new Settings();
+		$pdo = $this->_db;
 		$site = $this->row2Object($form);
 
-		if (substr($site->nzbpath, strlen($site->nzbpath) - 1) != '/')
+		if (substr($site->nzbpath, strlen($site->nzbpath) - 1) != '/') {
 			$site->nzbpath = $site->nzbpath . "/";
+		}
 
 		//
 		// Validate site settings
 		//
-		if ($site->mediainfopath != "" && !is_file($site->mediainfopath))
-			return Sites::ERR_BADMEDIAINFOPATH;
+		if ($site->mediainfopath != "" && !is_file($site->mediainfopath)) {
+			return \Sites::ERR_BADMEDIAINFOPATH;
+		}
 
-		if ($site->ffmpegpath != "" && !is_file($site->ffmpegpath))
-			return Sites::ERR_BADFFMPEGPATH;
+		if ($site->ffmpegpath != "" && !is_file($site->ffmpegpath)) {
+			return \Sites::ERR_BADFFMPEGPATH;
+		}
 
-		if ($site->unrarpath != "" && !is_file($site->unrarpath))
-			return Sites::ERR_BADUNRARPATH;
+		if ($site->unrarpath != "" && !is_file($site->unrarpath)) {
+			return \Sites::ERR_BADUNRARPATH;
+		}
 
-		if ($site->nzbpath != "" && !file_exists($site->nzbpath))
-			return Sites::ERR_BADNZBPATH;
+		if (empty($site->nzbpath)) {
+			return \Sites::ERR_BADNZBPATH_UNSET;
+		}
 
-		if ($site->checkpasswordedrar == 2 && !is_file($site->unrarpath))
-			return Sites::ERR_DEEPNOUNRAR;
+		if (!file_exists($site->nzbpath) || !is_dir($site->nzbpath)) {
+			return \Sites::ERR_BADNZBPATH;
+		}
 
-		if ($site->tmpunrarpath != "" && !file_exists($site->tmpunrarpath))
-			return Sites::ERR_BADTMPUNRARPATH;
+		if (!is_readable($site->nzbpath)) {
+			return \Sites::ERR_BADNZBPATH_UNREADABLE;
+		}
 
-		if ($site->lamepath != "" && !file_exists($site->lamepath))
-			return Sites::ERR_BADLAMEPATH;
+		if ($site->checkpasswordedrar == 1 && !is_file($site->unrarpath)) {
+			return \Sites::ERR_DEEPNOUNRAR;
+		}
 
-		if ($site->sabcompletedir != "" && !file_exists($site->sabcompletedir))
-			return Sites::ERR_SABCOMPLETEPATH;
+		if ($site->tmpunrarpath != "" && !file_exists($site->tmpunrarpath)) {
+			return \Sites::ERR_BADTMPUNRARPATH;
+		}
+
+		if ($site->yydecoderpath != "" && $site->yydecoderpath !== 'simple_php_yenc_decode' && !file_exists($site->yydecoderpath)) {
+			return \Sites::ERR_BAD_YYDECODER_PATH;
+		}
 
 		$sql = $sqlKeys = array();
 		foreach ($form as $settingK => $settingV) {
-			$sql[] = sprintf("WHEN %s THEN %s", $db->escapeString($settingK), $db->escapeString(trim($settingV)));
-			$sqlKeys[] = $db->escapeString($settingK);
+			$sql[] = sprintf("WHEN %s THEN %s", $pdo->escapeString($settingK), $pdo->escapeString($settingV));
+			$sqlKeys[] = $pdo->escapeString($settingK);
 		}
 
-		$db->queryExec(sprintf("update site SET value = CASE setting %s END WHERE setting IN (%s)", implode(' ', $sql), implode(', ', $sqlKeys)));
+		$table = $pdo->table();
+		$pdo->queryExec(
+			sprintf("UPDATE $table SET value = CASE setting %s END WHERE setting IN (%s)",
+				implode(' ', $sql),
+				implode(', ', $sqlKeys)
+			)
+		);
 
-		return $this->get(true);
+		return $site;
 	}
 
-	public function get($refresh = false)
+	public function get()
 	{
-		$sql = "select * from site";
+		$pdo = $this->_db;
+		$table = $pdo->table();
+		$rows = $pdo->query("SELECT setting, value FROM $table WHERE setting != ''");
 
-		if ($refresh) {
-			$cache = new Cache();
-			$cache->delete($sql);
+		if ($rows === false) {
+			return false;
 		}
 
-		$db = new Settings();
-		$rows = $db->query($sql, true, NN_CACHE_EXPIRY_MEDIUM);
-
-		if ($rows === false)
-			return false;
-
 		return $this->rows2Object($rows);
+	}
+
+	/**
+	 * Retrieve one or all settings from the Db as a string or an array;
+	 *
+	 * @param null $setting	Name of setting to retrieve, or null for all settings
+	 *
+	 * @return string|array|bool
+	 */
+	function getSetting($setting = null)
+	{
+		$results = array();
+		$table = $this->_db->table();
+		$sql = "SELECT setting, value FROM $table ";
+		if ($setting !== null) {
+			$sql .= "WHERE setting = '$setting' ";
+		}
+		$sql .= 'ORDER BY setting';
+
+		$result = $this->_db->queryArray($sql);
+		if ($result instanceof \Traversable) {
+			foreach($result as $row) {
+				$results[$row['setting']] = $row['value'];
+			}
+
+		}
+
+		return (count($results) === 1 ? $results[$setting] : $results);
 	}
 
 	public function rows2Object($rows)
 	{
 		$obj = new stdClass;
-		foreach ($rows as $row)
-			$obj->{$row['setting']} = $row['value'];
+		foreach ($rows as $row) {
+			$obj->{$row['setting']} = trim($row['value']);
+		}
 
 		$obj->{'version'} = $this->version();
-
 		return $obj;
 	}
 
@@ -102,54 +164,30 @@ class Sites
 	{
 		$obj = new stdClass;
 		$rowKeys = array_keys($row);
-		foreach ($rowKeys as $key)
-			$obj->{$key} = $row[$key];
-
-		return $obj;
-	}
-
-	public function getUnappliedPatches($site)
-	{
-		preg_match("/\d+/", $site->dbversion, $matches);
-		$currentrev = $matches[0];
-
-		$patchpath = WWW_DIR . "../db/patch/0.2.3/";
-		$patchfiles = glob($patchpath . "*.sql");
-		$missingpatch = array();
-		foreach ($patchfiles as $file) {
-			$filecontents = file_get_contents($file);
-			if (preg_match("/Rev\: (\d+)/", $filecontents, $matches)) {
-				$patchrev = $matches[1];
-				if ($patchrev > $currentrev)
-					$missingpatch[] = $file;
-			}
+		foreach ($rowKeys as $key) {
+			$obj->{$key} = trim($row[$key]);
 		}
 
-		return $missingpatch;
-	}
-
-	public function updateItem($setting, $value)
-	{
-		$db = new Settings();
-		$sql = sprintf("update site set value = %s where setting = %s", $db->escapeString($value), $db->escapeString($setting));
-
-		return $db->queryExec($sql);
-	}
-
-	public function updateLatestRegexRevision($rev)
-	{
-		return $this->updateItem("latestregexrevision", $rev);
+		return $obj;
 	}
 
 	public function getLicense($html = false)
 	{
 		$n = "\r\n";
-		if ($html)
+		if ($html) {
 			$n = "<br/>";
+		}
 
-		return $n . "newznab " . $this->version() . " Copyright (C) " . date("Y") . " newznab.com" . $n . "
+		return $n . "newznab " . $this->version() . $n . "
 
-This program is distributed with a commercial licence. See LICENCE.txt for
-further details." . $n;
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation." . $n . "
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+" . $n;
 	}
 }
