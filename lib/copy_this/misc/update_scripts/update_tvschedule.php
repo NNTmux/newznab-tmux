a8e3132c039b1c6818477cd305bb24f9655bef5d
Darko <dkrisan@gmail.com>
2014-09-22 13:51:54 +0200

    Wrong edit.

--- lib/copy_this/misc/update_scripts/update_tvschedule.php
+++ lib/copy_this/misc/update_scripts/update_tvschedule.php
@@ -4,5 +4,4 @@
 require("config.php");
 require_once(WWW_DIR.'/../misc/update_scripts/nix_scripts/tmux/lib/TvAnger.php');
 
-$m = new \TvAnger(['Echo' => true]);
-$m->updateUpcoming();
+(new \TvAnger(['Echo' => true]))->updateSchedule();
