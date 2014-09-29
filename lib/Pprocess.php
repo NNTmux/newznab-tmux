38c2a0fac3399efbdfc1f539d39f826bdaeec123
DariusIII <dkrisan@gmail.com>
2014-09-29 20:19:32 +0200

    Again the same error.

--- lib/Pprocess.php
+++ lib/Pprocess.php
@@ -185,7 +185,7 @@ class PProcess
 	public function processConsoles()
 	{
 		if ($this->site->lookupgames != 0) {
-			(new \Console(['Settings' => $this->pdo, 'Echo' => $this->echooutput]))->processConsoleReleases();
+			(new \Konsole(['Settings' => $this->pdo, 'Echo' => $this->echooutput]))->processConsoleReleases();
 		}
 	}
 
