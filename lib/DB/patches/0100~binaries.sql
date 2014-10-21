b41c0b106f4be20ed1f6330d066a0706993bcef5
Darko <dkrisan@gmail.com>
2014-10-21 14:50:09 +0200

    And again.

--- lib/DB/patches/0100~binaries.sql
+++ lib/DB/patches/0100~binaries.sql
@@ -1,8 +1,7 @@
 DROP TRIGGER IF EXISTS delete_binaries;
 
-CREATE TRIGGER delete_binaries BEFORE DELETE ON binaries FOR EACH ROW
-  BEGIN
-    DELETE FROM parts WHERE binaryID = OLD.ID;
-  END;
+DELIMITER $$
+CREATE TRIGGER delete_binaries BEFORE DELETE ON binaries FOR EACH ROW BEGIN DELETE FROM parts WHERE binaryID = OLD.ID; END; $$
+DELIMITER ;
 
 UPDATE tmux set value = '100' where setting = 'sqlpatch';
