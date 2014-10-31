7994aa3c3aff53af705ae249bc4b3805047e890c
Darko <dkrisan@gmail.com>
2014-10-31 15:21:37 +0100

    Update readme file, updated mysql_dump_tables script.

--- README.md
+++ README.md
@@ -11,11 +11,13 @@ Now, clone my github. These scripts need to run from this location and this is w
 
 There is a lib folder in main tmux folder. In that folder you will find two folders, /DB/ and /copy_this/. if you are setting up tmux for the first time, import db.sql using cli (mysql -u {┤your username} -p newznab < db.sql).
 
-1. Copy files from /lib/copy_this/ folder into your newznab installation /www folder. I have created the nntmux theme where all the settings are available.
+1. Copy files from /lib/copy_this/www folder into your newznab installation folder. I have created the nntmux theme where all the settings are available.
 
-2. Most of the scripts are now threaded, they need python installed. Install instructions are in threaded_scripts_readme.txt
+2. Copy files from /lib/copy_this/misc folder into your newznab installation folder.
 
-3. You need to create sample folder in you covers folder (aka. /www/covers/sample) so you can have samples taken from releases, mostly XXX, and can be viewed in releases. For video previews you need video folder /www/covers/video/.
+3. Most of the scripts are now threaded.
+
+4. All the required folders are created when you copy files from copy_this folder.
 
 # yEnc:
 
@@ -51,6 +53,25 @@ There is a lib folder in main tmux folder. In that folder you will find two fold
        rm -rf yydecode/
 
   Note: After installing you can change the yEnc setting in tmux edit accordingly.
+
+  #Threaded Scripts
+
+  Tmux comes with php threaded scripts, python scripts are still there, but are not used, and will be removed in future
+
+  #Table per group
+
+  Tmux can now be used with TPG ported from nZEDb, you need to run lib/testing/DB/convert_to_tpg.php to convert binaries, parts and partrepair tables, after this you will be able to use
+  threaded releases.php
+
+  #Alternate NNTP provider
+
+  Newznab-tmux has added support for dual NNTP providers, you need to edit the provided config.php in lib/copy_this/config_edit_before_copy folder
+
+  #Sphinx support
+
+  Newznab-tmux now comes with its own Sphinx support, ported from nZEDb. Installation readme is in misc/sphinxsearch folder, and in copy_this/misc/sphinxsearch folder
+
+  This version of tmux has many core newznab files modified, use at your own risk.
 
 Tmux is started with start.php (php start.php command). Scripts not used anymore are still available for your use, but you need to run them manualy.
 
--- lib/DB/mysqldump_tables.php
+++ lib/DB/mysqldump_tables.php
@@ -1,30 +1,25 @@
 <?php
 require_once(dirname(__FILE__)."/../../bin/config.php");
+require_once(WWW_DIR . "/lib/framework/db.php");
 require_once(WWW_DIR . "/lib/ColorCLI.php");
 
 
-//	This script can dump all tables or just collections/binaries/parts/partrepair/groups.
+//	This script can dump all tables or just binaries/parts/partrepair/groups.
 
-$db = new DB();
-$c = new ColorCLI();
-if ($db->dbSystem() == "pgsql") {
-	exit($c->error("\nThis script is only for mysql.\n"));
-}
+$pdo = new DB();
 
 $exportopts = "";
-$mysqlplatform = "";
 
 //determine mysql platform Percona or Other
-if($db->dbSystem() == "mysql") {
-	$mysqlplatform = exec('mysqladmin version | grep "Percona"', $mysqlplatform);
-	if (count($mysqlplatform) > 0) {
-		//Percona only has --innodb-optimize-keys
-		$exportopts = "--opt --innodb-optimize-keys --complete-insert --skip-quick";
-	} else {
-		//generic (or unknown) instance of MySQL
-		$exportopts = "--opt --complete-insert --skip-quick";
-	}
+$mysqlplatform = exec('mysqladmin version | grep "Percona"');
+if (strlen($mysqlplatform) > 0) {
+	//Percona only has --innodb-optimize-keys
+	$exportopts = "--opt --innodb-optimize-keys --complete-insert --skip-quick";
+} else {
+	//generic (or unknown) instance of MySQL
+	$exportopts = "--opt --complete-insert --skip-quick";
 }
+
 
 function newname($filename)
 {
@@ -35,15 +30,15 @@ function builddefaultsfile()
 {
 	//generate file contents
 	$filetext = "[mysqldump]"
-				."\n"
-				."user = " . DB_USER
-				."\n"
-				."password = " . DB_PASSWORD
-				."\n[mysql]"
-				."\n"
-				."user = " . DB_USER
-				."\n"
-				."password = " . DB_PASSWORD;
+		."\n"
+		."user = " . DB_USER
+		."\n"
+		."password = " . DB_PASSWORD
+		."\n[mysql]"
+		."\n"
+		."user = " . DB_USER
+		."\n"
+		."password = " . DB_PASSWORD;
 
 	$filehandle = fopen("mysql-defaults.txt", "w+");
 	if(!$filehandle) {
@@ -68,14 +63,12 @@ if (DB_SOCKET != '') {
 	$use = "-P$dbport";
 }
 
-if($db->dbSystem() == "mysql") {
-	//generate defaults file used to store database login information so it is not in cleartext in ps command for mysqldump
-	builddefaultsfile();
-}
+//generate defaults file used to store database login information so it is not in cleartext in ps command for mysqldump
+builddefaultsfile();
 
 if((isset($argv[1]) && $argv[1] == "db") && (isset($argv[2]) && $argv[2] == "dump") && (isset($argv[3]) && file_exists($argv[3]))) {
 	$filename = $argv[3]."/".$dbname.".gz";
-	echo $c->header("Dumping $dbname.");
+	echo $pdo->log->header("Dumping $dbname.");
 	if (file_exists($filename)) {
 		newname($filename);
 	}
@@ -84,19 +77,19 @@ if((isset($argv[1]) && $argv[1] == "db") && (isset($argv[2]) && $argv[2] == "dum
 } else if((isset($argv[1]) && $argv[1] == "db") && (isset($argv[2]) && $argv[2] == "restore") && (isset($argv[3]) && file_exists($argv[3]))) {
 	$filename = $argv[3]."/".$dbname.".gz";
 	if (file_exists($filename)) {
-		echo $c->header("Restoring $dbname.");
+		echo $pdo->log->header("Restoring $dbname.");
 		$command = "zcat < $filename | mysql --defaults-file=mysql-defaults.txt -h$dbhost $use $dbname";
-		$db->queryExec("SET FOREIGN_KEY_CHECKS=0");
+		$pdo->queryExec("SET FOREIGN_KEY_CHECKS=0");
 		system($command);
-		$db->queryExec("SET FOREIGN_KEY_CHECKS=1");
+		$pdo->queryExec("SET FOREIGN_KEY_CHECKS=1");
 	}
 } else if((isset($argv[1]) && $argv[1] == "all") && (isset($argv[2]) && $argv[2] == "dump") && (isset($argv[3]) && file_exists($argv[3]))) {
 	$sql = "SHOW tables";
-	$tables = $db->query($sql);
+	$tables = $pdo->query($sql);
 	foreach($tables as $row) {
 		$tbl = $row['tables_in_'.DB_NAME];
 		$filename = $argv[3]."/".$tbl.".gz";
-		echo $c->header("Dumping $tbl.");
+		echo $pdo->log->header("Dumping $tbl.");
 		if (file_exists($filename)) {
 			newname($filename);
 		}
@@ -105,23 +98,23 @@ if((isset($argv[1]) && $argv[1] == "db") && (isset($argv[2]) && $argv[2] == "dum
 	}
 } else if((isset($argv[1]) && $argv[1] == "all") && (isset($argv[2]) && $argv[2] == "restore") && (isset($argv[3]) && file_exists($argv[3]))) {
 	$sql = "SHOW tables";
-	$tables = $db->query($sql);
-	$db->queryExec("SET FOREIGN_KEY_CHECKS=0");
+	$tables = $pdo->query($sql);
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=0");
 	foreach($tables as $row) {
 		$tbl = $row['tables_in_'.DB_NAME];
 		$filename = $argv[3]."/".$tbl.".gz";
 		if (file_exists($filename)) {
-			echo $c->header("Restoring $tbl.");
+			echo $pdo->log->header("Restoring $tbl.");
 			$command = "zcat < $filename | mysql --defaults-file=mysql-defaults.txt -h$dbhost $use $dbname";
 			system($command);
 		}
 	}
-	$db->queryExec("SET FOREIGN_KEY_CHECKS=1");
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=1");
 } else if((isset($argv[1]) && $argv[1] == "test") && (isset($argv[2]) && $argv[2] == "dump") && (isset($argv[3]) && file_exists($argv[3]))) {
-	$arr = array("parts", "binaries", "collections", "partrepair", "groups");
+	$arr = array("parts", "binaries", "partrepair", "groups");
 	foreach ($arr as &$tbl) {
 		$filename = $argv[3]."/".$tbl.".gz";
-		echo $c->header("Dumping $tbl..");
+		echo $pdo->log->header("Dumping $tbl..");
 		if (file_exists($filename)) {
 			newname($filename);
 		}
@@ -129,58 +122,58 @@ if((isset($argv[1]) && $argv[1] == "db") && (isset($argv[2]) && $argv[2] == "dum
 		system($command);
 	}
 } else if((isset($argv[1]) && $argv[1] == "test") && (isset($argv[2]) && $argv[2] == "restore") && (isset($argv[3]) && file_exists($argv[3]))) {
-	$arr = array("parts", "binaries", "collections", "partrepair", "groups");
-	$db->execc("SET FOREIGN_KEY_CHECKS=0");
+	$arr = array("parts", "binaries", "partrepair", "groups");
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=0");
 	foreach ($arr as &$tbl) {
 		$filename = $argv[3]."/".$tbl.".gz";
 		if (file_exists($filename)) {
-			echo $c->header("Restoring $tbl.");
+			echo $pdo->log->header("Restoring $tbl.");
 			$command = "zcat < $filename | mysql --defaults-file=mysql-defaults.txt -h$dbhost $use $dbname";
 			system($command);
 		}
 	}
-	$db->queryExec("SET FOREIGN_KEY_CHECKS=1");
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=1");
 } else if((isset($argv[1]) && $argv[1] == "all") && (isset($argv[2]) && $argv[2] == "outfile") && (isset($argv[3]) && file_exists($argv[3]))) {
 	$sql = "SHOW tables";
-	$tables = $db->query($sql);
+	$tables = $pdo->query($sql);
 	foreach($tables as $row) {
 		$tbl = $row['tables_in_'.DB_NAME];
 		$filename = $argv[3].$tbl.".csv";
-		echo $c->header("Dumping $tbl.");
+		echo $pdo->log->header("Dumping $tbl.");
 		if (file_exists($filename)) {
 			newname($filename);
 		}
-		$db->queryDirect(sprintf("SELECT * INTO OUTFILE %s FROM %s", $db->escapeString($filename), $tbl));
+		$pdo->queryDirect(sprintf("SELECT * INTO OUTFILE %s FROM %s", $pdo->escapeString($filename), $tbl));
 	}
 } else if((isset($argv[1]) && $argv[1] == "all") && (isset($argv[2]) && $argv[2] == "infile") && (isset($argv[3]) && is_dir($argv[3]))) {
 	$sql = "SHOW tables";
-	$tables = $db->query($sql);
-	$db->queryExec("SET FOREIGN_KEY_CHECKS=0");
+	$tables = $pdo->query($sql);
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=0");
 	foreach($tables as $row) {
 		$tbl = $row['tables_in_'.DB_NAME];
 		$filename = $argv[3].$tbl.".csv";
 		if (file_exists($filename)) {
-			echo $c->header("Restoring $tbl.");
-			$db->queryExec(sprintf("LOAD DATA INFILE %s INTO TABLE %s", $db->escapeString($filename), $tbl));
+			echo $pdo->log->header("Restoring $tbl.");
+			$pdo->queryExec(sprintf("LOAD DATA INFILE %s INTO TABLE %s", $pdo->escapeString($filename), $tbl));
 		}
 	}
-	$db->queryExec("SET FOREIGN_KEY_CHECKS=1");
+	$pdo->queryExec("SET FOREIGN_KEY_CHECKS=1");
 } else {
 	passthru("clear");
-	echo $c->error("\nThis script can dump/restore all tables, compressed or OUTFILE/INFILE, or just collections/binaries/parts.\n\n"
-	. "**Single File\n"
-	. "php $argv[0] db dump /path/to/save/to              ...: To dump the database.\n"
-	. "php $argv[0] db restore /path/to/restore/from      ...: To restore the database.\n\n"
-	. "**Individual Table Files\n"
-	. "php $argv[0] all dump /path/to/save/to             ...: To dump all tables.\n"
-	. "php $argv[0] all restore /path/to/restore/from     ...: To restore all tables.\n\n"
-	. "**Three Tables (collections, binaries, parts)\n"
-	. "php $argv[0] test dump /path/to/save/to            ...: To dump collections, binaries, parts tables.\n"
-	. "php $argv[0] test restore /path/to/restore/from    ...: To restore collections, binaries, parts tables.\n\n"
-	. "**Individal Files - OUTFILE/INFILE - No schema\n"
-	. "**MySQL MUST have write permissions to this path\n"
-	. "php $argv[0] all outfile /path/to/save/to          ...: To dump all tables, using OUTFILE.\n"
-	. "php $argv[0] all infile /path/to/restore/from      ...: To restore all tables, using INFILE.\n\n");
+	echo $pdo->log->error("\nThis script can dump/restore all tables, compressed or OUTFILE/INFILE, or just collections/binaries/parts.\n\n"
+		. "**Single File\n"
+		. "php $argv[0] db dump /path/to/save/to              ...: To dump the database.\n"
+		. "php $argv[0] db restore /path/to/restore/from      ...: To restore the database.\n\n"
+		. "**Individual Table Files\n"
+		. "php $argv[0] all dump /path/to/save/to             ...: To dump all tables.\n"
+		. "php $argv[0] all restore /path/to/restore/from     ...: To restore all tables.\n\n"
+		. "**Two Tables (binaries, parts)\n"
+		. "php $argv[0] test dump /path/to/save/to            ...: To dump binaries, parts tables.\n"
+		. "php $argv[0] test restore /path/to/restore/from    ...: To restore binaries, parts tables.\n\n"
+		. "**Individal Files - OUTFILE/INFILE - No schema\n"
+		. "**MySQL MUST have write permissions to this path\n"
+		. "php $argv[0] all outfile /path/to/save/to          ...: To dump all tables, using OUTFILE.\n"
+		. "php $argv[0] all infile /path/to/restore/from      ...: To restore all tables, using INFILE.\n\n");
 }
 
 if(file_exists("mysql-defaults.txt")) {
