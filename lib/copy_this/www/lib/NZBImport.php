66a1a6f7155e485af755b910538e44aa54a537f3
DariusIII <dkrisan@gmail.com>
2014-11-28 19:56:16 +0100

    Fix for undefined indexes on import.

--- lib/copy_this/www/lib/NZBImport.php
+++ lib/copy_this/www/lib/NZBImport.php
@@ -110,6 +110,8 @@ class NZBImport
 		];
 		$options += $defaults;
 
+		$s = new Sites();
+		$this->site = $s->get();
 		$this->echoCLI = (!$this->browser && NN_ECHOCLI && $options['Echo']);
 		$this->pdo = ($options['Settings'] instanceof DB ? $options['Settings'] : new DB());
 		$this->binaries = ($options['Binaries'] instanceof \Binaries ? $options['Binaries'] : new \Binaries(['Settings' => $this->pdo, 'Echo' => $this->echoCLI]));
@@ -403,8 +405,11 @@ class NZBImport
 					'totalpart' => $nzbDetails['totalFiles'],
 					'groupID' => $nzbDetails['groupID'],
 					'guid' => $this->pdo->escapeString($this->relGuid),
+					'regexID' => 0,
 					'postdate' => $this->pdo->escapeString($nzbDetails['postDate']),
 					'fromname' => $escapedFromName,
+					'reqID' => 0,
+					'passwordstatus' => ($this->site->checkpasswordedrar > 0 ? -1 : 0),
 					'size' => $this->pdo->escapeString($nzbDetails['totalSize']),
 					'categoryID' => $this->category->determineCategory($nzbDetails['groupID'], $cleanName),
 					'isrenamed' => $renamed,
