e411822785a97da5189f83292b9954c44990b3c7
Darko <dkrisan@gmail.com>
2014-12-30 11:10:05 +0100

    Properly categorize a.b.triballs

--- lib/copy_this/www/lib/Categorize.php
+++ lib/copy_this/www/lib/Categorize.php
@@ -162,10 +162,15 @@ class Categorize extends Category
 					$this->tmpCat = \Category::CAT_MUSIC_MP3;
 					break;
 				case $group === 'alt.binaries.triballs':
-					if ($this->isMusic()) {
+					switch (true) {
+						case $this->isMusic():
+						case $this->isPCGame():
+						case $this->isMovie():
 						break;
+						default:
+							$this->tmpCat = \Category::CAT_MISC_OTHER;
+							break;
 					}
-					$this->tmpCat = \Category::CAT_MISC_OTHER;
 					break;
 				case preg_match('/alt\.binaries\.dvd(\-?r)?(\.(movies|))?$/i', $group):
 					if ($this->isMovie()) {
