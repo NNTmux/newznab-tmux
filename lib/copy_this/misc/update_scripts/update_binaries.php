267f9486c2f073a17c8c0e0c547c7d80c6ed6e9b
DariusIII <dkrisan@gmail.com>
2015-01-11 00:42:25 +0100

    Updated scripts, removed edits that are now part of newznab (nzbget related).

--- lib/copy_this/misc/update_scripts/update_binaries.php
+++ lib/copy_this/misc/update_scripts/update_binaries.php
@@ -3,20 +3,26 @@
 require_once(dirname(__FILE__) . "/config.php");
 require_once(WWW_DIR."/lib/groups.php");
 require_once(WWW_DIR."/lib/binaries.php");
+require_once(WWW_DIR."/lib/nntp.php");
 
-if (isset($argv[1]))
-{
-	$group = $argv[1];
-	echo "Updating group {$group}\n";
+$pdo = new DB();
 
-	$g = new Groups;
-	$group = $g->getByName($group);
-
-	$bin = new Binaries;
-	$bin->updateGroup($group);
+// Create the connection here and pass
+$nntp = new \NNTP(['Settings' => $pdo]);
+if ($nntp->doConnect() !== true) {
+	exit($pdo->log->error("Unable to connect to usenet."));
 }
-else
-{
-	$binaries = new Binaries;
-	$binaries->updateAllGroups();
+$binaries = new \Binaries(['NNTP' => $nntp, 'Settings' => $pdo]);
+
+if (isset($argv[1]) && !is_numeric($argv[1])) {
+	$groupName = $argv[1];
+	echo $pdo->log->header("Updating group: $groupName");
+
+	$grp = new \Groups(['Settings' => $pdo]);
+	$group = $grp->getByName($groupName);
+	if (is_array($group)) {
+		$binaries->updateGroup($group, (isset($argv[2]) && is_numeric($argv[2]) && $argv[2] > 0 ? $argv[2] : 0));
+	}
+} else {
+	$binaries->updateAllGroups((isset($argv[1]) && is_numeric($argv[1]) && $argv[1] > 0 ? $argv[1] : 0));
 }
