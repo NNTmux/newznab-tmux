c00e13b762c0bdf414c2e9d62ab3147879bc2b48
Darko <dkrisan@gmail.com>
2015-02-13 15:52:36 +0100

    Add the patch, up the version numbers.

---
+++ lib/DB/patches/0108~category.sql
@@ -0,0 +1,3 @@
+INSERT INTO category (ID, title, parentID) VALUES (6041, 'HD Clips', 6000);
+INSERT INTO category (ID, title, parentID) VALUES (6042, 'SD Clips', 6000);
+UPDATE `tmux` SET `value` = '108' WHERE `setting` = 'sqlpatch';
