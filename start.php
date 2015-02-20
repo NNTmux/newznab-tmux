e6fc566145a9912a9ac5f9e830a8e65398fa1a70
Darko <dkrisan@gmail.com>
2015-02-20 13:58:56 +0100

    Small fix in patchDB path.

--- start.php
+++ start.php
@@ -36,7 +36,7 @@ if (count($nntpkill) === 0) {
 
 // Check database patch version
 if ($patch < 108) {
-	exit($c->error("\nYour database is not up to date. Please update.\nphp ${DIR}/tmux/lib/DB/patchDB.php\n"));
+	exit($c->error("\nYour database is not up to date. Please update.\nphp ${DIR}lib/DB/patchDB.php\n"));
 }
 
 //check if session exists
