dfec9156dd830ec30a13f89e511fec8ad8b6088f
Darko <dkrisan@gmail.com>
2015-02-26 10:16:53 +0100

    Add DB based collection regexes and import count for nzbs.

---
+++ lib/DB/patches/0115~tmux.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO `tmux` (`setting`, `value`) VALUES ('import_count','50000');
+UPDATE `tmux` SET `value` = '115' WHERE `setting` = 'sqlpatch';
---
+++ lib/copy_this/www/admin/ajax_regex.php
@@ -0,0 +1,26 @@
+<?php
+require_once("config.php");
+require_once(WWW_DIR."/lib/adminpage.php");
+require_once(WWW_DIR."/lib/CollectionsCleaning.php");
+require_once(WWW_DIR."/lib/binaries.php");
+
+// Login Check
+$admin = new AdminPage;
+
+if (!isset($_GET['action'])) {
+	exit();
+}
+
+switch($_GET['action']) {
+	case 1:
+		$id = (int) $_GET['col_id'];
+		(new CollectionsCleaning(['Settings' => $admin->settings]))->deleteRegex($id);
+		print "Regex $id deleted.";
+		break;
+
+	case 2:
+		$id = (int) $_GET['bin_id'];
+		(new Binaries(['Settings' => $admin->settings]))->deleteBlacklist($id);
+		print "Blacklist $id deleted.";
+		break;
+}
