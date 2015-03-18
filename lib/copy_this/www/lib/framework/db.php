63a0fa9285b1fbabdedca8a7c7547b186e0e4d5e
Darko <dkrisan@gmail.com>
2015-03-18 14:35:48 +0100

    Enable $this->pdo->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);

--- lib/copy_this/www/lib/framework/db.php
+++ lib/copy_this/www/lib/framework/db.php
@@ -322,7 +322,7 @@ class DB extends \PDO
 		}
 
 		// For backwards compatibility, no need for a patch.
-		//$this->pdo->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
+		$this->pdo->setAttribute(\PDO::ATTR_CASE, \PDO::CASE_LOWER);
 		$this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 		$this->pdo->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 	}
