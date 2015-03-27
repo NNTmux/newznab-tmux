dfb9616d8cd06751dd53b19e768c29d6492e8ff5
Darko <dkrisan@gmail.com>
2015-03-27 14:35:08 +0100

    Another batch of changes and fixes. (cherry picked from commit e52d74b)

--- lib/copy_this/www/pages/failed.php
+++ lib/copy_this/www/pages/failed.php
@@ -9,6 +9,8 @@ $page = new Page();
 
 	if (isset($_GET['userid']) && is_numeric($_GET['userid']) && isset($_GET['rsstoken']) && isset($_GET['guid'])) {
 		$rel = $releases->getByGuid($_GET["guid"]);
+		$userid = $_GET['userid'];
+		$rsstoken = $_GET['rsstoken'];
 
 		if (!$rel)
 			$page->show404();
@@ -18,6 +20,6 @@ $page = new Page();
 			$page->show404();
 		}
 		//http://blah.net/getnzb/GUID.nzb&i=<usernumber>&r=APIKEY
-		$url = $page->serverurl . 'getnzb/' . $alt['guid'] . '.nzb' . '&i=' . $_GET['userid'] . '&r=' . $_GET['rsstoken'];
+		$url = $page->serverurl . 'getnzb/' . $alt['guid'] . '.nzb' . '&i=' . $userid . '&r=' . $rsstoken;
 		header('Location: ' . $url . '');
 	}
