6bba3addac0dc4f39fa090d5520fd6f48b5f4bb5
Darko <dkrisan@gmail.com>
2015-03-27 13:22:28 +0100

    Expand support: Add failed download tracking for X-DNZB-Failure headers. (cherry picked from commit 9337549)

--- lib/copy_this/www/pages/failed.php
+++ lib/copy_this/www/pages/failed.php
@@ -7,19 +7,16 @@ $releases = new Releases(['Settings' => $page->settings]);
 $users = new Users();
 $page = new Page();
 
-	if (isset($_GET["userid"])) {
+	if (isset($_GET["userid"]) && isset($_GET['rsstoken']) && isset($_GET['guid'])) {
 		$rel = $releases->getByGuid($_GET["guid"]);
 
 		if (!$rel)
 			$page->show404();
 
-		$alt = $releases->getAlternate($rel['guid'], $rel['searchname']);
+		$alt = $releases->getAlternate($rel['guid'], $rel['searchname'], $_GET['userid']);
 		if (!$alt) {
 			$page->show404();
 		}
-		$userid = $users->getById($_GET["userid"]);
-		$uid = $userid['id'];
-		$rsstoken = $userid['rsstoken'];
 		//http://blah.net/getnzb/GUID.nzb&i=<usernumber>&r=APIKEY
 		$url = $page->serverurl . 'getnzb/' . $alt['guid'] . '.nzb' . '&i=' . $_GET['userid'] . '&r=' . $_GET['rsstoken'];
 		header('Location: ' . $url . '');
