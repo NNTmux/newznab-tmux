0b0a7ba48e24c878a34ee1d9b944cd0e7de8246d
Darko <dkrisan@gmail.com>
2015-05-04 09:00:00 +0200

    Remove stuff related to dev-regexless, again.

--- lib/copy_this/www/admin/ajax.php
+++ lib/copy_this/www/admin/ajax.php
@@ -18,24 +18,6 @@ switch($_GET['action']) {
 		print "Blacklist $id deleted.";
 		break;
 
-	case 'category_regex_delete':
-		$id = (int) $_GET['row_id'];
-		(new Regexes(['Settings' => $admin->settings, 'Table_Name' => 'category_regexes']))->deleteRegex($id);
-		print "Regex $id deleted.";
-		break;
-
-	case 'collection_regex_delete':
-		$id = (int) $_GET['row_id'];
-		(new Regexes(['Settings' => $admin->settings, 'Table_Name' => 'collection_regexes']))->deleteRegex($id);
-		print "Regex $id deleted.";
-		break;
-
-	case 'release_naming_regex_delete':
-		$id = (int) $_GET['row_id'];
-		(new Regexes(['Settings' => $admin->settings, 'Table_Name' => 'release_naming_regexes']))->deleteRegex($id);
-		print "Regex $id deleted.";
-		break;
-
 	case 'group_edit_purge_all':
 		session_write_close();
 		(new Groups($settings))->purge();
