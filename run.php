49480a89300ff736c14209927dd44a05e386d7c6
Darko <dkrisan@gmail.com>
2015-05-06 09:34:17 +0200

    Fix the wrong regex usage.

--- run.php
+++ run.php
@@ -38,7 +38,7 @@ if (count($nntpkill) !== 0) {
 }
 
 // Check database patch version
-if ($patch < 127) {
+if ($patch < 130) {
 	exit($c->error("\nYour database is not up to date. Please update.\nphp /var/www/newznab/misc/update_scripts/nix_scripts/tmux/lib/DB/patchDB.php\n"));
 }
 
