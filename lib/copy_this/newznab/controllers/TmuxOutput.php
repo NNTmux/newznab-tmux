86e476e911aad64dcc1379944e189ffeb6d70bd3
DariusIII <dkrisan@gmail.com>
2015-05-23 03:54:49 +0200

    Up the version number.

--- lib/copy_this/newznab/controllers/TmuxOutput.php
+++ lib/copy_this/newznab/controllers/TmuxOutput.php
@@ -114,7 +114,7 @@ class TmuxOutput extends Tmux
 		$buffer = '';
 		$state = ($this->runVar['settings']['is_running'] == 1) ? 'Running' : 'Disabled';
 		//$version = $this->_tvers . 'r' . $this->_vers;
-		$tversion = '0.5r01273';
+		$tversion = '0.5r01275';
 
 		$buffer .= sprintf($this->tmpMasks[2],
 					"Monitor $state v$tversion @ $this->_tvers [" . $this->_vers ."]: ",
