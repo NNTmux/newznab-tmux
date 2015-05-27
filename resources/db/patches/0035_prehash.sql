e4fb319b660f45b06800d9ff8d298ae5c9ad7e7f
Darko <dkrisan@gmail.com>
2015-05-27 11:20:47 +0200

    Push patch fixes to the new folder

--- resources/db/patches/0035_prehash.sql
+++ resources/db/patches/0035_prehash.sql
@@ -6,10 +6,8 @@ ALTER TABLE `releasefiles` ADD INDEX `ix_releasefiles_ishashed` (`ishashed`);
 DROP TRIGGER IF EXISTS check_rfinsert;
 DROP TRIGGER IF EXISTS check_rfupdate;
 
-DELIMITER $$
-CREATE TRIGGER check_rfinsert BEFORE INSERT ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;$$
-CREATE TRIGGER check_rfupdate BEFORE UPDATE ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;$$
-DELIMITER ;
+CREATE TRIGGER check_rfinsert BEFORE INSERT ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;
+CREATE TRIGGER check_rfupdate BEFORE UPDATE ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;
 
 UPDATE `releasefiles` SET ishashed = 1 WHERE name REGEXP '[a-fA-F0-9]{32}';
 
