dc50ecfbdd48bfc7ddb756ed7c0262b0de3df764
Darko <dkrisan@gmail.com>
2015-05-27 10:23:09 +0200

    Update patch 72.

--- misc/update_scripts/nix_scripts/tmux/lib/DB/patches/0072~site.sql
+++ misc/update_scripts/nix_scripts/tmux/lib/DB/patches/0072~site.sql
@@ -6,6 +6,9 @@ DROP TRIGGER IF EXISTS check_rfupdate;
 DROP TRIGGER IF EXISTS check_insert;
 DROP TRIGGER IF EXISTS check_update;
 DROP TRIGGER IF EXISTS insert_MD5;
+DROP TRIGGER IF EXISTS insert_search;
+DROP TRIGGER IF EXISTS update_search;
+DROP TRIGGER IF EXISTS delete_search;
 
 CREATE TRIGGER insert_hashes AFTER INSERT ON prehash FOR EACH ROW BEGIN INSERT INTO predbhash (pre_id, hashes) VALUES (NEW.id, CONCAT_WS(',', MD5(NEW.title), MD5(MD5(NEW.title)), SHA1(NEW.title)));END;
 CREATE TRIGGER update_hashes AFTER UPDATE ON prehash FOR EACH ROW BEGIN IF NEW.title != OLD.title THEN UPDATE predbhash SET hashes = CONCAT_WS(',', MD5(NEW.title), MD5(MD5(NEW.title)), SHA1(NEW.title)) WHERE pre_id = OLD.id; END IF;END;
