8edf5403507b2027ad29d8a1c1f4d8648e53d9e4
Darko <dkrisan@gmail.com>
2015-06-01 10:49:19 +0200

    Fix typo in postprocess.php.

--- misc/update_scripts/nix_scripts/tmux/bin/postprocess.php
+++ misc/update_scripts/nix_scripts/tmux/bin/postprocess.php
@@ -65,7 +65,7 @@ if (!isset($argv[1]) || !in_array($argv[1], $args) || !isset($argv[2]) || !in_ar
 $nntp = null;
 if ($args[$argv[1]] === true) {
 	$nntp = new NNTP(['Settings' => $pdo]);
-	if (($this->pdo->getSetting('alternate_nntp') == 1 ? $nntp->doConnect(true, true) : $nntp->doConnect()) !== true) {
+	if (($pdo->getSetting('alternate_nntp') == 1 ? $nntp->doConnect(true, true) : $nntp->doConnect()) !== true) {
 		exit($pdo->log->error("Unable to connect to usenet." . PHP_EOL));
 	}
 }
