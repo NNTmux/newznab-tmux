3e79386c0fc567050761e1f28ac7cb1547360f92
Darko <dkrisan@gmail.com>
2015-06-03 10:28:10 +0200

    Fixed the cart issues. (cherry picked from commit 2ab7193)

--- www/pages/cart.php
+++ www/pages/cart.php
@@ -1,50 +1,45 @@
 <?php
 
-if (!$users->isLoggedIn())
+if (!$page->users->isLoggedIn()) {
 	$page->show403();
+}
 
-if (isset($_REQUEST["add"]))
-{
-	if (isset($_GET['add']) && !empty($_GET['add']))
-		$ids = array($_GET['add']);
-	elseif (isset($_POST['add']) && is_array($_POST['add']))
-		$ids = $_POST['add'];
+if (isset($_GET["add"])) {
+	$releases = new Releases(['Settings' => $page->settings]);
+	$guids = explode(',', $_GET['add']);
+	$data = $releases->getByGuid($guids);
 
-	$releases = new Releases;
-	$data = $releases->getByGuid($ids);
-
-	if (!$data)
+	if (!$data) {
 		$page->show404();
+	}
 
-	foreach($data as $d)
-		$users->addCart($users->currentUserId(), $d["id"]);
-}
-elseif (isset($_REQUEST["delete"]))
-{
-	if (isset($_GET['delete']) && !empty($_GET['delete']))
+	foreach ($data as $d) {
+		$page->users->addCart($page->users->currentUserId(), $d["id"]);
+	}
+} elseif (isset($_REQUEST["delete"])) {
+	if (isset($_GET['delete']) && !empty($_GET['delete'])) {
 		$ids = array($_GET['delete']);
-	elseif (isset($_POST['delete']) && is_array($_POST['delete']))
+	} elseif (isset($_POST['delete']) && is_array($_POST['delete'])) {
 		$ids = $_POST['delete'];
+	}
 
-	if (isset($ids))
-		$users->delCartByGuid($ids, $users->currentUserId());
+	if (isset($ids)) {
+		$page->users->delCartByGuid($ids, $page->users->currentUserId());
+	}
 
-	if (!isset($_POST['delete']))
-		header("Location: ".WWW_TOP."/cart");
+	if (!isset($_POST['delete'])) {
+		header("Location: " . WWW_TOP . "/cart");
+	}
 
-	die();
-}
-else
-{
+	exit();
+} else {
 	$page->meta_title = "My Nzb Cart";
 	$page->meta_keywords = "search,add,to,cart,nzb,description,details";
 	$page->meta_description = "Manage Your Nzb Cart";
 
-	$results = $users->getCart($users->currentUserId());
+	$results = $users->getCart($page->users->currentUserId());
 	$page->smarty->assign('results', $results);
 
 	$page->content = $page->smarty->fetch('cart.tpl');
 	$page->render();
 }
-
-
