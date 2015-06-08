73158bc4f23da2f4495dbf022956c82b56041490
Darko <dkrisan@gmail.com>
2015-06-08 09:29:27 +0200

    Update BasePage and login, fix reference to non-existent folder in rss.tpl. (cherry picked from commit b506e10)

--- newznab/controllers/BasePage.php
+++ newznab/controllers/BasePage.php
@@ -280,9 +280,12 @@ class BasePage
 		$this->body = $this->body." ".$attr;
 	}
 
+	/**
+	 * @return bool
+	 */
 	public function isPostBack()
 	{
-		return (strtoupper($_SERVER["REQUEST_METHOD"]) === "POST");
+		return (strtoupper($_SERVER['REQUEST_METHOD']) === 'POST');
 	}
 
 	/**
