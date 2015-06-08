402af5c784b6db6ebf9a27824320d878c15c8650
DariusIII <dkrisan@gmail.com>
2015-06-08 03:29:07 +0200

    Update rss handling.

--- www/pages/rss.php
+++ www/pages/rss.php
@@ -1,22 +1,21 @@
 <?php
 
-$category = new Category;
-$releases = new Releases;
+use newznab\db\Settings;
 
-//
-// user has to either be logged in, or using rsskey
-//
+$category = new Category(['Settings' => $page->settings]);
+$releases = new Releases(['Settings' => $page->settings]);
 
-//
-// if no content id provided then show user the rss selection page
-//
-if (!isset($_GET["t"]) && !isset($_GET["rage"]) && !isset($_GET["anidb"]))
-{
-	//
-	// must be logged in to view this help page
-	//
-	if (!$users->isLoggedIn())
-		$page->show403();
+// If no content id provided then show user the rss selection page.
+if (!isset($_GET["t"]) && !isset($_GET["rage"]) && !isset($_GET["anidb"])) {
+	// User has to either be logged in, or using rsskey.
+	if (!$page->users->isLoggedIn()) {
+		if ($page->settings->getSetting('registerstatus') != Settings::REGISTER_STATUS_API_ONLY) {
+			header('X-newznab: ERROR: You must be logged in or provide a valid User ID and API key!');
+			$page->show403();
+		} else {
+			header("Location: " . $page->settings->getSetting('code'));
+		}
+	}
 
 	$page->title = "Rss Feeds";
 	$page->meta_title = "Rss Nzb Feeds";
@@ -24,114 +23,100 @@ if (!isset($_GET["t"]) && !isset($_GET["rage"]) && !isset($_GET["anidb"]))
 	$page->meta_description = "View available Rss Nzb feeds.";
 
 	$categorylist = $category->get(true, $page->userdata["categoryexclusions"]);
-	$page->smarty->assign('categorylist',$categorylist);
+	$page->smarty->assign('categorylist', $categorylist);
 
 	$parentcategorylist = $category->getForMenu($page->userdata["categoryexclusions"]);
-	$page->smarty->assign('parentcategorylist',$parentcategorylist);
+	$page->smarty->assign('parentcategorylist', $parentcategorylist);
 
 	$page->content = $page->smarty->fetch('rssdesc.tpl');
 	$page->render();
-
-}
-//
-// user requested a feed, ensure either logged in or passing a valid token
-//
-else
-{
-	$uid = -1;
-	$rsstoken = -1;
-	if (!$users->isLoggedIn())
-	{
-		if (!isset($_GET["i"]) || !isset($_GET["r"]))
-			$page->show403();
-
-		$res = $users->getByIdAndRssToken($_GET["i"], $_GET["r"]);
-		if (!$res)
-			$page->show403();
-
-		$uid = $_GET["i"];
-		$rsstoken = $_GET["r"];
-		$maxrequests = $res['apirequests'];
-	}
-	else
-	{
+} else {
+	$rsstoken = $uid = -1;
+	// User requested a feed, ensure either logged in or passing a valid token.
+	if ($page->users->isLoggedIn()) {
 		$uid = $page->userdata["id"];
 		$rsstoken = $page->userdata["rsstoken"];
 		$maxrequests = $page->userdata['apirequests'];
-	}
-
-	//
-	// A hash of the users ip to record against the api hit
-	//
-	$hosthash = "";
-	if ($page->site->storeuserips == 1)
-		$hosthash = $users->getHostHash($_SERVER["REMOTE_ADDR"], $page->site->siteseed);
-
-	$apirequests = $users->getApiRequests($uid);
-	if ($apirequests['num'] > $maxrequests)
-	{
-		$page->show429($apirequests['nextrequest']);
 	} else {
-		$users->addApiRequest($uid, $_SERVER['REQUEST_URI'], $hosthash);
+		if ($page->settings->getSetting('registerstatus') == Settings::REGISTER_STATUS_API_ONLY) {
+			$res = $page->users->getById(0);
+		} else {
+			if (!isset($_GET["i"]) || !isset($_GET["r"])) {
+				header('X-newznab: ERROR: Both the User ID and API key are required for viewing the RSS!');
+				$page->show403();
+			}
+
+			$res = $page->users->getByIdAndRssToken($_GET["i"], $_GET["r"]);
+		}
+
+		if (!$res) {
+			header('X-newznab: ERROR: Invalid API key or User ID!');
+			$page->show403();
+		}
+
+		$uid = $res["id"];
+		$rsstoken = $res['rsstoken'];
+		$maxrequests = $res['apirequests'];
 	}
 
-	//
-	// valid or logged in user, get them the requested feed
-	//
-	if (isset($_GET["dl"]) && $_GET["dl"] == "1")
-		$page->smarty->assign("dl","1");
+	$apirequests = $page->users->getApiRequests($uid);
+	if ($apirequests['num'] > $maxrequests) {
+		header('X-newznab: ERROR: You have reached your daily limit for API requests!');
+		$page->show503();
+	} else {
+		$page->users->addApiRequest($uid, $_SERVER['REQUEST_URI']);
+	}
+
+	// Valid or logged in user, get them the requested feed.
+	if (isset($_GET["dl"]) && $_GET["dl"] == "1") {
+		$page->smarty->assign("dl", "1");
+	}
 
 	$usercat = -1;
-	if (isset($_GET["t"]))
-		$usercat = ($_GET["t"]==0 ? -1 : $_GET["t"]);
+	if (isset($_GET["t"])) {
+		$usercat = ($_GET["t"] == 0 ? -1 : $_GET["t"]);
+	}
 
-	$userrage = -1;
-	$useranidb = -1;
-	$userseries = -1;
-	if (isset($_GET["rage"]))
-	{
-		$userrage = ($_GET["rage"]==0 ? -1 : $_GET["rage"]+0);
+	$userrage = $useranidb = $userseries = -1;
+	if (isset($_GET["rage"])) {
+		$userrage = ($_GET["rage"] == 0 ? -1 : $_GET["rage"] + 0);
 	} elseif (isset($_GET["anidb"])) {
-		$useranidb = ($_GET["anidb"]==0 ? -1 : $_GET["anidb"]+0);
+		$useranidb = ($_GET["anidb"] == 0 ? -1 : $_GET["anidb"] + 0);
 	}
 
 	$usernum = 100;
-	if (isset($_GET["num"]))
-		$usernum = $_GET["num"]+0;
+	if (isset($_GET["num"])) {
+		$usernum = $_GET["num"] + 0;
+	}
 
-	if (isset($_GET["del"]) && $_GET["del"] == "1")
-		$page->smarty->assign("del","1");
+	if (isset($_GET["del"]) && $_GET["del"] == "1") {
+		$page->smarty->assign("del", "1");
+	}
 
 	$userairdate = -1;
-	if (isset($_GET["airdate"]))
-		$userairdate = $_GET["airdate"]+0;
+	if (isset($_GET["airdate"])) {
+		$userairdate = $_GET["airdate"] + 0;
+	}
 
-	$page->smarty->assign('uid',$uid);
-	$page->smarty->assign('rsstoken',$rsstoken);
+	$page->smarty->assign('uid', $uid);
+	$page->smarty->assign('rsstoken', $rsstoken);
 
-	if ($usercat == -3)
-	{
-		$page->smarty->assign('rsstitle',"My Shows Feed");
-		$catexclusions = $users->getCategoryExclusion($uid);
+	if ($usercat == -3) {
+		$catexclusions = $page->users->getCategoryExclusion($uid);
 		$reldata = $releases->getShowsRss($usernum, $uid, $catexclusions, $userairdate);
-	}
-	elseif ($usercat == -4)
-	{
-		$page->smarty->assign('rsstitle',"My Movies Feed");
-		$catexclusions = $users->getCategoryExclusion($uid);
+	} elseif ($usercat == -4) {
+		$catexclusions = $page->users->getCategoryExclusion($uid);
 		$reldata = $releases->getMyMoviesRss($usernum, $uid, $catexclusions);
+	} else {
+		$reldata = $releases->getRss(explode(",", $usercat),
+			$usernum,
+			$userrage,
+			$useranidb,
+			$uid,
+			$userairdate);
 	}
-	else
-	{
-		if ($usercat == -2)
-			$page->smarty->assign('rsstitle',"My Cart Feed");
 
-		$reldata = $releases->getRss(explode(",",$usercat), $usernum, $uid, $userrage, $useranidb, $userairdate);
-	}
-	$page->smarty->assign('releases',$reldata);
+	$page->smarty->assign('releases', $reldata);
 	header("Content-type: text/xml");
-
-	$page->smarty->assign('rsshead',$page->smarty->fetch('rssheader.tpl'));
 	echo trim($page->smarty->fetch('rss.tpl'));
-
 }
