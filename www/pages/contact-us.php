32938ccf7c42e6be78b64671e7fb9378ff36f6cd
DariusIII <dkrisan@gmail.com>
2015-06-09 23:09:14 +0200

    Change site into settings in rest of the files.

--- www/pages/contact-us.php
+++ www/pages/contact-us.php
@@ -9,7 +9,7 @@ if (isset($_POST["useremail"])) {
 
 	if ($page->captcha->getError() === false) {
 		$email = $_POST["useremail"];
-		$mailto = $page->site->email;
+		$mailto = $page->settings->getSetting('email');
 		$mailsubj = "Contact Form Submitted";
 		$mailhead = "From: $email\n";
 		$mailbody = "Values submitted from contact form:\n";
@@ -24,13 +24,13 @@ if (isset($_POST["useremail"])) {
 			Utility::sendEmail($mailto, $mailsubj, $mailbody, $email);
 		}
 
-		$page->smarty->assign("msg", "<h2 style='padding-top:25px;'>Thanks for getting in touch with " . $page->site->title . ".</h2>");
+		$page->smarty->assign("msg", "<h2 style='padding-top:25px;'>Thanks for getting in touch with " . $page->settings->getSetting('title') . ".</h2>");
 	}
 }
-$page->title = "Contact ".$page->site->title;
-$page->meta_title = "Contact ".$page->site->title;
+$page->title = "Contact ".$page->settings->getSetting('title');
+$page->meta_title = "Contact ".$page->settings->getSetting('title');
 $page->meta_keywords = "contact us,contact,get in touch,email";
-$page->meta_description = "Contact us at ".$page->site->title." and submit your feedback";
+$page->meta_description = "Contact us at ".$page->settings->getSetting('title')." and submit your feedback";
 
 $page->content = $page->smarty->fetch('contact.tpl');
 
