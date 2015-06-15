25e80fdf46760f8b084b09d36f3935cf38436152
Darko <dkrisan@gmail.com>
2015-06-15 16:01:55 +0200

    Fix some of the notices for userimdbids, etc.

--- www/pages/mymoviesedit.php
+++ www/pages/mymoviesedit.php
@@ -1,44 +1,39 @@
 <?php
 
 
-if (!$page->users->isLoggedIn())
+if (!$page->users->isLoggedIn()) {
 	$page->show403();
+}
 
-$um = new UserMovies();
-
-if (isset($_REQUEST["del"]))
-{
+$um = new UserMovies(['Settings' => $page->settings]);
+if (isset($_REQUEST["del"])) {
 	$um->delMovie($page->users->currentUserId(), $_REQUEST["del"]);
 }
 
-$cat = new Category;
-$tmpcats = $cat->getChildren(Category::CAT_PARENT_MOVIE, true, $page->userdata["categoryexclusions"]);
+$cat = new Category(['Settings' => $page->settings]);
+$tmpcats = $cat->getChildren(Category::CAT_PARENT_MOVIE);
 $categories = array();
-foreach($tmpcats as $c)
-	$categories[$c['ID']] = $c['title'];
+foreach ($tmpcats as $c) {
+	$categories[$c['id']] = $c['title'];
+}
 
 $movies = $um->getMovies($page->users->currentUserId());
 $results = array();
-foreach ($movies as $mov=>$m)
-{
-	$movcats = explode('|', $m['categoryID']);
-	if (is_array($movcats) && sizeof($movcats) > 0)
-	{
+foreach ($movies as $mov => $m) {
+	$movcats = explode('|', $m['categoryid']);
+	if (is_array($movcats) && sizeof($movcats) > 0) {
 		$catarr = array();
-		foreach ($movcats as $movcat)
-		{
-			if (!empty($movcat))
+		foreach ($movcats as $movcat) {
+			if (!empty($movcat)) {
 				$catarr[] = $categories[$movcat];
+			}
 		}
 		$m['categoryNames'] = implode(', ', $catarr);
-	}
-	else
-	{
+	} else {
 		$m['categoryNames'] = '';
-
 	}
-	$results[$mov] = $m;
 
+	$results[$mov] = $m;
 }
 $page->smarty->assign('movies', $results);
 
@@ -49,5 +44,3 @@ $page->meta_description = "Manage Your Movies";
 
 $page->content = $page->smarty->fetch('mymoviesedit.tpl');
 $page->render();
-
-
