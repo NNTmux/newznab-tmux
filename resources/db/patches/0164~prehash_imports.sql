d93f90b1158aed39dd59e6ebf7a086c6c83e68af
DariusIII <dkrisan@gmail.com>
2015-07-26 15:52:28 +0200

    Wrong row name.

--- resources/db/patches/0164~prehash_imports.sql
+++ resources/db/patches/0164~prehash_imports.sql
@@ -13,7 +13,7 @@ CREATE TABLE prehash_imports (
   source     VARCHAR(50)
              COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   requestid  INT(10) UNSIGNED        NOT NULL DEFAULT '0',
-  group_id   INT(10) UNSIGNED        NOT NULL DEFAULT '0'
+  groupid   INT(10) UNSIGNED        NOT NULL DEFAULT '0'
   COMMENT 'FK to groups',
   nuked      TINYINT(1)              NOT NULL DEFAULT '0'
   COMMENT 'Is this pre nuked? 0 no 2 yes 1 un nuked 3 mod nuked',
