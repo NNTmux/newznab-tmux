fa5b06eb2e0e7c72b295b092fde0aa96b188bd01
Darko <dkrisan@gmail.com>
2015-07-31 12:26:32 +0200

    Move IRCScraoer into misc/testing folder

--- misc/testing/IRCScraper/scrape.php
+++ misc/testing/IRCScraper/scrape.php
@@ -1,5 +1,5 @@
 <?php
-require_once(dirname(__FILE__) . "/../../bin/config.php");
+require_once dirname(__FILE__) . '/../../../www/config.php';
 
 if (!is_file(NN_TMUX . 'lib' . DS . 'IRCScraper' .DS . 'settings.php')) {
 	exit('Copy settings_example.php to settings.php and change the settings.' . PHP_EOL);
@@ -18,7 +18,7 @@ if (!isset($argv[1]) || $argv[1] !== 'true') {
 	);
 }
 
-require_once (NN_TMUX . 'lib' . DS . 'IRCScraper' .DS . 'settings.php');
+require_once (NN_MISC . 'testing' . DS . 'IRCScraper' .DS . 'settings.php');
 
 if (!defined('SCRAPE_IRC_NICKNAME')) {
 	exit('ERROR! You must update settings.php using settings_example.php.');
