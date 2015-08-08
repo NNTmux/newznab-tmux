28baabb9048c6d64bb1f5c7bc91653d88eeddb82
DariusIII <dkrisan@gmail.com>
2015-08-09 00:07:27 +0200

    Changes to movie list. (cherry picked from commit 945d362)

--- www/admin/movie-list.php
+++ www/admin/movie-list.php
@@ -2,30 +2,23 @@
 
 require_once("config.php");
 
-$page = new AdminPage();
-
-$movie = new Film(['Settings' => $page->settings]);
+$page  = new AdminPage();
+$movie = new Movie(['Settings' => $page->settings]);
 
 $page->title = "Movie List";
-
-$tname = "";
-if (isset($_REQUEST['moviename']) && !empty($_REQUEST['moviename']))
-	$tname = $_REQUEST['moviename'];
 
 $movcount = $movie->getCount();
 
 $offset = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$tsearch = ($tname != "") ? 'moviename='.$tname.'&amp;' : '';
-
-$page->smarty->assign('pagertotalitems',$movcount);
-$page->smarty->assign('pageroffset',$offset);
-$page->smarty->assign('pageritemsperpage',ITEMS_PER_PAGE);
+$page->smarty->assign('pagertotalitems', $movcount);
+$page->smarty->assign('pageroffset', $offset);
+$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
 $page->smarty->assign('pagerquerybase', WWW_TOP . "/movie-list.php?offset=");
 $pager = $page->smarty->fetch("pager.tpl");
 $page->smarty->assign('pager', $pager);
 
 $movielist = $movie->getRange($offset, ITEMS_PER_PAGE);
-$page->smarty->assign('movielist',$movielist);
+$page->smarty->assign('movielist', $movielist);
 
 $page->content = $page->smarty->fetch('movie-list.tpl');
 $page->render();
