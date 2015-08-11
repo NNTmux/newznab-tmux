98bbb4d6615dc6307a181f1ce2d74e00b6ff5714
DariusIII <dkrisan@gmail.com>
2015-08-11 19:47:19 +0200

    Fix the patch.

--- resources/db/patches/0173~predb_hashes.sql
+++ resources/db/patches/0173~predb_hashes.sql
@@ -13,10 +13,10 @@ ALTER TABLE predbhash ADD hash VARBINARY(20) FIRST;
 -- Adding a primary key to the hash column.
 ALTER TABLE predbhash ADD PRIMARY KEY(hash);
 
--- Creating hashes from predb titles, this can be long.
+-- Creating hashes from prehash titles, this can be long.
 -- Stage 1: UNHEX(md5(title))
-INSERT IGNORE INTO predbhash SELECT UNHEX(md5(title)), id from predb;
+INSERT IGNORE INTO predbhash SELECT UNHEX(md5(title)), id from prehash;
 -- Stage 2: UNHEX(md5(md5(title)))
-INSERT IGNORE INTO predbhash SELECT UNHEX(md5(md5(title))), id from predb;
+INSERT IGNORE INTO predbhash SELECT UNHEX(md5(md5(title))), id from prehash;
 -- Stage 3: UNHEX(sha1(title))
-INSERT IGNORE INTO predbhash SELECT UNHEX(sha1(title)), id from predb;
+INSERT IGNORE INTO predbhash SELECT UNHEX(sha1(title)), id from prehash;
