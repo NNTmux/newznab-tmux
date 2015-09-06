dfd08c410c3cd6fa171fac6567ae9b88acb4825b
DariusIII <dkrisan@gmail.com>
2015-09-06 21:23:41 +0200

    Change class used for books, remove the old Book class. (cherry picked from commit bc44d9b)

--- www/pages/books.php
+++ www/pages/books.php
@@ -1,13 +1,14 @@
 <?php
 
-$b = new Book;
+if (!$page->users->isLoggedIn()) {
+	$page->show403();
+}
+
+$book = new Books(['Settings' => $page->settings]);
 $cat = new Category(['Settings' => $page->settings]);
 
-if (!$page->users->isLoggedIn())
-	$page->show403();
-
 $boocats = $cat->getChildren(Category::CAT_PARENT_BOOK);
-$btmp = [];
+$btmp = array();
 foreach ($boocats as $bcat) {
 	$btmp[$bcat['id']] = $bcat;
 }
@@ -16,56 +17,69 @@ if (isset($_REQUEST["t"]) && array_key_exists($_REQUEST['t'], $btmp)) {
 	$category = $_REQUEST["t"] + 0;
 }
 
-$catarray = [];
+$catarray = array();
 $catarray[] = $category;
 
 $page->smarty->assign('catlist', $btmp);
 $page->smarty->assign('category', $category);
 
-$browsecount = $b->getBookCount();
+$browsecount = $book->getBookCount($catarray, -1, $page->userdata["categoryexclusions"]);
 
 $offset = (isset($_REQUEST["offset"]) && ctype_digit($_REQUEST['offset'])) ? $_REQUEST["offset"] : 0;
-$ordering = $b->getBookOrdering();
+$ordering = $book->getBookOrdering();
 $orderby = isset($_REQUEST["ob"]) && in_array($_REQUEST['ob'], $ordering) ? $_REQUEST["ob"] : '';
 
-$results = $b->getBookRange($offset, ITEMS_PER_COVER_PAGE, $orderby);
+$results = $books = array();
+$results = $book->getBookRange($catarray, $offset, ITEMS_PER_COVER_PAGE, $orderby, $page->userdata["categoryexclusions"]);
 
-$title = (isset($_REQUEST['title']) && !empty($_REQUEST['title'])) ? stripslashes($_REQUEST['title']) : '';
-$page->smarty->assign('title', $title);
+$maxwords = 50;
+foreach ($results as $result) {
+	if (!empty($result['overview'])) {
+		$words = explode(' ', $result['overview']);
+		if (sizeof($words) > $maxwords) {
+			$newwords = array_slice($words, 0, $maxwords);
+			$result['overview'] = implode(' ', $newwords) . '...';
+		}
+	}
+	$books[] = $result;
+}
 
 $author = (isset($_REQUEST['author']) && !empty($_REQUEST['author'])) ? stripslashes($_REQUEST['author']) : '';
 $page->smarty->assign('author', $author);
 
-$browseby_link = '?title='.$title.'&amp;author='.$author;
+$title = (isset($_REQUEST['title']) && !empty($_REQUEST['title'])) ? stripslashes($_REQUEST['title']) : '';
+$page->smarty->assign('title', $title);
 
-$page->smarty->assign('pagertotalitems',$browsecount);
-$page->smarty->assign('pageroffset',$offset);
-$page->smarty->assign('pageritemsperpage',ITEMS_PER_COVER_PAGE);
-$page->smarty->assign('pagerquerybase', WWW_TOP."/books".$browseby_link."&amp;ob=".$orderby."&amp;offset=");
+$browseby_link = '&amp;title=' . $title . '&amp;author=' . $author;
+
+$page->smarty->assign('pagertotalitems', $browsecount);
+$page->smarty->assign('pageroffset', $offset);
+$page->smarty->assign('pageritemsperpage', ITEMS_PER_COVER_PAGE);
+$page->smarty->assign('pagerquerybase', WWW_TOP . "/books?t=" . $category . $browseby_link . "&amp;ob=" . $orderby . "&amp;offset=");
 $page->smarty->assign('pagerquerysuffix', "#results");
 
 $pager = $page->smarty->fetch("pager.tpl");
 $page->smarty->assign('pager', $pager);
 
-if ($category == -1)
-	$page->smarty->assign("catname","All");
-else
-{
-	$cat = new Category();
+if ($category == -1) {
+	$page->smarty->assign("catname", "All");
+} else {
 	$cdata = $cat->getById($category);
-	if ($cdata)
-		$page->smarty->assign('catname',$cdata["title"]);
-	else
+	if ($cdata) {
+		$page->smarty->assign('catname', $cdata["title"]);
+	} else {
 		$page->show404();
+	}
 }
 
-foreach($ordering as $ordertype)
-	$page->smarty->assign('orderby'.$ordertype, WWW_TOP."/books".$browseby_link."&amp;ob=".$ordertype."&amp;offset=0");
+foreach ($ordering as $ordertype) {
+	$page->smarty->assign('orderby' . $ordertype, WWW_TOP . "/books?t=" . $category . $browseby_link . "&amp;ob=" . $ordertype . "&amp;offset=0");
+}
 
-$page->smarty->assign('results',$results);
+$page->smarty->assign('results', $books);
 
 $page->meta_title = "Browse Books";
-$page->meta_keywords = "browse,nzb,ebooks,books,e-books";
+$page->meta_keywords = "browse,nzb,books,description,details";
 $page->meta_description = "Browse for Books";
 
 $page->content = $page->smarty->fetch('books.tpl');
