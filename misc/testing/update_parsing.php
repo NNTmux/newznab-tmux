2b23fd419438f0454993a3eb77062d2e797ffbce
Darko <dkrisan@gmail.com>
2015-09-16 12:37:58 +0200

    update import.php in misc/testing folder. Remove scripts  and classes not used in newznab-tmux.

--- misc/testing/update_parsing.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/../../www/config.php';
-
-$echoonly = false;
-$limittotoday = false;
-$verbose = true;
-
-$p = new Parsing($echoonly, $limittotoday, $verbose);
-$p->process();
