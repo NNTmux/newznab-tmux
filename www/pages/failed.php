1d2949c7bee740b292e5f2447a52343c74a269d8
Darko <dkrisan@gmail.com>
2015-09-23 10:46:14 +0200

    Improve X-DNZB-Failure headers support.

--- www/pages/failed.php
+++ www/pages/failed.php
@@ -1,22 +1,24 @@
 <?php
 
-$releases = new Releases(['Settings' => $page->settings]);
-$page->users = new Users();
-$page = new Page();
+if (isset($_GET['guid']) && isset($_GET['searchname']) && isset($_GET['userid']) && is_numeric($_GET['userid']) && isset($_GET['rsstoken'])) {
 
-if (isset($_GET['userid']) && is_numeric($_GET['userid']) && isset($_GET['rsstoken']) && isset($_GET['guid'])) {
-	$rel = $releases->getByGuid($_GET["guid"]);
+	$page = new Page();
+	$releases = new Releases(['Settings' => $page->settings]);
+	$users = new Users();
+
 	$userid = $_GET['userid'];
 	$rsstoken = $_GET['rsstoken'];
+	$rel = $releases->getByGuid($_GET['guid']);
 
 	if (!$rel) {
 		$page->show404();
 	}
 
-	$alt = $releases->getAlternate($rel['guid'], $rel['searchname'], $userid);
+	$alt = $releases->getAlternate($_GET['guid'], $_GET['searchname'], $userid);
 	if (!$alt) {
 		$page->show404();
 	}
+
 	$url = $page->serverurl . 'getnzb/' . $alt['guid'] . '.nzb' . '&i=' . $userid . '&r=' . $rsstoken;
 	header('Location: ' . $url . '');
 }
