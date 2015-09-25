d148cfdf908ff7a866117d5d09c6d0717200f54b
Darko <dkrisan@gmail.com>
2015-09-25 11:48:29 +0200

    Fix link to animate.min.css in Charisma.

--- www/templates/charisma/js/functions.js
+++ www/templates/charisma/js/functions.js
@@ -1,4 +1,4 @@
-$('head').append('<link rel="stylesheet" href="templates/omicron/dist/css/animate.css" type="text/css" />');
+$('head').append('<link rel="stylesheet" href="templates/charisma/css/animate.min.css" type="text/css" />');
 
 // event bindings
 jQuery(function($){
