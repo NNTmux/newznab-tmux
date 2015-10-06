b96532d503693a54378aa0d7e8291d90c28aa93f
Darko <dkrisan@gmail.com>
2015-10-06 08:49:29 +0200

    Update the updateTvRage script and TmuxRun.

--- newznab/controllers/TmuxRun.php
+++ newznab/controllers/TmuxRun.php
@@ -475,7 +475,7 @@ class TmuxRun extends Tmux
 						{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}update_scripts/update_theaters.php $log; \
 						{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/populate_tvrage.php true $log; \
 						{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}update_scripts/update_tvschedule.php $log; \
-						{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/updateTvRage.php $log; date +\"{$this->_dateFormat}\"' 2>&1 1> /dev/null"
+						{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/updateTvRage.php update $log; date +\"{$this->_dateFormat}\"' 2>&1 1> /dev/null"
 				);
 				$runVar['timers']['timer4'] = time();
 				break;
@@ -500,7 +500,7 @@ class TmuxRun extends Tmux
 				{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}update_scripts/update_theaters.php $log; \
 				{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/populate_tvrage.php true $log; \
 				{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}update_scripts/update_tvschedule.php $log; \
-				{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/updateTvRage.php $log; date +\"{$this->_dateFormat}\"' 2>&1 1> /dev/null"
+				{$runVar['commands']['_phpn']} {$runVar['paths']['misc']}testing/PostProc/updateTvRage.php update $log; date +\"{$this->_dateFormat}\"' 2>&1 1> /dev/null"
 			);
 			$runVar['timers']['timer4'] = time();
 
