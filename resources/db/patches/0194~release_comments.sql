dfba911fd5c503582a3844f73bea33e1341e936f
DariusIII <dkrisan@gmail.com>
2015-10-13 02:09:21 +0200

    Invert the order in patch 194.

--- resources/db/patches/0194~release_comments.sql
+++ resources/db/patches/0194~release_comments.sql
@@ -1,11 +1,11 @@
 # This patch will remove column and index added by patch 383
 
-#Drop the text_hash column
-ALTER TABLE release_comments DROP text_hash;
-
 #Drop the trigger
 DROP TRIGGER IF EXISTS insert_MD5:
 
 #Drop the indexes
 ALTER TABLE release_comments DROP INDEX ix_release_comments_hash_releaseID;
 ALTER TABLE release_comments DROP INDEX ix_release_hash_gid;
+
+#Drop the text_hash column
+ALTER TABLE release_comments DROP text_hash;
