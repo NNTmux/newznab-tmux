98b951c28888e3d4dfaa174cd02c271c4eb26cb1
DariusIII <thunderstorm1977>
2015-10-20 19:22:46 +0200

    Update patch 83.

--- resources/db/patches/mysql/0083~site.sql
+++ resources/db/patches/mysql/0083~site.sql
@@ -2,4 +2,4 @@ INSERT IGNORE INTO settings (setting, value) VALUES
 ('lookup_reqids',	'1'),
 ('reqidthreads', '1'),
 ('request_hours', '1'),
-('request_url',	'http://reqid.nzedb.com/index.php');
+('request_url',	'http://reqid.newznab-tmux.pw/index.php');
