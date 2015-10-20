540ec1e1e8b0b2d18487a5c2178cca39a30f63bc
DariusIII <thunderstorm1977>
2015-10-20 19:25:46 +0200

    Ušdate patch 56

--- resources/db/patches/mysql/0056~menu.sql
+++ resources/db/patches/mysql/0056~menu.sql
@@ -1,1 +1,1 @@
-INSERT INTO menu (href, title, tooltip, role, ordinal ) VALUES ('newposterwall', 'New Releases', "Newest Releases Poster Wall", 1, 11);
+INSERT IGNORE INTO menu (href, title, tooltip, role, ordinal ) VALUES ('newposterwall', 'New Releases', "Newest Releases Poster Wall", 1, 11);
