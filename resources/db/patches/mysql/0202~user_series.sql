d5c822e3902250f541711a4e2d68724f662659cf
DariusIII <dkrisan@gmail.com>
2015-11-04 09:59:36 +0100

    Fix patch 202

--- resources/db/patches/mysql/0202~user_series.sql
+++ resources/db/patches/mysql/0202~user_series.sql
@@ -1,13 +1,22 @@
 # This patch will make the necessary changes to the user_series
 # table to support the new videos implementation
-# At this time, we are not sure if we can preserve the users shows
+# At this time, we're not sure if we can preserve the users shows
 # data as the tvrage data is largely useless due to the title insert bug
 
-# Truncate for now
-TRUNCATE TABLE userseries;
+# Drop the old user_series table
+DROP TABLE IF EXISTS userseries;
 
-# Change rageid column to new videos_id
-ALTER TABLE userseries
-  DROP INDEX ix_userseries_userid,
-  CHANGE COLUMN rageid videos_id INT(16) NOT NULL COMMENT 'FK to videos.id',
-  ADD INDEX ix_userseries_videos_id (userid, videos_id);
+# Create userseries table with new schema
+CREATE TABLE userseries (
+  id          INT(16) UNSIGNED NOT NULL AUTO_INCREMENT,
+  userid     INT(16)          NOT NULL,
+  videos_id   INT(16)          NOT NULL COMMENT 'FK to videos.id',
+  categoryid  VARCHAR(64)      NULL DEFAULT NULL,
+  createddate DATETIME         NOT NULL,
+  PRIMARY KEY (id),
+  INDEX ix_userseries_videos_id (userid, videos_id)
+)
+  ENGINE = MYISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci
+  AUTO_INCREMENT = 1;
