d105e2681e9e174b14dd7552ed409c98bafc4d87
DariusIII <dkrisan@gmail.com>
2015-11-18 14:20:26 +0100

    Remove newzdash from tmux

--- newzdash/includes/bottombar.php
+++
@@ -1,6 +0,0 @@
-<!-- bottombar starts -->
-<footer>
-	<p class="pull-left">&copy; <a href="https://github.com/DariusIII/newzdash" target="_blank">DariusIII</a> 2015</p>
-	<p class="pull-right">Powered by: NewzDash</p>
-</footer>
-<!-- bottombar ends -->
