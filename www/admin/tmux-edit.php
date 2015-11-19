76723bc2434244aa09b634c9be58193731e45b23
DariusIII <dkrisan@gmail.com>
2015-11-19 10:47:57 +0100

    Remove option to postprocess properly renamed releases, useless option that caused too much problems

--- www/admin/tmux-edit.php
+++ www/admin/tmux-edit.php
@@ -54,8 +54,6 @@ $page->smarty->assign('sequential_ids', array(0,1,2));
 $page->smarty->assign('sequential_names', array('Disabled', 'Basic Sequential', 'Complete Sequential'));
 $page->smarty->assign('binaries_ids', array(0,1,2));
 $page->smarty->assign('binaries_names', array('Disabled', 'Simple Threaded Update', 'Complete Threaded Update'));
-$page->smarty->assign('post_non_ids', array(0,1,2));
-$page->smarty->assign('post_non_names', array('Disabled', 'All Available Releases', 'Properly Renamed Releases'));
 $page->smarty->assign('lookup_reqids_ids', array(0,1,2));
 $page->smarty->assign('lookup_reqids_names', array('Disabled', 'Lookup Request IDs', 'Lookup Request IDs Threaded'));
 $page->smarty->assign('predb_ids', array(0,1));
