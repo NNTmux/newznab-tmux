db405cf9dbe7e6a18ab80da788b9da0df3b6f748
Rick <RickDB@gmail.com>
2015-11-23 12:26:34 +0100

    Added request id backfill script for AllFilled.

---
+++ misc/update_scripts/backfill_requestIDs.php
@@ -0,0 +1,100 @@
+<?php
+require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'indexer.php');
+
+use newznab\ConsoleTools;
+use newznab\NNTP;
+use newznab\db\Settings;
+use newznab\Categorize;
+use newznab\Releases;
+
+$consoletools = new ConsoleTools(['ColorCLI' => $pdo->log]);
+$releases = new Releases(['Settings' => $pdo, 'ConsoleTools' => $consoletools]);
+
+$pdo = new Settings();
+$cat = new Categorize(['Settings' => $pdo]);
+$releases = new Releases(['Settings' => $pdo]);
+
+// Backfill settings
+$group = 'alt.binaries.erotica';
+$limit = 200;
+$delay = 3;
+$outputOnly = true;
+
+
+echo $pdo->log->primary("Started backfill request id lookups (remote)\n");
+
+$sql = 	sprintf ('
+SELECT r.id, r.name, r.searchname, r.guid, r.fromname, r.reqid, r.groupid, g.name AS groupname, r.groupid, r.categoryid
+FROM releases r
+INNER JOIN groups g ON r.groupid = g.id
+WHERE r.nzbstatus = 1
+AND r.prehashid = 0
+AND r.isrequestid = 1
+AND r.isrenamed = 0
+%s
+ORDER BY r.postdate DESC
+LIMIT %d',
+'AND  g.name = ' .  $pdo->escapeString($group),
+$limit);
+			
+$releasesResults = $pdo->queryDirect($sql);	
+
+if ($releasesResults instanceof \Traversable) {
+	$totalProcessed = 0;
+	$totalReleases = $releasesResults->rowCount();
+	
+	echo $pdo->log->primary(sprintf(
+			"Release count to process: %s",
+			$totalReleases
+		)
+	);
+	
+	foreach($releasesResults as $release) {
+		$reqid = $release['reqid'];
+		$newtitle = $releases->getReleaseNameForReqId($pdo->getSetting('reqidurl'), $pdo->getSetting('newznabID'), 'alt.binaries.erotica', $release['reqid']);			
+		
+		if(empty($newtitle))
+		{
+			$newtitle = $releases->getReleaseNameForReqId($pdo->getSetting('reqidurl'), $pdo->getSetting('newznabID'), 'alt.binaries.erotica', $release['name']);
+		}
+		
+		if(!empty($newtitle) && $newtitle != 'no feed')
+		{
+			$catId = $cat->determineCategory($release['groupid'], $newtitle);
+			$query = sprintf("UPDATE releases SET searchname = %s, categoryID = %s, isrenamed = 1 WHERE guid = %s", $pdo->escapeString($newtitle), $catId, $pdo->escapeString($release['guid']));
+			
+			if($outputOnly == false)
+			{
+				$pdo->queryExec($query);
+		
+				echo $pdo->log->primary(sprintf(
+						"Updated release: \nOriginal title: %s\nNew title: %s\nNew category: %s\nGUID: %s\n",
+						$release['name'], $newtitle, $catId, $release['guid']
+					)
+				);
+			}
+			else
+			{
+				echo $pdo->log->primary(sprintf(
+						"[Output only] Would have updated release: \nOriginal title: %s\nNew title: %s\nNew category: %s\nGUID: %s\n",
+						$release['name'], $newtitle, $catId, $release['guid']
+					)
+				);
+			}	
+		}
+		
+		$totalProcessed++;
+		
+		echo $pdo->log->primary(sprintf(
+				"Total processed: %s / %s",
+				$totalProcessed, $totalReleases
+			)
+		);
+			
+		$newtitle = '';
+		$requestId  = '';
+		sleep($delay);
+	}
+}
+
+echo $pdo->log->primary("Completed backfill request id lookups (remote)\n");
