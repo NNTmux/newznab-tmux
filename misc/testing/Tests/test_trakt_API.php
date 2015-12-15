1cc3e2b52a1caee28e2549f4a374f553317e8002
DariusIII <dkrisan@gmail.com>
2015-12-15 14:25:07 +0100

    Add API testing scripts

---
+++ misc/testing/Tests/test_trakt_API.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once realpath(dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'indexer.php');
+
+use newznab\processing\tv\TraktTv;
+
+$c = new newznab\ColorCLI();
+$trakt = new TraktTv();
+
+if (!empty($argv[1]) && is_numeric($argv[2]) && is_numeric($argv[3])) {
+
+	// Test if your Trakt API key and configuration are working
+	// If it works you should get a printed array of the show/season/episode entered
+
+	// Search for a show
+	$series = $trakt->client->showSearch((string)$argv[1], 'show');
+
+	// Use the first show found (highest match) and get the requested season/episode from $argv
+	if (is_array($series)) {
+
+		$series = $trakt->client->showSummary($series[0]['show']['ids']['trakt'], 'full,images');
+		$episode = $trakt->client->episodeSummary($series['ids']['trakt'], (int)$argv[2], (int)$argv[3], 'full');
+
+		print_r($series);
+		print_r($episode);
+
+	} else {
+		exit($c->error("Error retrieving Trakt data."));
+	}
+} else {
+	exit($c->error("Invalid arguments.  This script requires a text string (show name) followed by a season and episode number."));
+}
