17a3948601fbfc34c26a63feeea590f64ef525ce
DariusIII <dkrisan@gmail.com>
2015-12-15 11:48:38 +0100

    Drop tvrage, tvrage_episodes and tvrage_tables if they still exist in DB

---
+++ resources/db/patches/mysql/0216~tvrage.sql
@@ -0,0 +1,11 @@
+#Drop tvrage, tvrage_episodes and tvrage_titles tables if they exist.
+
+#Drop tvrage table
+DROP TABLE IF EXISTS tvrage;
+
+#Drop tvrage_episodes table
+DROP TABLE IF EXISTS tvrage_episodes;
+
+#Drop tvrage_titles table
+DROP TABLE IF EXISTS tvrage_titles;
+
