285a7f4052a2165053b9a945678012aa4d27258d
DariusIII <dkrisan@gmail.com>
2016-01-18 10:28:31 +0100

    Remove + files update nntmux_fi_schema.

--- resources/db/schema/nntmux_fi_schema.sql
+++ resources/db/schema/nntmux_fi_schema.sql
@@ -4068,7 +4068,7 @@ INSERT INTO settings (setting, value, section, subsection, name, hint) VALUES
 ('spotnabsiteprvkey', '', '', '', 'spotnabsiteprvkey', ''),
 ('spotnabsitepubkey', '', '', '', 'spotnabsitepubkey', ''),
 ('spotnabuser', '', '', '', 'spotnabuser', ''),
-('sqlpatch', '235', '', '', 'sqlpatch', ''),
+('sqlpatch', '237', '', '', 'sqlpatch', ''),
 ('storeuserips', '0', '', '', 'storeuserips', ''),
 ('strapline', 'A great usenet indexer','', '', 'strapline', ''),
 ('style', 'Omicron', '', '', 'style', ''),
