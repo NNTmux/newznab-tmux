5c115e0f5cc87e67f91819706621e17def18cc33
DariusIII <dkrisan@gmail.com>
2016-01-18 10:48:58 +0100

    remove the + files

--- resources/db/patches/mysql/+1~category.sql
+++
@@ -1,4 +0,0 @@
-#Update category table with new values for Other Misc and Hashed
-UPDATE category SET id = 0000 WHERE id = 8000;
-UPDATE category SET id = 0010 WHERE id = 8010;
-UPDATE category SET id = 0020 WHERE id = 8020;
