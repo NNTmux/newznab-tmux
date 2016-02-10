6df27c5a8e01fa554667b78c61c154054a801e52
DariusIII <dkrisan@gmail.com>
2016-02-10 14:16:14 +0100

    Add the patches

---
+++ resources/db/patches/mysql/+2~category.sql
@@ -0,0 +1,6 @@
+#Update category table with new values for Other categories
+UPDATE category SET id = 1999 WHERE id = 1090;
+UPDATE category SET id = 2999 WHERE id = 2020;
+UPDATE category SET id = 3999 WHERE id = 3050;
+UPDATE category SET id = 5999 WHERE id = 5050;
+UPDATE category SET id = 6999 WHERE id = 6070;
