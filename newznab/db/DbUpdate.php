19d1964fd61e1d7cf3126cac73f0e52b182d0234
DariusIII <dkrisan@gmail.com>
2016-05-05 10:04:15 +0200

    Reinstate the LINES TERMINATED BY clause to loading data files to override default behaviour of only using '\n' instead of '\r\n' that we need.

--- newznab/db/DbUpdate.php
+++ newznab/db/DbUpdate.php
@@ -98,7 +98,8 @@ class DbUpdate
 		$files = empty($options['files']) ? Utility::getDirFiles($options) : $options['files'];
 		natsort($files);
 		$local = $this->pdo->isLocalDb() ? '' : 'LOCAL ';
-		$sql = 'LOAD DATA ' . $local . 'INFILE "%s" IGNORE INTO TABLE `%s` FIELDS TERMINATED BY "\t" OPTIONALLY ENCLOSED BY "\"" IGNORE 1 LINES (%s)';
+		$sql = 'LOAD DATA ' .
+			$local . 'INFILE "%s" IGNORE INTO TABLE `%s` FIELDS TERMINATED BY "\t" OPTIONALLY ENCLOSED BY "\"" LINES TERMINATED BY "\r\n" IGNORE 1 LINES (%s)';
 		foreach ($files as $file) {
 			if ($show === true) {
 				echo "File: $file\n";
