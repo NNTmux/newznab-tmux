59e2e50d8e54f3bdbb66a2b709a15c09efb4ccac
DariusIII <thunderstorm1977>
2016-05-11 00:14:15 +0200

    Merge branch feature-db to dev

--- resources/db/patches/mysql/0253~predb_hashes.sql
+++ resources/db/patches/mysql/0253~predb_hashes.sql
@@ -1,6 +1,6 @@
-# Rename predb_id to predb_id in line with lithium conventions.
+# Rename pre_id to predb_id in line with lithium conventions.
 ALTER TABLE predb_hashes
-CHANGE COLUMN predb_id predb_id INT(11) UNSIGNED NOT NULL
+CHANGE COLUMN pre_id predb_id INT(11) UNSIGNED NOT NULL
 COMMENT 'id, of the predb entry, this hash belongs to';
 
 DROP TRIGGER IF EXISTS insert_hashes;
