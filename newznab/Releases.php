8bdddde63ab3256bf5d54d7c7db6f9d0e48e20d4
DariusIII <dkrisan@gmail.com>
2016-06-06 09:19:13 +0200

    TV fallback searches not episode specific / fix Season Pack lookups

--- newznab/Releases.php
+++ newznab/Releases.php
@@ -1010,6 +1010,10 @@ class Releases
 					$showSql = sprintf('AND r.tv_episodes_id IN (%s)', $show['episodes']);
 				} else if ((int)$show['video'] > 0) {
 					$showSql = 'AND r.videos_id = ' . $show['video'];
+					// If $series is set but episode is not, return Season Packs only
+					if (!empty($series) && empty($episode)) {
+						$showSql .= ' AND r.tv_episodes_id = 0';
+					}
 				} else {
 					// If we were passed Episode Info and no match was found, do not run the query
 					return array();
@@ -1017,6 +1021,18 @@ class Releases
 			} else {
 				// If we were passed Site ID Info and no match was found, do not run the query
 				return array();
+			}
+		}
+
+		// If $name is set it is a fallback search, add available SxxExx/airdate info to the query
+		if (!empty($name) && $showSql === '') {
+			if (!empty($series) && (int)$series < 1900) {
+				$name .= sprintf(' S%s', str_pad($series, 2, '0', STR_PAD_LEFT));
+				if (!empty($episode)  && strpos($episode, '/') === false) {
+					$name .= sprintf('E%s', str_pad($episode, 2, '0', STR_PAD_LEFT));
+				}
+			} else if (!empty($airdate)) {
+				$name .= sprintf(' %s', str_replace(['/', '-', '.', '_'], ' ', $airdate));
 			}
 		}
 
@@ -1075,7 +1091,6 @@ class Releases
 		$releases = $this->pdo->query($sql, true, NN_CACHE_EXPIRY_MEDIUM);
 		return $releases;
 	}
-
 
 	/**
 	 * @param        $aniDbID
