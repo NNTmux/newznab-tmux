a6a41d279087fb27e693a38b59a6f7faa1b7b69b
DariusIII <thunderstorm1977>
2016-06-20 21:21:49 +0200

    SQL injection fix in Movie.php @pkostiuk

--- newznab/Movie.php
+++ newznab/Movie.php
@@ -405,7 +405,7 @@ class Movie
 					$bbv .= '.';
 				}
 				if ($bb === 'imdb') {
-					$browseBy .= 'AND m.' . $bb . 'id = ' . $bbv;
+					$browseBy .= sprintf('AND m.%sid = %d', $bb, $bbv);
 				} else {
 					$browseBy .= 'AND m.' . $bb . ' ' . $this->pdo->likeString($bbv, true, true);
 				}
