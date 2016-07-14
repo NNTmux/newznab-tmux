8a32b3799788a44a561b540a4f8d586c726aedc2
DariusIII <dkrisan@gmail.com>
2016-07-14 08:48:42 +0200

    Remove now unused files

--- initialise.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'SPLClassLoader.php';
-require_once 'constants.php';
-
-$classLoader = new SplClassLoader('nntmux', [__DIR__ . DIRECTORY_SEPARATOR . 'nntmux']);
-$classLoader->register();
-
-?>
