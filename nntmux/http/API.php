053b1ad5d74f0aa83c9d067a9d0792e62fc158b9
DariusIII <dkrisan@gmail.com>
2016-07-18 13:58:57 +0200

    API search now respects group parameter for searches

--- nntmux/http/API.php
+++ nntmux/http/API.php
@@ -23,6 +23,7 @@ namespace nntmux\http;
 use nntmux\db\Settings;
 use nntmux\utility\Utility;
 use nntmux\Category;
+use nntmux\Groups;
 
 /**
  * Class API
@@ -116,6 +117,22 @@ class API extends Capabilities {
 			$categoryID = explode(',', $categoryIDs);
 		}
 		return $categoryID;
+	}
+
+	/**
+	 * Verify groupName parameter.
+	 * @return mixed
+	 */
+	public function group()
+	{
+		$groupName = -1;
+		if (isset($this->getRequest['group'])) {
+			$group = (new Groups())->isValidGroup($this->getRequest['group']);
+			if ($group !== false){
+				$groupName = $group;
+			}
+		}
+		return $groupName;
 	}
 
 	/**
