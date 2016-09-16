94626d6a0d185aa2988eb406ef7378924f1b6611
DariusIII <dkrisan@gmail.com>
2016-09-16 14:25:21 +0200

    Update PHP DocBlock in Capabilites related to offset

--- nntmux/http/Capabilities.php
+++ nntmux/http/Capabilities.php
@@ -63,7 +63,7 @@ abstract class Capabilities
 	 * @param array  $data   Data to print.
 	 * @param array  $params Additional request parameters
 	 * @param bool   $xml    True: Print as XML False: Print as JSON.
-	 * @param        $offset
+	 * @param int    $offset How much releases to skip
 	 * @param string $type   What type of API query to format if XML
 	 */
 	public function output($data, $params, $xml = true, $offset, $type = '')
