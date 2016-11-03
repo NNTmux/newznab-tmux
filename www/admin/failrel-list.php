0abe6d85b62a81b1e859ac584ebdcf227ff578e6
DariusIII <dkrisan@gmail.com>
2016-11-03 14:21:35 +0100

    Fix notices in failed releases list

--- www/admin/failrel-list.php
+++ www/admin/failrel-list.php
@@ -14,10 +14,14 @@ $page->title = "Failed Releases List";
 $frelcount = $failed->getCount();
 
 $offset = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$page->smarty->assign('pagertotalitems',$frelcount);
-$page->smarty->assign('pageroffset',$offset);
-$page->smarty->assign('pageritemsperpage',ITEMS_PER_PAGE);
-$page->smarty->assign('pagerquerybase', WWW_TOP."/failrel-list.php?offset=");
+$page->smarty->assign([
+	'pagertotalitems'   => $frelcount,
+	'pagerquerysuffix'  => "#results",
+	'pageroffset'       => $offset,
+	'pageritemsperpage' => ITEMS_PER_PAGE,
+	'pagerquerybase'    => WWW_TOP."/failrel-list.php?offset=",
+	]
+);
 $pager = $page->smarty->fetch("pager.tpl");
 $page->smarty->assign('pager', $pager);
 
