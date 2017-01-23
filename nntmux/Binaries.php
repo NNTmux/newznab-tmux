b51c2b11748242e2dd0a694bbc15f51cdbd189d1
DariusIII <dkrisan@gmail.com>
2017-01-23 14:26:31 +0100

    Revert to original setting for binaries update, ratehr have wrong size reported than have no releases

--- nntmux/Binaries.php
+++ nntmux/Binaries.php
@@ -793,12 +793,11 @@ class Binaries
 				$table = $multiGroup === true ? 'multigroup_binaries' : $tableNames['bname'];
 				$hash = $multiGroup === true ? md5($matches[1] . $header['From']) :
 					md5($matches[1] . $header['From'] . $groupMySQL['id']);
-				$onDuplicate = $multiGroup === true ? sprintf('ON DUPLICATE KEY UPDATE currentparts = CASE WHEN currentparts < totalparts * 1.1 THEN currentparts + 1 ELSE currentparts END, partsize = CASE WHEN currentparts < totalparts * 1.1 THEN partsize + %d ELSE partsize END', $header['Bytes']) : sprintf('ON DUPLICATE KEY UPDATE currentparts = currentparts + 1, partsize = partsize + %d', $header['Bytes']);
 				$binaryID = $this->_pdo->queryInsert(
 					sprintf("
 						INSERT INTO %s (binaryhash, name, collections_id, totalparts, currentparts, filenumber, partsize)
 						VALUES (UNHEX('%s'), %s, %d, %d, 1, %d, %d)
-						%s",
+						ON DUPLICATE KEY UPDATE currentparts = currentparts + 1, partsize = partsize + %d",
 						$table,
 						$hash,
 						$this->_pdo->escapeString(utf8_encode($matches[1])),
@@ -806,7 +805,7 @@ class Binaries
 						$matches[3],
 						$fileCount[1],
 						$header['Bytes'],
-						$onDuplicate
+						$header['Bytes']
 					)
 				);
 
