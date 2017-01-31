a79c3b83a43f709123a2f5fd1889bd9e52859562
DariusIII <dkrisan@gmail.com>
2017-01-31 21:41:41 +0100

    Update mysql-ddl

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -1429,30 +1429,30 @@ CREATE PROCEDURE loop_cbpm(IN method CHAR(10))
     main: BEGIN
     DECLARE done INT DEFAULT 0;
     DECLARE tname VARCHAR(255) DEFAULT '';
+    DECLARE regstr VARCHAR(255) CHARSET utf8 COLLATE utf8_general_ci DEFAULT '';
+
     DECLARE cur1 CURSOR FOR
       SELECT TABLE_NAME
       FROM information_schema.TABLES
       WHERE
         TABLE_SCHEMA = (SELECT DATABASE())
-        AND
-        (
-          TABLE_NAME LIKE 'collections%'
-          OR TABLE_NAME LIKE 'parts%'
-          OR TABLE_NAME LIKE 'binaries%'
-          OR TABLE_NAME LIKE 'missed%'
-        )
+        AND TABLE_NAME REGEXP regstr
       ORDER BY TABLE_NAME ASC;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
     IF method NOT IN ('repair', 'analyze', 'optimize', 'drop', 'truncate')
     THEN LEAVE main; END IF;
 
+    IF method = 'drop' THEN SET regstr = '^(collections|binaries|parts|missed_parts)_[0-9]+$';
+    ELSE SET regstr = '^(multigroup_)?(collections|binaries|parts|missed_parts)(_[0-9]+)?$';
+    END IF;
+
     OPEN cur1;
-    alter_loop: LOOP FETCH cur1
+    cbpm_loop: LOOP FETCH cur1
     INTO tname;
       IF done
-      THEN LEAVE alter_loop; END IF;
-      SET @SQL := CONCAT(method, " TABLE ", tname);
+      THEN LEAVE cbpm_loop; END IF;
+      SET @SQL := CONCAT(method, ' TABLE ', tname);
       PREPARE _stmt FROM @SQL;
       EXECUTE _stmt;
       DEALLOCATE PREPARE _stmt;
