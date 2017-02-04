fa91ec903b752b99c8a411d54e17b9714ace528d
DariusIII <dkrisan@gmail.com>
2017-02-04 13:33:45 +0100

    Update the patch

--- resources/db/patches/mysql/+1~steam_apps.sql
+++ resources/db/patches/mysql/+1~steam_apps.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS steam_apps;
 CREATE TABLE steam_apps (
-  name         VARCHAR(191)        NOT NULL DEFAULT '' COMMENT 'Steam application name',
+  name         VARCHAR(255)        NOT NULL DEFAULT '' COMMENT 'Steam application name',
   appid        INT(11) UNSIGNED    NULL COMMENT 'Steam application id'
 )
   ENGINE          = MYISAM
