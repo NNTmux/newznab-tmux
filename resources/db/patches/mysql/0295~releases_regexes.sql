f4d366d8dcf979ac56392c5c9b2a638735eedebc
DariusIII <dkrisan@gmail.com>
2017-02-14 22:33:18 +0100

    Fix typo in patch

--- resources/db/patches/mysql/0295~releases_regexes.sql
+++ resources/db/patches/mysql/0295~releases_regexes.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS release_regexes;
 CREATE TABLE releases_regexes (
   releases_id           INT(11) UNSIGNED    NOT NULL DEFAULT '0',
   collection_regex_id   INT(11) UNSIGNED    NOT NULL DEFAULT '0',
-  naming_regex_id       INT(11) UNSIGNED    NOT NULL DEFAULT '0'
+  naming_regex_id       INT(11) UNSIGNED    NOT NULL DEFAULT '0',
   PRIMARY KEY (releases_id, collection_regex_id, naming_regex_id)
 )
   ENGINE          = MYISAM
