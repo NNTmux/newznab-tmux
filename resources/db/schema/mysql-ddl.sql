f4a1b3dc69e535dd571373b1075e0a9db85c066f
DariusIII <dkrisan@gmail.com>
2017-03-02 11:02:23 +0100

    Update mysql-ddl.sql

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -598,6 +598,16 @@ CREATE TABLE content (
   AUTO_INCREMENT = 1000001;
 
 
+DROP TABLE IF EXISTS par_hashes;
+CREATE TABLE par_hashes (
+  releases_id INT(11) NOT NULL COMMENT 'FK to releases.id',
+  hash VARCHAR(32) NOT NULL COMMENT 'hash_16k block of par2',
+  PRIMARY KEY (releases_id, hash)
+)
+  ENGINE = MYISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
 DROP TABLE IF EXISTS parts;
 CREATE TABLE parts (
   binaries_id      BIGINT(20) UNSIGNED                      NOT NULL DEFAULT '0',
