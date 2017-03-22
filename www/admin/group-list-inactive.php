8316961a5542b3323d49a8cf41c99f6226e9ca1b
DariusIII <dkrisan@gmail.com>
2017-03-22 09:17:07 +0100

    Add pagerquerysuffix to group list pages

--- www/admin/group-list-inactive.php
+++ www/admin/group-list-inactive.php
@@ -21,6 +21,7 @@ $page->smarty->assign('groupname',$groupname);
 $page->smarty->assign('pagertotalitems',$groupcount);
 $page->smarty->assign('pageroffset',$offset);
 $page->smarty->assign('pageritemsperpage',ITEMS_PER_PAGE);
+$page->smarty->assign('pagerquerysuffix', '#results');
 
 $groupsearch = $gname != '' ? 'groupname='.$gname.'&amp;' : '';
 $page->smarty->assign('pagerquerybase', WWW_TOP.'/group-list-inactive.php?'.$groupsearch.'offset=');
