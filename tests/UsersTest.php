35f64a15f2b727f27bacbd2d29eababf5bb2e175
DariusIII <dkrisan@gmail.com>
2017-04-26 13:48:30 +0200

    Change test suite for travis, add phpunit tests

---
+++ tests/UsersTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of NNTmux.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package NNTmux
+ * @author DariusIII
+ * @copyright (c) 2017, NNTmux
+ * @version 0.0.1
+ */
+
+namespace tests;
+
+use nntmux\db\DB;
+use nntmux\Users;
+
+class UsersTest extends \PHPUnit_Framework_TestCase
+{
+	const USERNAME = 'test';
+	const PASSWORD = '1234567';
+	const EMAIL = 'example@example.com';
+
+	public function testUsersAdd() {
+		$pdo = new DB();
+		$users = new Users();
+		$users->add(self::USERNAME, self::PASSWORD, self::EMAIL, '2', '', '');
+		$userTest = $users->getByEmail(self::EMAIL);
+		$this->assertEquals(self::EMAIL, $userTest['email']);
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+// Grab our autoloader
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app/config/bootstrap.php';
