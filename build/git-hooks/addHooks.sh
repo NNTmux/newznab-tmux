1c7c0efd49cf8121d2e6bfa4623acaf9ba1c92d7
DariusIII <dkrisan@gmail.com>
2017-05-05 11:36:48 +0200

    Update addHooks.sh

--- build/git-hooks/addHooks.sh
+++ build/git-hooks/addHooks.sh
@@ -4,6 +4,7 @@ NNTMUX=`pwd`
 HOOKS=/build/git-hooks
 GIT=/.git/hooks
 PC=/pre-commit
+WSR=/white-space-removal
 
 NNTMUX=${NNTMUX%${HOOKS}}
 
@@ -13,5 +14,11 @@ then
 	rm "${NNTMUX}${GIT}${PC}"
 	echo .
 fi
+if [ -x "${NNTMUX}${GIT}${WSR}" ]
+then
+	rm "${NNTMUX}${GIT}${WSR}"
+	echo .
+fi
 
 ln -s ${NNTMUX}${HOOKS}${PC} ${NNTMUX}${GIT}${PC}
+ln -s ${NNTMUX}${HOOKS}${WSR} ${NNTMUX}${GIT}${WSR}
