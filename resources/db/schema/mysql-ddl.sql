f85baf623b059e83ff5d3bb690d3a69af9738146
DariusIII <dkrisan@gmail.com>
2017-05-15 15:17:24 +0200

    Alter columns in release_regexes tables to allow signed values for collection_regexes_id and naming_regexes_id

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -836,8 +836,8 @@ CREATE TABLE releases_groups (
 DROP TABLE IF EXISTS release_regexes;
 CREATE TABLE release_regexes (
   releases_id           INT(11) UNSIGNED    NOT NULL DEFAULT '0',
-  collection_regex_id   INT(11) UNSIGNED    NOT NULL DEFAULT '0',
-  naming_regex_id       INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  collection_regex_id   INT(11) NOT NULL DEFAULT '0',
+  naming_regex_id       INT(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (releases_id, collection_regex_id, naming_regex_id)
 )
   ENGINE          = MYISAM
