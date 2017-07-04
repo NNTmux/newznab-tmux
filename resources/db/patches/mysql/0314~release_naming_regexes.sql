577147e4641c74c7c14ebd55442ef440d81b4269
Amir Zarrinkafsh <nightah@me.com>
2017-07-04 15:00:49 +1000

    Adjust ordinal in patch 314 to match 10-release_naming_regexes.tsv

--- resources/db/patches/mysql/0314~release_naming_regexes.sql
+++ resources/db/patches/mysql/0314~release_naming_regexes.sql
@@ -9,5 +9,5 @@ VALUES (
   '/^\\[?AFL.+\\" ?[ .-]?(?P<match0>.+?) ?[ .](7z|avi|md5|mkv|mp4|nfo|nzb|par|rar|sfv|vol)t?\\d?+.+yEnc$/',
   1,
   '//[AFL 2017 Round 8 NMFC vs SYD 720p] - "AFL 2017 Round 8 NMFC vs SYD 720p par2" yEnc or AFL.2017.Round.8.ADEL.vs.MELB.480p] - "AFL.2017.Round.8.ADEL.vs.MELB.480p.sfv" yEnc',
-  1
+  5
 );
