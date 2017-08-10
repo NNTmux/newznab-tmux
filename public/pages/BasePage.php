8283cff56b4faf271a89e2bddc522ccacc35b31b
DariusIII <dkrisan@gmail.com>
2017-08-10 11:51:03 +0200

    Further improve maintenance mode

--- public/pages/BasePage.php
+++ public/pages/BasePage.php
@@ -306,53 +306,9 @@ class BasePage
 	 * Show maintenance page.
 	 *
 	 */
-	public function showMaintenance(): void
+	public function showMaintenance()
 	{
-		header('HTTP/1.1 503 Service Temporarily Unavailable');
-		die('
-				<html>
-					<head>
-        				<title>Be right back.</title>
-
-        				<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-       					 <style>
-            					html, body {
-                				height: 100%;
-            					}
-            					body {
-                						margin: 0;
-                						padding: 0;
-                						width: 100%;
-                						color: #B0BEC5;
-                						display: table;
-                						font-weight: 100;
-                						font-family: \'Lato\', sans-serif;
-            							}
-            					.container {
-               					text-align: center;
-                				display: table-cell;
-                				vertical-align: middle;
-            					}
-            					.content {
-                				text-align: center;
-                				display: inline-block;
-            					}
-            					.title {
-                				font-size: 72px;
-                				margin-bottom: 40px;
-            					}
-        				</style>
-    				</head>
-   					 <body>
-        				<div class="container">
-            				<div class="content">
-                				<div class="title">We are currently performing maintenance. Please try again later!</div>
-            				</div>
-       					</div>
-    				</body>
-				</html>'
-		);
+		die(response()->view('errors.503', [], 503));
 	}
 
 	/**
