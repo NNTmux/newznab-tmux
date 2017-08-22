678ec8ccb6770006861c8fcada36fc06d396d1c4
DariusIII <dkrisan@gmail.com>
2017-08-22 12:51:33 +0200

    Use RFC4122 formatted GUID in releases table

--- nntmux/Releases.php
+++ nntmux/Releases.php
@@ -121,7 +121,11 @@ class Releases
 	 */
 	public function createGUID(): string
 	{
-		return bin2hex(openssl_random_pseudo_bytes(20));
+		$data = openssl_random_pseudo_bytes(16);
+		$data[6] = chr(ord($data[6]) & 0x0f | 0x40);    // set version to 0100
+		$data[8] = chr(ord($data[8]) & 0x3f | 0x80);    // set bits 6-7 to 10
+		return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+
 	}
 
 	/**
