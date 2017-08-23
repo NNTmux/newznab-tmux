a3b0b0865362c41aa98a71d0db2d6e99cf4d007f
DariusIII <dkrisan@gmail.com>
2017-08-23 15:00:17 +0200

    Fix game-list.php too

--- public/admin/game-list.php
+++ public/admin/game-list.php
@@ -7,16 +7,21 @@ use nntmux\Games;
 $page = new AdminPage();
 $game = new Games(['Settings' => $page->settings]);
 
-$page->title = "Game List";
+$page->title = 'Game List';
 
-$gamecount = $game->getCount();
+$gameCount = $game->getCount();
 
-$offset = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$page->smarty->assign('pagertotalitems',$gamecount);
-$page->smarty->assign('pageroffset',$offset);
-$page->smarty->assign('pageritemsperpage',ITEMS_PER_PAGE);
-$page->smarty->assign('pagerquerybase', WWW_TOP."/game-list.php?offset=");
-$pager = $page->smarty->fetch("pager.tpl");
+$offset = $_REQUEST['offset'] ?? 0;
+
+$page->smarty->assign([
+	'pagertotalitems' => $gameCount,
+	'pagerquerysuffix'  => '#results',
+	'pageroffset' => $offset,
+	'pageritemsperpage' => ITEMS_PER_PAGE,
+	'pagerquerybase' => WWW_TOP. '/game-list.php?offset=',
+]);
+
+$pager = $page->smarty->fetch('pager.tpl');
 $page->smarty->assign('pager', $pager);
 
 $gamelist = $game->getRange($offset, ITEMS_PER_PAGE);
