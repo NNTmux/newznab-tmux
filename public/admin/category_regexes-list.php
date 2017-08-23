33fa8b8db48df5460961e2e15b4df55f5bcfea6f
DariusIII <dkrisan@gmail.com>
2017-08-23 15:22:44 +0200

    Fix category_regexes_list page

--- public/admin/category_regexes-list.php
+++ public/admin/category_regexes-list.php
@@ -7,21 +7,22 @@ use nntmux\Regexes;
 $page = new AdminPage();
 $regexes = new Regexes(['Settings' => $page->settings, 'Table_Name' => 'category_regexes']);
 
-$page->title = "Category Regex List";
+$page->title = 'Category Regex List';
 
 $group = ((isset($_REQUEST['group']) && !empty($_REQUEST['group'])) ? $_REQUEST['group'] : '');
-$offset = (isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0);
+$offset = ($_REQUEST['offset'] ?? 0);
 $regex = $regexes->getRegex($group, ITEMS_PER_PAGE, $offset);
 $count = $regexes->getCount($group);
 
 $page->smarty->assign([
 		'group'				=> $group,
 		'pagertotalitems'   => $count,
+		'pagerquerysuffix'  => '#results',
 		'pageroffset'       => $offset,
 		'pageritemsperpage' => ITEMS_PER_PAGE,
 		'regex'             => $regex,
-		'pagerquerybase'    => (WWW_TOP . "/category_regexes-list.php?" . $group . "offset="),
-		'pager'             => $page->smarty->fetch("pager.tpl")
+		'pagerquerybase'    => WWW_TOP . '/category_regexes-list.php?' . $group . 'offset=',
+		'pager'             => $page->smarty->fetch('pager.tpl')
 	]
 );
 
