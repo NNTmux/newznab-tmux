34094832945d9eccbcd63769a72013b9776cad7d
DariusIII <dkrisan@gmail.com>
2017-09-01 00:21:23 +0200

    Remove redundant line

--- public/smarty.php
+++ public/smarty.php
@@ -18,7 +18,6 @@
  * @author niel
  * @copyright 2015 NN
  */
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'nntmux'.DIRECTORY_SEPARATOR.'constants.php';
 require_once NN_ROOT.DS.'bootstrap'.DS.'autoload.php';
 
 use nntmux\config\Configure;
