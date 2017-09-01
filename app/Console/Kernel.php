3aa87f8f1e4032a599cd2933a438f9ed1b9f5094
DariusIII <dkrisan@gmail.com>
2017-09-01 13:41:17 +0200

    Update app/Console/Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,56 +2,41 @@
 
 namespace App\Console;
 
-use App\Console\Commands\TmuxUIStop;
-use App\Console\Commands\TmuxUIStart;
-use App\Console\Commands\UpdateNNTmux;
-use App\Console\Commands\UpdateNNTmuxDB;
-use App\Console\Commands\UpdateNNTmuxGit;
 use Illuminate\Console\Scheduling\Schedule;
-use App\Console\Commands\VerifyNNTmuxVersion;
-use App\Console\Commands\UpdateNNTmuxComposer;
-use App\Console\Commands\VerifyNNTmuxSettings;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        UpdateNNTmux::class,
-		UpdateNNTmuxDB::class,
-		UpdateNNTmuxGit::class,
-		UpdateNNTmuxComposer::class,
-		VerifyNNTmuxSettings::class,
-		VerifyNNTmuxVersion::class,
-		TmuxUIStart::class,
-		TmuxUIStop::class,
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		//
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')
-        //          ->hourly();
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		// $schedule->command('inspire')
+		//          ->hourly();
+	}
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
-    }
+		require base_path('routes/console.php');
+	}
 }
