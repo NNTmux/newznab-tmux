e6f87341de7d56dc3db8cfa46a572a9b4258d78a
DariusIII <dkrisan@gmail.com>
2017-09-04 13:04:27 +0200

    Use AnidbInfo model, update related migration

--- app/Models/AnidbInfo.php
+++ app/Models/AnidbInfo.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class AnidbInfo extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'anidbid';
+
+    /**
+     * @var bool
+     */
+    protected $dateFormat = false;
+
+    /**
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'anidbid',
+        'type',
+        'startdate',
+        'enddate',
+        'updated',
+        'related',
+        'similar',
+        'creators',
+        'description',
+        'rating',
+        'picture',
+        'categories',
+        'characters',
+    ];
 }
--- config/nntmux.php
+++ config/nntmux.php
@@ -11,16 +11,16 @@ $dotenv->load();
 $capsule = new Capsule;
 
 $capsule->addConnection([
-	'driver' => env('DB_SYSTEM'),
-	'host' => env('DB_HOST', 'localhost'),
-	'port' => env('DB_PORT', '3306'),
-	'database' => env('DB_NAME', 'nntmux'),
-	'username' => env('DB_USER', 'root'),
-	'password' => env('DB_PASSWORD', ''),
-	'unix_socket' => env('DB_SOCKET', ''),
-	'charset' => 'utf8',
-	'collation' => 'utf8_unicode_ci',
-	'strict' => false,
+    'driver' => env('DB_SYSTEM', 'mysql'),
+    'host' => env('DB_HOST', 'localhost'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_NAME', 'nntmux'),
+    'username' => env('DB_USER', 'root'),
+    'password' => env('DB_PASSWORD', ''),
+    'unix_socket' => env('DB_SOCKET', ''),
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'strict' => false,
 ]);
 
 $capsule->setAsGlobal();
