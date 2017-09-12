554d349fb76360ec11e1380fc36e91a8c70dd5a3
DariusIII <dkrisan@gmail.com>
2017-09-12 13:55:32 +0200

    Declare table name in ForumPost model

--- app/Models/Forumpost.php
+++ app/Models/Forumpost.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Forumpost extends Model
 {
     /**
+     * @var string
+     */
+    protected $table = 'forumpost';
+
+    /**
      * @var bool
      */
     protected $dateFormat = false;
