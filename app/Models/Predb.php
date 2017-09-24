d4e559aff283c77b78ad6e218b2f06470a47825c
DariusIII <dkrisan@gmail.com>
2017-09-25 00:18:43 +0200

    Update Predb and related migration

--- app/Models/Predb.php
+++ app/Models/Predb.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Predb extends Model
 {
-    //
+    public $timestamps = false;
+
+    protected $dateFormat = false;
+
+    protected $fillable = [
+        'id',
+        'title',
+        'nfo',
+        'size',
+        'category',
+        'predate',
+        'source',
+        'requestid',
+        'groups_id',
+        'nuked',
+        'nukereason',
+        'files',
+        'filename',
+        'searched'
+    ];
 }
--- database/migrations/2017_09_02_205236_create_predb_table.php
+++ database/migrations/2017_09_02_205236_create_predb_table.php
@@ -15,7 +15,26 @@ class CreatePredbTable extends Migration
     {
         Schema::create('predb', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+            $table->string('title');
+            $table->string('nfo');
+            $table->string('size');
+            $table->string('category');
+            $table->dateTime('predate');
+            $table->string('source', 50);
+            $table->unsignedInteger('request_id');
+            $table->unsignedInteger('groups_id');
+            $table->tinyInteger('nuked');
+            $table->string('nukereason');
+            $table->string('files');
+            $table->string('filename');
+            $table->tinyInteger('searched');
+            $table->unique('title', 'ix_predb_title');
+            $table->index('nfo', 'ix_predb_nfo');
+            $table->index('predate', 'ix_predb_predate');
+            $table->index('source', 'ix_predb_source');
+            $table->index(['request_id', 'groups_id'], 'ix_predb_request_group_id');
+            $table->index('filename');
+            $table->index('searched', 'ix_predb_search');
         });
     }
 
