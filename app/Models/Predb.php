52615f573a99730d2a3b1a4b00255e39e07b8de5
DariusIII <dkrisan@gmail.com>
2017-09-25 11:18:55 +0200

    Update Predb model with proper table name

--- app/Models/Predb.php
+++ app/Models/Predb.php
@@ -6,10 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Predb extends Model
 {
+    /**
+     * @var string
+     */
+    protected $table = 'predb';
+
+    /**
+     * @var bool
+     */
     public $timestamps = false;
 
+    /**
+     * @var bool
+     */
     protected $dateFormat = false;
 
+    /**
+     * @var array
+     */
     protected $fillable = [
         'id',
         'title',
