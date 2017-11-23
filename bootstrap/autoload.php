c8effb147be7eaa925a3d52e73c90536833fa181
DariusIII <dkrisan@gmail.com>
2017-11-23 13:35:03 +0100

    Remove bootstrap.php from nntmux folder and add its contents to bootstrap/autoload.php

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,10 +2,11 @@
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'nntmux'.DIRECTORY_SEPARATOR.'constants.php';
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'nntmux'.DIRECTORY_SEPARATOR.'bootstrap.php';
 require __DIR__.DIRECTORY_SEPARATOR.'app.php';
 
 use Dotenv\Dotenv;
+use nntmux\utility\Utility;
+use nntmux\config\Configure;
 
 $dotenv = new Dotenv(dirname(__DIR__, 1));
 $dotenv->load();
@@ -17,6 +18,12 @@ define('NN_APP_PATH', dirname(__DIR__).DS.'app');
 if (! defined('NN_ROOT')) {
     define('NN_ROOT', dirname(NN_APP_PATH, 2));
 }
+
+if (! defined('HAS_WHICH')) {
+    define('HAS_WHICH', Utility::hasWhich() ? true : false);
+}
+
+new Configure('indexer');
 
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 require_once __DIR__.DIRECTORY_SEPARATOR.'yenc.php';
