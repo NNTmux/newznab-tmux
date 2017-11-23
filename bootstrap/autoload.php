46e4169910b3cd9f44b08a897d22b6c28d410252
DariusIII <dkrisan@gmail.com>
2017-11-23 11:16:52 +0100

    Optimize autoloder

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -3,12 +3,9 @@
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'nntmux'.DIRECTORY_SEPARATOR.'constants.php';
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'nntmux'.DIRECTORY_SEPARATOR.'bootstrap.php';
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'nntmux.php';
 require __DIR__.DIRECTORY_SEPARATOR.'app.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'yenc.php';
 
 use Dotenv\Dotenv;
-use Illuminate\Contracts\Console\Kernel;
 
 $dotenv = new Dotenv(dirname(__DIR__, 1));
 $dotenv->load();
@@ -21,4 +18,5 @@ if (! defined('NN_ROOT')) {
     define('NN_ROOT', dirname(NN_APP_PATH, 2));
 }
 
-$app->make(Kernel::class)->bootstrap();
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+require_once __DIR__.DIRECTORY_SEPARATOR.'yenc.php';
