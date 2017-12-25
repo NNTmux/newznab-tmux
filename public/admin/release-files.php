6f75fd6264a86c8951de6d6a448a86c21d4beca8
DariusIII <dkrisan@gmail.com>
2017-12-25 23:15:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- public/admin/release-files.php
+++ public/admin/release-files.php
@@ -2,9 +2,9 @@
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'smarty.php';
 
-use App\Models\User;
 use nntmux\NZB;
 use nntmux\db\DB;
+use App\Models\User;
 use nntmux\Releases;
 
 $page = new AdminPage;
