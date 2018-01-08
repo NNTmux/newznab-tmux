f86cb2e1ebc8c5f1419b65a7f5d07fc734a57201
DariusIII <dkrisan@gmail.com>
2018-01-08 11:58:00 +0100

    Move constants.php to bootstrap folder, config folder was a bad place

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'constants.php';
+require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'bootstrap'.DIRECTORY_SEPARATOR.'constants.php';
 require __DIR__.DIRECTORY_SEPARATOR.'app.php';
 
 use Dotenv\Dotenv;
