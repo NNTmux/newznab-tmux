b989286e4c320f0586c73647f2c53b21aeed3693
DariusIII <dkrisan@gmail.com>
2018-01-17 11:48:04 +0100

    Update webpack.mix.js and package.json

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+let mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,4 +12,4 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css');
