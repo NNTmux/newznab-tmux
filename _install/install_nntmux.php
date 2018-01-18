a91cafc79a39be76bdd800da52584743bdd74293
DariusIII <dkrisan@gmail.com>
2018-01-18 11:26:29 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- _install/install_nntmux.php
+++ _install/install_nntmux.php
@@ -97,17 +97,15 @@ if (! $error) {
     );
     $pdo->exec('SET FOREIGN_KEY_CHECKS=0;');
 
-
     try {
         $DbSetup->processSQLFile(); // Setup default schema
         ColorCLI::doEcho(ColorCLI::header('Migrating tables and populating them'));
         passthru('php '.NN_ROOT.'artisan migrate');
         passthru('php '.NN_ROOT.'artisan db:seed');
-    } catch (\RuntimeException $err){
+    } catch (\RuntimeException $err) {
         $error = true;
-        ColorCLI::doEcho(ColorCLI::error('Error (' . $err->getMessage() . ')'));
+        ColorCLI::doEcho(ColorCLI::error('Error ('.$err->getMessage().')'));
     }
-
 
     if (! $error) {
         // Check one of the standard tables was created and has data.
