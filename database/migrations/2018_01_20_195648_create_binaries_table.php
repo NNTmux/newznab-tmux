a1c29d29d690327cbbe74907b5c4ace8b7c82301
DariusIII <dkrisan@gmail.com>
2018-01-22 10:54:11 +0100

    Update binaries table migration

--- database/migrations/2018_01_20_195648_create_binaries_table.php
+++ database/migrations/2018_01_20_195648_create_binaries_table.php
@@ -20,7 +20,7 @@ class CreateBinariesTable extends Migration {
 			$table->integer('filenumber')->unsigned()->default(0);
 			$table->integer('totalparts')->unsigned()->default(0);
 			$table->integer('currentparts')->unsigned()->default(0);
-			$table->binary('binaryhash', 16)->default('\'0$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$$UP$\'')->unique('ix_binaries_binaryhash');
+			$table->binary('binaryhash', 16)->default('0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0')->unique('ix_binaries_binaryhash');
 			$table->boolean('partcheck')->default(0)->index('ix_binaries_partcheck');
 			$table->bigInteger('partsize')->unsigned()->default(0);
 		});
