073cf28028bba3bb18f2fa487bd2a26f911d9391
DariusIII <dkrisan@gmail.com>
2018-03-05 22:38:33 +0100

    Move plugins and themes to resources folder

--- public/pages/BasePage.php
+++ public/pages/BasePage.php
@@ -8,6 +8,7 @@ use Blacklight\SABnzbd;
 use App\Models\Settings;
 use Illuminate\Support\Carbon;
 use App\Models\RoleExcludedCategory;
+use Ytake\LaravelSmarty\Smarty;
 
 class BasePage
 {
@@ -17,9 +18,9 @@ class BasePage
     public $settings = null;
 
     /**
-     * @var null|\Smarty
+     * @var \Ytake\LaravelSmarty\Smarty
      */
-    public $smarty = null;
+    public $smarty;
 
     public $title = '';
     public $content = '';
@@ -122,16 +123,6 @@ class BasePage
         $this->settings = new Settings();
         $this->pdo = new DB();
         $this->smarty = new Smarty();
-
-        $this->smarty->setCompileDir(NN_SMARTY_TEMPLATES);
-        $this->smarty->setConfigDir(NN_SMARTY_CONFIGS);
-        $this->smarty->setCacheDir(NN_SMARTY_CACHE);
-        $this->smarty->setPluginsDir(
-            [
-                NN_WWW.'plugins/',
-                SMARTY_DIR.'plugins/',
-            ]
-        );
         $this->smarty->error_reporting = E_ALL - E_NOTICE;
 
         $this->https = request()->secure();
