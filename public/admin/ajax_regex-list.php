8d5e0cacff60f5f184f6d4a436e119753216c2db
DariusIII <dkrisan@gmail.com>
2018-03-06 23:50:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- public/admin/ajax_regex-list.php
+++ public/admin/ajax_regex-list.php
@@ -4,7 +4,6 @@ require_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'resources/views/themes/sma
 
 use App\Models\ReleaseRegex;
 
-
 if (request()->has('action') && request()->input('action') === '2') {
     $id = (int) request()->input('regex_id');
     ReleaseRegex::query()->where('releases_id', $id);
