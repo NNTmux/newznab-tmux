172462b0ae965035ac619c886ac8673724844127
DariusIII <dkrisan@gmail.com>
2018-04-19 08:38:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Console/Commands/InstallNntmux.php
+++ app/Console/Commands/InstallNntmux.php
@@ -4,9 +4,9 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use App\Models\Settings;
+use Illuminate\Support\Carbon;
 use Illuminate\Console\Command;
 use App\Extensions\util\Versions;
-use Illuminate\Support\Carbon;
 use Symfony\Component\Process\Process;
 
 class InstallNntmux extends Command
@@ -123,7 +123,7 @@ class InstallNntmux extends Command
                 }
 
                 if (! $error && $this->addAdminUser()) {
-                    @file_put_contents(base_path().'/_install/install.lock', 'application install locked on ' . Carbon::now());
+                    @file_put_contents(base_path().'/_install/install.lock', 'application install locked on '.Carbon::now());
                     $this->info('Generating application key');
                     $process = new Process('php artisan key:generate --force');
                     $process->setTimeout(600);
