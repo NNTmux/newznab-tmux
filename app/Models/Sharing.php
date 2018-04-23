36da69ee9491cbe32333b9023069bbab038a128b
DariusIII <dkrisan@gmail.com>
2018-04-23 08:54:49 +0200

    Fix Sharing and SharingSite models definitions

--- app/Models/Sharing.php
+++ app/Models/Sharing.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sharing extends Model
 {
-    //
+    /**
+     * @var string
+     */
+    protected $table = 'sharing';
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'site_guid';
+
+    /**
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * @var bool
+     */
+    protected $dateFormat = false;
 }
