5e398bbb634b0117ade7abca6e8f5b1a6d98505a
DariusIII <dkrisan@gmail.com>
2018-04-27 07:00:10 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/GetNzbController.php
+++ app/Http/Controllers/GetNzbController.php
@@ -143,9 +143,10 @@ class GetNzbController extends BasePageController
         }
 
         $headers += ['X-DNZB-RCode:' => '200',
-            'X-DNZB-RText:' => 'OK, NZB content follows.'];
+            'X-DNZB-RText:' => 'OK, NZB content follows.', ];
 
-
-        return response()->streamDownload(function () use($nzbPath) {readgzfile($nzbPath);}, $cleanName, $headers);
+        return response()->streamDownload(function () use ($nzbPath) {
+            readgzfile($nzbPath);
+        }, $cleanName, $headers);
     }
 }
