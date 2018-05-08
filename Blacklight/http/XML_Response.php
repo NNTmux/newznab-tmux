547a1fd8c206fa39b626d1a985f9b87558f9a70d
DariusIII <dkrisan@gmail.com>
2018-05-08 10:22:22 +0200

    Revert totalrows change in XML_Response class

--- Blacklight/http/XML_Response.php
+++ Blacklight/http/XML_Response.php
@@ -347,7 +347,7 @@ class XML_Response
     {
         $this->xml->startElement($this->namespace.':response');
         $this->xml->writeAttribute('offset', $this->offset);
-        $this->xml->writeAttribute('total', $this->releases->total() ?? 0);
+        $this->xml->writeAttribute('total', $this->releases[0]['_totalrows'] ?? 0);
         $this->xml->endElement();
     }
 
