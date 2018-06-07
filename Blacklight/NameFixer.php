76291592dde04b8bd8768f2cea7017faee129dbf
DariusIII <dkrisan@gmail.com>
2018-06-07 14:25:38 +0200

    Fix another unintentinal error

--- Blacklight/NameFixer.php
+++ Blacklight/NameFixer.php
@@ -1255,7 +1255,7 @@ class NameFixer
         if (\strlen($this->_fileName) > 0 && strpos($this->_fileName, '.') !== 0) {
             switch (true) {
 
-                case strpos($this->_fileName, '.') ``:
+                case strpos($this->_fileName, '.') !== false:
                     //some filenames start with a period that ends up creating bad matches so we don't process them
                     $this->_fileName = Utility::cutStringUsingLast('.', $this->_fileName, 'left', false);
                     continue;
