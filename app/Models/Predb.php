f3fa30d9042b3c60b925f207a93f0b6635aedd61
DariusIII <dkrisan@gmail.com>
2018-06-11 11:28:24 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Models/Predb.php
+++ app/Models/Predb.php
@@ -105,8 +105,8 @@ class Predb extends Model
             ->where('releases.predb_id', '<', 1)
             ->join('releases', 'predb.title', '=', 'releases.searchname')
             ->select(['predb.id as predb_id', 'releases.id as releases_id']);
-        if ($dateLimit !== false && (int)$dateLimit > 0) {
-            $query->where('adddate', '>', now()->subDays((int)$dateLimit));
+        if ($dateLimit !== false && (int) $dateLimit > 0) {
+            $query->where('adddate', '>', now()->subDays((int) $dateLimit));
         }
 
         $res = $query->get();
