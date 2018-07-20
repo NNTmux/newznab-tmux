6512e96b9cbe326cd92aa56c19e067993a858850
DariusIII <dkrisan@gmail.com>
2018-07-20 13:51:44 +0200

    Update rest of the postproc scripts

--- misc/testing/PostProc/updateMusicImages.php
+++ misc/testing/PostProc/updateMusicImages.php
@@ -2,13 +2,13 @@
 
 require_once dirname(__DIR__, 3).DIRECTORY_SEPARATOR.'bootstrap/autoload.php';
 
-use Blacklight\db\DB;
+use App\Models\MusicInfo;
+use Blacklight\ColorCLI;
 
-$pdo = new DB();
 $covers = $updated = $deleted = 0;
 
-if ($argc == 1 || $argv[1] != 'true') {
-    exit($pdo->log->error("\nThis script will check all images in covers/music and compare to db->musicinfo.\nTo run:\nphp $argv[0] true\n"));
+if ($argc === 1 || $argv[1] !== 'true') {
+    exit(ColorCLI::error("\nThis script will check all images in covers/music and compare to db->musicinfo.\nTo run:\nphp $argv[0] true\n"));
 }
 
 $path2covers = NN_COVERS.'music'.DS;
@@ -19,28 +19,27 @@ foreach ($itr as $filePath) {
     if (is_file($filePath) && preg_match('/\d+\.jpg/', $filePath)) {
         preg_match('/(\d+)\.jpg/', basename($filePath), $match);
         if (isset($match[1])) {
-            $run = $pdo->queryDirect('UPDATE musicinfo SET cover = 1 WHERE cover = 0 AND id = '.$match[1]);
-            if ($run->rowCount() >= 1) {
+            $run = MusicInfo::query()->where('cover', '=', 0)->where('id', $match[1])->update(['cover' => 1]);
+            if ($run >= 1) {
                 $covers++;
             } else {
-                $run = $pdo->queryDirect('SELECT id FROM musicinfo WHERE id = '.$match[1]);
-                if ($run->rowCount() == 0) {
-                    echo $pdo->log->info($filePath.' not found in db.');
+                $run = MusicInfo::query()->where('id', $match[1])->select(['id'])->get();
+                if ($run->count() === 0) {
+                    echo ColorCLI::info($filePath.' not found in db.');
                 }
             }
         }
     }
 }
 
-$qry = $pdo->queryDirect('SELECT id FROM musicinfo WHERE cover = 1');
-if ($qry instanceof \Traversable) {
-    foreach ($qry as $rows) {
-        if (! is_file($path2covers.$rows['id'].'.jpg')) {
-            $pdo->queryDirect('UPDATE musicinfo SET cover = 0 WHERE cover = 1 AND id = '.$rows['id']);
-            echo $pdo->log->info($path2covers.$rows['id'].'.jpg does not exist.');
-            $deleted++;
-        }
+$qry = MusicInfo::query()->where('cover', '=', 1)->select(['id'])->get();
+foreach ($qry as $rows) {
+    if (! is_file($path2covers.$rows['id'].'.jpg')) {
+        MusicInfo::query()->where(['cover' => 1, 'id' => $rows['id']])->update(['cover' => 0]);
+        echo ColorCLI::info($path2covers.$rows['id'].'.jpg does not exist.');
+        $deleted++;
     }
 }
-echo $pdo->log->header($covers.' covers set.');
-echo $pdo->log->header($deleted.' music unset.');
+echo ColorCLI::header($covers.' covers set.');
+echo ColorCLI::header($deleted.' music unset.');
+
