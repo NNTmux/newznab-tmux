0d4ea6099d9444a0a51352f3716b6ecfb920fe23
DariusIII <dkrisan@gmail.com>
2018-10-02 12:35:58 +0200

    Update branches and their handling, remove restriction for updating non dev branches

--- app/Console/Commands/UpdateNNTmuxGit.php
+++ app/Console/Commands/UpdateNNTmuxGit.php
@@ -41,11 +41,6 @@ class UpdateNNTmuxGit extends Command
     public function handle()
     {
         $this->initialiseGit();
-        if (! \in_array($this->git->getBranch(), $this->git->getBranchesMain(), false)) {
-            $this->error('Not on the stable or dev branch! Refusing to update repository');
-
-            return;
-        }
 
         $wasRunning = false;
 
@@ -61,6 +56,9 @@ class UpdateNNTmuxGit extends Command
         }
     }
 
+    /**
+     *
+     */
     protected function initialiseGit()
     {
         if (! ($this->git instanceof Git)) {
