1bfb1f88e7feacd7ea190718062e78d3c62260bd
DariusIII <dkrisan@gmail.com>
2018-10-17 15:22:43 +0200

    Add resend_email_verification.php script to resend user email verification if server failed to send it for some reason

---
+++ misc/testing/resend_activation_email.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\User;
+use Blacklight\ColorCLI;
+use Jrean\UserVerification\Facades\UserVerification;
+
+require_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'bootstrap/autoload.php';
+
+if (isset($argv[1]) && is_numeric($argv[1])) {
+    $user = User::find($argv[1]);
+    UserVerification::generate($user);
+
+    UserVerification::send($user, 'User email verification required');
+
+    ColorCLI::doEcho(ColorCLI::notice('Email has been sent'));
+
+} else {
+    ColorCLI::doEcho(ColorCLI::info('You need to provide user id as argument'));
+}
