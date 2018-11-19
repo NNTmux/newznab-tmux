202db73ffa96cf7b63ac9b5b4de2efac6e4e5971
DariusIII <dkrisan@gmail.com>
2018-11-19 13:48:55 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Blacklight\Contents;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ContentController extends BasePageController
 {
