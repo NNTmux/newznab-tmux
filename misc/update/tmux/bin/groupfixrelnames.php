c67bde32c82653c27d337fd5035a085f04c0f155
DariusIII <dkrisan@gmail.com>
2018-11-30 12:43:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- misc/update/tmux/bin/groupfixrelnames.php
+++ misc/update/tmux/bin/groupfixrelnames.php
@@ -2,17 +2,17 @@
 
 require_once dirname(__DIR__, 4).DIRECTORY_SEPARATOR.'bootstrap/autoload.php';
 
-use App\Models\Category;
+use Blacklight\Nfo;
+use Blacklight\NZB;
+use Blacklight\NNTP;
 use App\Models\Predb;
+use App\Models\Category;
 use App\Models\Settings;
 use Blacklight\ColorCLI;
 use Blacklight\NameFixer;
-use Blacklight\Nfo;
-use Blacklight\NNTP;
-use Blacklight\NZB;
 use Blacklight\NZBContents;
-use Blacklight\processing\PostProcess;
 use Illuminate\Support\Facades\DB;
+use Blacklight\processing\PostProcess;
 
 $colorCli = new ColorCLI();
 if (! isset($argv[1])) {
