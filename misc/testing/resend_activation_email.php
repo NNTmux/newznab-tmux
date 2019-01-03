e65b6443bedb26960f15287662f6f87e288022db
DariusIII <dkrisan@gmail.com>
2019-01-03 10:30:19 +0100

    Update resend_user_verification script

--- misc/testing/resend_activation_email.php
+++ misc/testing/resend_activation_email.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Models\User;
+use Blacklight\ColorCLI;
 use Jrean\UserVerification\Facades\UserVerification;
 
 require_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'bootstrap/autoload.php';
+
+$colorCli = new ColorCLI();
 
 if (isset($argv[1]) && is_numeric($argv[1])) {
     $user = User::find($argv[1]);
@@ -11,7 +14,7 @@ if (isset($argv[1]) && is_numeric($argv[1])) {
 
     UserVerification::send($user, 'User email verification required');
 
-    echo 'Email has been sent';
+    $colorCli->info('Email has been sent');
 } else {
-    echo 'You need to provide user id as argument';
+    $colorCli->error('You need to provide user id as argument');
 }
