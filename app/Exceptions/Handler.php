86b500bdb5603447b57c4a49d640d14625ffebfa
DariusIII <dkrisan@gmail.com>
2019-01-09 11:02:41 +0100

    Update base laravel classess and middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -29,10 +29,11 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $exception
+     * @param  \Exception $exception
+     *
      * @return void
+     * @throws \Exception
      */
     public function report(Exception $exception)
     {
