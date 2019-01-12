ab0dcc9d7aeb8020107747d908db98203d88c06f
DariusIII <dkrisan@gmail.com>
2019-01-12 17:35:05 +0100

    Update nntmux:git command to also pull tags (addresses issue #842)

--- app/Console/Commands/UpdateNNTmuxGit.php
+++ app/Console/Commands/UpdateNNTmuxGit.php
@@ -46,10 +46,10 @@ class UpdateNNTmuxGit extends Command
 
         if ((new Tmux())->isRunning() === true) {
             $wasRunning = true;
-            $this->call('tmux-ui:stop', ['type' => 'true']);
+            $this->call('tmux-ui:stop', ['--kill' => true]);
         }
 
-        $this->git->gitPull();
+        $this->git->gitPull()->getTags();
 
         if ($wasRunning === true) {
             $this->call('tmux-ui:start');
