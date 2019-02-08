c02aeabc79047b65975666155e28399d50637eb4
DariusIII <dkrisan@gmail.com>
2019-02-08 10:52:10 +0100

    Fix the new account job

--- app/Jobs/SendNewRegisteredAccountMail.php
+++ app/Jobs/SendNewRegisteredAccountMail.php
@@ -10,6 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
 
 class SendNewRegisteredAccountMail implements ShouldQueue
 {
