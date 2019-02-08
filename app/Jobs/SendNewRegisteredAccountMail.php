2f743a9e6da1a45a746f7551516e5ef50f956aed
DariusIII <dkrisan@gmail.com>
2019-02-08 10:30:40 +0100

    Add missing job file

---
+++ app/Jobs/SendNewRegisteredAccountMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\NewAccountCreatedEmail;
+use App\Models\Settings;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendNewRegisteredAccountMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * @var \App\Models\User
+     */
+    private $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param \App\Models\User $user
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to(Settings::settingValue('site.main.email'))->send(new NewAccountCreatedEmail($this->user));
+    }
+}
