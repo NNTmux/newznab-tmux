a538dd3cf64da396d36be63eec3917d85edd7702
DariusIII <dkrisan@gmail.com>
2019-02-20 23:48:38 +0100

    Add horizon snapshot to kernel schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('clean:directories')->hourly();
         $schedule->exec('php '.base_path().'/misc/testing/DB/checkUsers.php')->twiceDaily(1, 13);
         $schedule->command('telescope:prune')->daily();
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
