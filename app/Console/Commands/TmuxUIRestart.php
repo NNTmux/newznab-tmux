5450f0cf77fa6817c599f4050de9c96ae57aefcc
DariusIII <dkrisan@gmail.com>
2019-02-27 09:13:53 +0100

    Add tmux-ui:restart command to restart tmux scripts

---
+++ app/Console/Commands/TmuxUIRestart.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TmuxUIRestart extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tmux-ui:restart';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Restart processing of tmux scripts completely';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        $this->call('tmux-ui:stop', ['--kill' => true]);
+        $this->call('tmux-ui:start');
+    }
+}
