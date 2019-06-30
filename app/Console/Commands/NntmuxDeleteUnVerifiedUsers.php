897647d04a86ba66626b09dfdc01e4aa9d3b547d
DariusIII <dkrisan@gmail.com>
2019-06-30 23:03:52 +0200

    Add artisan commands to update expired roles and delete unverified users

---
+++ app/Console/Commands/NntmuxDeleteUnVerifiedUsers.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class NntmuxDeleteUnVerifiedUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nntmux:delete-unverified-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes unverified users from site';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('Deleting unverified users.');
+        User::deleteUnVerified();
+        $this->info('Unverified users deleted.');
+    }
+}
---
+++ app/Jobs/SendAccountWillExpireEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\Mail\AccountWillExpire;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendAccountWillExpireEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $days;
+
+    private $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $days)
+    {
+        $this->user = $user;
+        $this->days = $days;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->user->email)->send(new AccountWillExpire($this->user, $this->days));
+    }
+}
