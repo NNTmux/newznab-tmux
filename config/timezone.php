7f94570edd540179ca0addfe4527673742eab62e
DariusIII <dkrisan@gmail.com>
2020-01-23 22:30:08 +0100

    Publish config/timezone

---
+++ config/timezone.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Flash messages
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure if to use the laracasts/flash package for flash
+    | notifications when a users timezone is set.
+    | options [standard, laracasts]
+    |
+    */
+
+    'flash' => 'standard',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Overwrite Existing Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure if you would like to overwrite existing
+    | timezones if they have been already set in the database.
+    | options [true, false]
+    |
+    */
+
+    'overwrite' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Overwrite Default Format
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure if you would like to overwrite the
+    | default format.
+    |
+    */
+
+    'format' => 'jS F Y g:i:a',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lookup Array
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the lookup array whom it will be used to fetch the user remote address.
+    | When a key is found inside the lookup array that key it will be used.
+    |
+    */
+
+    'lookup' => [
+        'server' => [
+            'REMOTE_ADDR',
+        ],
+        'headers' => [
+
+        ],
+    ],
+
+];
