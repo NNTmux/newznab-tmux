ffefc044b75388a41cd6cdaf464208bfe520b98e
DariusIII <dkrisan@gmail.com>
2020-03-01 21:02:14 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/nntmux_api.php
+++ config/nntmux_api.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'anidb_api_key' => env('ANIDB_APIKEY', ''),
-  'giantbomb_api_key' => env('GIANTBOMB_APIKEY', ''),
-  'fanarttv_api_key' => env('FANARTTV_APIKEY', ''),
-  'omdb_api_key' => env('OMDB_APIKEY', ''),
-  'trakttv_api_key' => env('TRAKTTV_APIKEY', ''),
+    'anidb_api_key' => env('ANIDB_APIKEY', ''),
+    'giantbomb_api_key' => env('GIANTBOMB_APIKEY', ''),
+    'fanarttv_api_key' => env('FANARTTV_APIKEY', ''),
+    'omdb_api_key' => env('OMDB_APIKEY', ''),
+    'trakttv_api_key' => env('TRAKTTV_APIKEY', ''),
 ];
