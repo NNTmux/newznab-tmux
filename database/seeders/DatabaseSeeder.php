c19f569b4514f4390b663fc0e3cc8f04726f73fc
DariusIII <dkrisan@gmail.com>
2020-10-09 10:18:26 +0200

    Update files with latest Laravel 8 changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // \App\Models\User::factory(10)->create();
         $this->call(CountriesSeeder::class);
         $this->call(RootCategoriesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
