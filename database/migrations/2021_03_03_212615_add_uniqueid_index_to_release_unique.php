d138e7e9e507f7c84f878febf949bc21a4399384
nzbsrus <nzbsrus>
2021-03-03 21:29:25 +0000

    Update migration AddUniqueIdIndexToReleaseUnique; performance improvement;

---
+++ database/migrations/2021_03_03_212615_add_uniqueid_index_to_release_unique.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @noinspection PhpUnused, SpellCheckingInspection */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueIdIndexToReleaseUnique extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('release_unique', function (Blueprint $table) {
+            $table->index('uniqueid', 'ix_release_unique_uniqueid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('release_unique', function (Blueprint $table) {
+            $table->dropUnique('ix_release_unique_uniqueid');
+        });
+    }
+}
