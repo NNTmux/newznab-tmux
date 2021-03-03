e81a0169f392eb15dcd36249cf309c59fc6a1c75
nzbsrus <nzbsrus>
2021-03-03 23:27:37 +0000

    Update migration AddCountSuggestIndexToTaggingTags; performance improvement;

---
+++ database/migrations/2021_03_03_231915_add_count_suggest_index_to_tagging_tags.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @noinspection PhpUnused, SpellCheckingInspection */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCountSuggestIndexToTaggingTags extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tagging_tags', function (Blueprint $table) {
+            $table->index(['count', 'suggest'], 'ix_tagging_tags_count_suggest');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tagging_tags', function (Blueprint $table) {
+            $table->dropUnique('ix_tagging_tags_count_suggest');
+        });
+    }
+}
