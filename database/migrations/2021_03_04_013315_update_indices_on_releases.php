a8ddd812b74c51bcf4862b17a9bb20fbb471c2d5
nzbsrus <nzbsrus>
2021-03-10 10:08:33 +0000

    Update migration UpdateIndicesOnReleases; performance improvement;

--- database/migrations/2021_03_04_013315_update_indices_on_releases.php
+++ database/migrations/2021_03_04_013315_update_indices_on_releases.php
@@ -21,6 +21,8 @@ class UpdateIndicesOnReleases extends Migration
             $table->dropIndex('ix_releases_passwordstatus');
             $table->index(['passwordstatus', 'postdate'], 'ix_releases_passwordstatus_postdate');
             $table->index(['categories_id', 'nzbstatus', 'musicinfo_id'], 'ix_releases_categories_id_nzbstatus_musicinfo_id');
+            $table->index(['size', 'haspreview', 'passwordstatus', 'nzbstatus', 'leftguid'], 'ix_releases_size_etc');
+            $table->index(['isrenamed', 'nzbstatus', 'nfostatus', 'passwordstatus', 'predb_id'], 'ix_releases_isrenamed');
         });
     }
 
@@ -37,6 +39,8 @@ class UpdateIndicesOnReleases extends Migration
             $table->dropIndex('ix_releases_passwordstatus_postdate');
             $table->index(['name'], 'ix_releases_passwordstatus');
             $table->dropIndex('ix_releases_categories_id_nzbstatus_musicinfo_id');
+            $table->dropIndex('ix_releases_size_etc');
+            $table->dropIndex('ix_releases_isrenamed');
         });
     }
 }
