7652c155965ec1120ac9e59a014453c176bcb881
DariusIII <dkrisan@gmail.com>
2022-02-09 21:54:33 +0100

    Use ntimyeboah/laravel-database-trigger once again

--- database/migrations/2018_12_20_223131_create_delete_hashes_trigger.php
+++ database/migrations/2018_12_20_223131_create_delete_hashes_trigger.php
@@ -1,7 +1,7 @@
 <?php
 
-use DariusIII\LaravelDatabaseTrigger\TriggerFacade as Schema;
 use Illuminate\Database\Migrations\Migration;
+use NtimYeboah\LaravelDatabaseTrigger\TriggerFacade as Schema;
 
 class CreateDeleteHashesTrigger extends Migration
 {
