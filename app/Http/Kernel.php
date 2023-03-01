c360dc1e09db335d84ec66cc597ebeaf73633c36
DariusIII <dkrisan@gmail.com>
2023-03-01 20:20:44 +0100

    Add rector with laravel rules and composer normalizer

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -50,7 +50,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
