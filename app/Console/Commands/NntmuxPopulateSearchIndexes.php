6c69befe7bf2fb6f8c6f8984db6f0f6162f8879d
DariusIII <dkrisan@gmail.com>
2023-03-22 23:24:40 +0100

    Update elastic related functions

--- app/Console/Commands/NntmuxPopulateSearchIndexes.php
+++ app/Console/Commands/NntmuxPopulateSearchIndexes.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\Predb;
 use App\Models\Release;
+use Blacklight\ElasticSearchSiteSearch;
 use Blacklight\ManticoreSearch;
 use Elasticsearch;
 use Illuminate\Console\Command;
@@ -148,77 +149,42 @@ class NntmuxPopulateSearchIndexes extends Command
      */
     private function elasticReleases()
     {
-        $max = $this->option('count');
-        DB::statement('SET SESSION group_concat_max_len=16384;');
-        $query = 'SELECT r.id, r.name, r.searchname, r.fromname, r.categories_id, r.adddate, r.postdate, IFNULL(GROUP_CONCAT(rf.name SEPARATOR " "),"") filename
-            FROM releases r
-            LEFT JOIN release_files rf ON r.id = rf.releases_id
-            WHERE r.id > %d
-            GROUP BY r.id
-            ORDER BY r.id ASC
-            LIMIT %d';
-
-        $totals = Release::fromQuery('SELECT COUNT(id) AS c, MIN(id) AS min FROM releases')->first();
-        if (! $totals) {
-            $this->error('Could not get database information for releases table.');
+        $elastic = new ElasticSearchSiteSearch();
+        $total = Release::count();
+        if (! $total) {
+            $this->warn('Could not get database information for releases table.');
+            exit();
         }
-        $total = $totals->c;
-        $minId = $totals->min;
-
-        $lastId = $minId - 1;
+        $max = $this->option('count');
         $this->info("Starting to populate ElasticSearch index releases with $total releases.");
+
         $bar = $this->output->createProgressBar($total);
         $bar->setOverwrite(true); // Terminal needs to support ANSI Encoding for this?
         $bar->start();
-        $data = ['body' => []];
-        for ($i = $minId; $i <= ($total + $minId); $i += $max) {
-            $rows = DB::select(sprintf($query, $lastId, $max));
-            if ($rows === 0) {
-                continue;
-            }
 
-            foreach ($rows as $row) {
-                if ($row->id > $lastId) {
-                    $lastId = $row->id;
+        DB::statement('SET SESSION group_concat_max_len=16384;');
+        DB::table('releases')
+            ->orderBy('id')
+            ->join('release_files', 'releases.id', '=', 'release_files.releases_id')
+            ->select('releases.id', 'releases.name', 'releases.searchname', 'releases.fromname', 'releases.categories_id', 'releases.postdate')
+            ->selectRaw('IFNULL(GROUP_CONCAT(release_files.name SEPARATOR " "),"") filename')
+            ->groupBy('id')
+            ->chunk($max, function ($releases) use ($elastic, $bar) {
+                foreach ($releases as $r) {
+                    $searchName = str_replace(['.', '-'], ' ', $r->searchname);
+                    $elastic->insertRelease([
+                        'id' => $r->id,
+                        'name' => $r->name,
+                        'searchname' => $r->searchname,
+                        'plainsearchname' => $searchName,
+                        'fromname' => $r->fromname,
+                        'categories_id' => $r->categories_id,
+                        'filename' => $r->filename,
+                        'postdate' => $r->postdate,
+                    ]);
+                    $bar->advance();
                 }
-                $searchName = str_replace(['.', '-'], ' ', $row->searchname);
-                $data['body'][] = [
-                    'index' => [
-                        '_index' => 'releases',
-                        '_id' => $row->id,
-                    ],
-                ];
-
-                $data['body'][] = [
-                    'id' => $row->id,
-                    'name' => $row->name,
-                    'searchname' => $row->searchname,
-                    'plainsearchname' => $searchName,
-                    'fromname' => $row->fromname,
-                    'categories_id' => $row->categories_id,
-                    'filename' => $row->filename,
-                    'add_date' => $row->adddate,
-                    'post_date' => $row->postdate,
-                ];
-            }
-            if (! empty($data['body'])) {
-                // Stop and send the bulk request
-                $responses = Elasticsearch::bulk($data);
-
-                // erase the old bulk request
-                $data = ['body' => []];
-
-                // unset the bulk response when you are done to save memory
-                unset($responses);
-
-                $bar->advance();
-            }
-        }
-
-        // Send the last batch if it exists
-        if (! empty($data['body'])) {
-            $responses = Elasticsearch::bulk($data);
-        }
+            });
         $bar->finish();
         $this->newLine();
         $this->info('Done');
@@ -229,76 +195,44 @@ class NntmuxPopulateSearchIndexes extends Command
      */
     private function elasticPreDB()
     {
-        $max = $this->option('count');
-        DB::statement('SET SESSION group_concat_max_len=16384;');
-        $query = 'SELECT id, title, filename, source
-				FROM predb
-				WHERE id > %d
-				GROUP BY id
-				ORDER BY id ASC
-				LIMIT %d';
-
-        $totals = Predb::fromQuery('SELECT COUNT(id) AS c, MIN(id) AS min FROM predb')->first();
-        if (! $totals) {
-            $this->error('Could not get database information for releases table.');
+        $total = Predb::count();
+        if (! $total) {
+            $this->warn('Could not get database information for predb table.');
+            exit();
         }
-        $total = $totals->c;
-        $minId = $totals->min;
+        $max = $this->option('count');
 
-        $lastId = $minId - 1;
-        $this->info("Starting to populate ElasticSearch index predb with $total releases.");
+        DB::statement('SET SESSION group_concat_max_len=16384;');
+
+        $this->info("Starting to populate Manticore RT index 'predb_rt' with $total rows and $max inserts per loop.");
+
         $bar = $this->output->createProgressBar($total);
         $bar->setOverwrite(true); // Terminal needs to support ANSI Encoding for this?
         $bar->start();
-        $data = ['body' => []];
-        for ($i = $minId; $i <= ($total + $minId); $i += $max) {
-            $rows = DB::select(sprintf($query, $lastId, $max));
-            if ($rows === 0) {
-                continue;
-            }
 
-            foreach ($rows as $row) {
-                if ($row->id > $lastId) {
-                    $lastId = $row->id;
+        DB::table('predb')
+            ->select('id', 'title', 'filename', 'source')
+            ->groupBy('id')
+            ->orderBy('id')
+            ->chunk($max, function ($pre) use ($manticore, $bar) {
+                foreach ($pre as $p) {
+                    $manticore->insertPredb([
+                        'id' => $p->id,
+                        'title' => $p->title,
+                        'filename' => $p->filename,
+                        'source' => $p->source,
+                    ]);
+                    $bar->advance();
                 }
-                $data['body'][] = [
-                    'index' => [
-                        '_index' => 'predb',
-                        '_id' => $row->id,
-                    ],
-                ];
-                $data['body'][] = [
-                    'id' => $row->id,
-                    'title' => $row->title,
-                    'filename' => $row->filename,
-                    'source' => $row->source,
-                ];
-            }
-            if (! empty($data['body'])) {
-                // Stop and send the bulk request
-                $responses = Elasticsearch::bulk($data);
+            });
 
-                // erase the old bulk request
-                $data = ['body' => []];
-
-                // unset the bulk response when you are done to save memory
-                unset($responses);
-
-                $bar->advance();
-            }
-        }
-
-        // Send the last batch if it exists
-        if (! empty($data['body'])) {
-            $responses = Elasticsearch::bulk($data);
-        }
         $bar->finish();
         $this->newLine();
         $this->info('Done');
     }
 
     /**
-     * Run predb.
+     * Optimize Manticore indexes
      */
     private function optimize(): void
     {
