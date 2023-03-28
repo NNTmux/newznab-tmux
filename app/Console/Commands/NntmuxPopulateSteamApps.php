69c32524657320e2bd4d104d2f8effc944006a6e
DariusIII <dkrisan@gmail.com>
2023-03-28 13:53:37 +0200

    Add command to populate steam apps table

---
+++ app/Console/Commands/NntmuxPopulateSteamApps.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Blacklight\Steam;
+use Illuminate\Console\Command;
+
+class NntmuxPopulateSteamApps extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nntmux:populate-steam-apps';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Populate Steam apps table';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $steam = new Steam();
+
+        try {
+            $steam->populateSteamAppsTable();
+        } catch (\Exception $e) {
+            $this->error($e->getMessage());
+            $this->error('There was an error populating the steam_apps table');
+        }
+    }
+}
--- cli/data/populate_steam_apps.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once dirname(__DIR__, 2).DIRECTORY_SEPARATOR.'bootstrap/autoload.php';
-
-use Blacklight\Steam;
-
-$steam = new Steam();
-
-$steam->populateSteamAppsTable();
