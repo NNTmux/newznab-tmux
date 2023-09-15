c3729318d28b8b851a6ad4e9563f57978cd7ae05
Shift <shift@laravelshift.com>
2023-09-15 18:12:10 +0000

    Configure Vite

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                ,
+            ],
+            refresh: true,
+        }),
+    ],
+});
