d05f0ef08d4fb9b2d20df2f9092082d23e522686
DariusIII <dkrisan@gmail.com>
2023-10-04 23:10:49 +0200

    Update scripts

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,15 +1,17 @@
 <?php
 
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-require __DIR__.DIRECTORY_SEPARATOR.'app.php';
-require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'app/Extensions/util/PhpYenc.php';
+    use Dotenv\Dotenv;
+    use Dotenv\Repository\RepositoryBuilder;
+    use Illuminate\Contracts\Console\Kernel;
 
-use Dotenv\Dotenv;
-use Dotenv\Repository\RepositoryBuilder;
+    require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+    require __DIR__.DIRECTORY_SEPARATOR.'app.php';
+    require_once dirname(__DIR__).DIRECTORY_SEPARATOR.'app/Extensions/util/PhpYenc.php';
 
-$repository = RepositoryBuilder::createWithDefaultAdapters()
-    ->make();
+    $repository = RepositoryBuilder::createWithDefaultAdapters()->make();
 
-$dotenv = Dotenv::create($repository, dirname(__DIR__, 1), null)->load();
+    $dotenv = Dotenv::create($repository, dirname(__DIR__, 1), null)->load();
 
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+    /** @var Kernel $kernel */
+    $kernel = app(Kernel::class);
+    $kernel->bootstrap();
