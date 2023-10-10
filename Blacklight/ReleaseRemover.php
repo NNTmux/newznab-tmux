3175a995071b8cbaf5f2788b7f83a38f3257044b
DariusIII <dkrisan@gmail.com>
2023-10-10 22:37:47 +0200

    Remove one more rse query

--- Blacklight/ReleaseRemover.php
+++ Blacklight/ReleaseRemover.php
@@ -657,13 +657,13 @@ class ReleaseRemover
                 if ($this->crapTime === '') {
                     $regexMatch = $this->extractSrchFromRegx($dbRegex);
                     if ($regexMatch !== '') {
-                        $ftMatch = sprintf('rse.query = "@(name,searchname) %s;limit=1000000;maxmatches=1000000;mode=any" AND', str_replace('|', ' ', str_replace('"', '', $regexMatch)));
+                        $ftMatch = str_replace('|', ' ', str_replace('"', '', $regexMatch));
                     }
                 }
 
                 switch ((int) $regex->msgcol) {
                     case Binaries::BLACKLIST_FIELD_SUBJECT:
-                        $regexSQL = sprintf('WHERE %s (r.name REGEXP %s OR r.searchname REGEXP %2$s)', $ftMatch, $dbRegex);
+                        $regexSQL = sprintf('WHERE (r.name REGEXP %s OR r.searchname REGEXP %2$s)', $ftMatch, $dbRegex);
                         $opTypeName = 'Subject';
                         break;
                     case Binaries::BLACKLIST_FIELD_FROM:
