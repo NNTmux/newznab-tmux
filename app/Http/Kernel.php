5bc99fdba1134eb8ebc290fb4045d64c4c3859a9
DariusIII <dkrisan@gmail.com>
2023-11-19 17:11:24 +0100

    Fix permission middleware name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,9 +59,9 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'isVerified' => \Jrean\UserVerification\Middleware\IsVerified::class,
-        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
-        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
-        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
+        'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         'clearance' => \App\Http\Middleware\ClearanceMiddleware::class,
         '2fa' => \App\Http\Middleware\Google2FAMiddleware::class,
     ];
