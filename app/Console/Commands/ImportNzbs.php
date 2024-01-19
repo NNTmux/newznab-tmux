db3f96e0536af7c2b0b56e992665a2e2012ef894
DariusIII <dkrisan@gmail.com>
2024-01-19 21:49:04 +0100

    Add ImportNzbs command

---
+++ app/Console/Commands/ImportNzbs.php
@@ -0,0 +1,65 @@
+<?php
+
+    namespace App\Console\Commands;
+
+    use Blacklight\NZBImport;
+    use Illuminate\Console\Command;
+    use Illuminate\Contracts\Filesystem\FileNotFoundException;
+    use Illuminate\Support\Facades\File;
+
+    class ImportNzbs extends Command
+    {
+        /**
+         * The name and signature of the console command.
+         *
+         * @var string
+         */
+        protected $signature = 'nntmux:import-nzbs
+        {--folder= : Import folder path}
+        {--filename : Use filename true or false}
+        {--delete : Delete files after import}
+        {--delete-failed : Delete files after failed import}';
+
+        /**
+         * The console command description.
+         *
+         * @var string
+         */
+        protected $description = 'Import nzb files for indexing';
+
+        /**
+         * Execute the console command.
+         */
+        public function handle()
+        {
+            if ($this->option('folder')) {
+                if ($this->option('filename')) {
+                    $useNzbName = $this->option('filename');
+                } else {
+                    $useNzbName = false;
+                }
+                if ($this->option('delete')) {
+                    $deleteNZB = $this->option('delete');
+                } else {
+                    $deleteNZB = false;
+                }
+                if ($this->option('delete-failed')) {
+                    $deleteFailedNZB = $this->option('delete-failed');
+                } else {
+                    $deleteFailedNZB = false;
+                }
+                $folder = $this->option('folder');
+                $files = File::allFiles($folder);
+                $NZBImport = new NZBImport();
+
+                try {
+                    $NZBImport->beginImport($files, $useNzbName, $deleteNZB, $deleteFailedNZB);
+                } catch (FileNotFoundException $e) {
+                    $this->error($e->getMessage());
+                }
+            } else {
+                $this->error('Folder path must not be empty');
+                exit();
+            }
+        }
+    }
