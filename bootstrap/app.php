3a1a970f7e6847f2da09f2a126d62818bf676569
Shift <shift@laravelshift.com>
2024-03-07 14:25:02 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,34 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append([
+            \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+            \Monicahq\Cloudflare\Http\Middleware\TrustProxies::class,
+            \App\Http\Middleware\ForceJsonOnAPI::class,
+        ]);
+
+        $middleware->web(\Illuminate\Session\Middleware\AuthenticateSession::class);
+
+        $middleware->api([
+            'throttle:60,1',
+            'bindings',
+        ]);
+
+        $middleware->replace(\Illuminate\Foundation\Http\Middleware\TrimStrings::class, \App\Http\Middleware\TrimStrings::class);
+
+        $middleware->replaceInGroup('web', \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class, \App\Http\Middleware\VerifyCsrfToken::class);
+
+        $middleware->alias([
+            '2fa' => \App\Http\Middleware\Google2FAMiddleware::class,
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'clearance' => \App\Http\Middleware\ClearanceMiddleware::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'isVerified' => \Jrean\UserVerification\Middleware\IsVerified::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
