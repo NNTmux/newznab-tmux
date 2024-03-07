1e299e3e78bc47938d10f32042f3d2b411ddbe37
Shift <shift@laravelshift.com>
2024-03-07 14:24:41 +0000

    Remove unnecessary `$model` property

--- database/factories/ReleaseFactory.php
+++ database/factories/ReleaseFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ReleaseFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Release::class;
-
-    /**
      * Define the model's default state.
      */
     public function definition(): array
