b2b52e38da3302aa6221291f2fde41b591e3b04f
DariusIII <dkrisan@gmail.com>
2024-04-28 01:54:48 +0200

    CS fixes

--- config/app.php
+++ config/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
--- config/database.php
+++ config/database.php
@@ -50,9 +50,9 @@ return [
 
     'migrations' => [
         'table' => [
-        'table' => 'migrations',
-        'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
-    ],
+            'table' => 'migrations',
+            'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
+        ],
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
     ],
 
