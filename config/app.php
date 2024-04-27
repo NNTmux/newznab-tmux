7a71f2e5d18628fba1511953b6729d5ec7a6f6f3
Shift <shift@laravelshift.com>
2024-04-27 18:43:45 +0000

    Streamline config files

--- config/app.php
+++ config/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Facade;
 
 return [
--- config/database.php
+++ config/database.php
@@ -49,7 +49,10 @@ return [
     ],
 
     'migrations' => [
+        'table' => [
         'table' => 'migrations',
+        'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
+    ],
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
     ],
 
