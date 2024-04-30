7e87b855294c93b1df0fde3e5f92eeac49b8f181
DariusIII <dkrisan@gmail.com>
2024-04-30 21:24:27 +0200

    Don't check csrf token on logout page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'failed',
             'admin/*',
             'btcpay/webhook',
+            'logout',
         ]);
 
         $middleware->append([
