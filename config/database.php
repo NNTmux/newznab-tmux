ca4c5de38c0973eec399b95a8205ee753b9e42e0
Shift <shift@laravelshift.com>
2024-10-31 20:46:55 +0000

    Streamline config files

--- config/database.php
+++ config/database.php
@@ -67,33 +67,6 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run on the database.
-    |
-    */
-
-    'migrations' => [
-        'table' => 'migrations',
-        'update_date_on_publish' => true,
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as Memcached. You may define your connection settings here.
-    |
-    */
-
     'redis' => [
         'client' => env('REDIS_CLIENT', 'phpredis'),
         'options' => [
