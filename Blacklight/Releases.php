cff4a1de38c759984c8a2f27533dd9bde035ab5b
DariusIII <dkrisan@gmail.com>
2025-01-17 13:29:57 +0100

    Revert pager changes

--- Blacklight/Releases.php
+++ Blacklight/Releases.php
@@ -98,12 +98,11 @@ class Releases extends Release
         }
         $sql = $this->fromQuery($qry);
         if (\count($sql) > 0) {
-            $possibleRows = $sql->count();
+            $possibleRows = $this->getBrowseCount($cat, $maxAge, $excludedCats, $groupName);
             $sql[0]->_totalcount = $sql[0]->_totalrows = $possibleRows;
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($qry.$page), $sql, $expiresAt);
-
         return $sql;
     }
 
@@ -533,7 +532,7 @@ class Releases extends Release
         }
         $releases = $this->fromQuery($sql);
         if ($releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount($baseSql);
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -614,7 +613,7 @@ class Releases extends Release
             $releases = collect();
         }
         if ($releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount($baseSql);
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -753,7 +752,9 @@ class Releases extends Release
         }
         $releases = ((! empty($name) && count($searchResult) !== 0) || empty($name)) ? $this->fromQuery($sql) : [];
         if (count($releases) !== 0 && $releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount(
+                preg_replace('#LEFT(\s+OUTER)?\s+JOIN\s+(?!tv_episodes)\s+.*ON.*=.*\n#i', ' ', $baseSql)
+            );
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -885,7 +886,9 @@ class Releases extends Release
         }
         $releases = $this->fromQuery($sql);
         if ($releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount(
+                preg_replace('#LEFT(\s+OUTER)?\s+JOIN\s+(?!tv_episodes)\s+.*ON.*=.*\n#i', ' ', $baseSql)
+            );
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -955,7 +958,7 @@ class Releases extends Release
         }
         $releases = $this->fromQuery($sql);
         if ($releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount($baseSql);
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -1031,7 +1034,7 @@ class Releases extends Release
         }
         $releases = $this->fromQuery($sql);
         if ($releases->isNotEmpty()) {
-            $releases[0]->_totalrows = $releases->count();
+            $releases[0]->_totalrows = $this->getPagerCount($baseSql);
         }
         $expiresAt = now()->addMinutes(config('nntmux.cache_expiry_medium'));
         Cache::put(md5($sql), $releases, $expiresAt);
@@ -1076,10 +1079,10 @@ class Releases extends Release
     private function getPagerCount(string $query): int
     {
         $queryBuilder = DB::table(DB::raw('('.preg_replace(
-            '/SELECT.+?FROM\s+releases/is',
-            'SELECT r.id FROM releases',
-            $query
-        ).' LIMIT '.(int) config('nntmux.max_pager_results').') as z'))
+                '/SELECT.+?FROM\s+releases/is',
+                'SELECT r.id FROM releases',
+                $query
+            ).' LIMIT '.(int) config('nntmux.max_pager_results').') as z'))
             ->selectRaw('COUNT(z.id) as count');
 
         $sql = $queryBuilder->toSql();
