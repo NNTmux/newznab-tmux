60a9bd3e31c5c408db026f7b694981fd43883ec1
DariusIII <dkrisan@gmail.com>
2025-03-26 12:37:25 +0100

    CS fixes

--- app/Extensions/helper/helpers.php
+++ app/Extensions/helper/helpers.php
@@ -8,7 +8,6 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
 use GuzzleHttp\Cookie\SetCookie;
 use GuzzleHttp\Exception\RequestException;
-use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
