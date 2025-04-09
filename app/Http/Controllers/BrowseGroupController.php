957928485f07ed3e910f4521b357ef7ccb641d4c
DariusIII <dkrisan@gmail.com>
2025-04-09 15:44:27 +0200

    Update group browsing page

--- app/Http/Controllers/BrowseGroupController.php
+++ app/Http/Controllers/BrowseGroupController.php
@@ -1,26 +1,39 @@
 <?php
 
-namespace App\Http\Controllers;
+        namespace App\Http\Controllers;
 
-use App\Models\UsenetGroup;
+        use App\Models\UsenetGroup;
+        use Illuminate\Http\Request;
 
-class BrowseGroupController extends BasePageController
-{
-    /**
-     * @throws \Exception
-     */
-    public function show(): void
-    {
-        $this->setPreferences();
-        $groupList = UsenetGroup::getGroupsRange('', true);
-        $this->smarty->assign('results', $groupList);
+        class BrowseGroupController extends BasePageController
+        {
+            /**
+             * @throws \Exception
+             */
+            public function show(Request $request): void
+            {
+                $this->setPreferences();
 
-        $meta_title = 'Browse Groups';
-        $meta_keywords = 'browse,groups,description,details';
-        $meta_description = 'Browse groups';
+                // Get the search term from the request
+                $search = $request->get('search', '') ?? '';
 
-        $content = $this->smarty->fetch('browsegroup.tpl');
-        $this->smarty->assign(compact('content', 'meta_title', 'meta_keywords', 'meta_description'));
-        $this->pagerender();
-    }
-}
+                // Get groups based on search term
+                $groupList = UsenetGroup::getGroupsRange($search, true);
+
+                // Pass data to the view
+                $this->smarty->assign('results', $groupList);
+                $this->smarty->assign('search', $search);
+
+                $meta_title = 'Browse Groups';
+                $meta_keywords = 'browse,groups,description,details';
+                $meta_description = 'Browse groups';
+                if (!empty($search)) {
+                    $meta_title .= ' - Search: '.$search;
+                    $meta_description = 'Browse groups search results for '.$search;
+                }
+
+                $content = $this->smarty->fetch('browsegroup.tpl');
+                $this->smarty->assign(compact('content', 'meta_title', 'meta_keywords', 'meta_description'));
+                $this->pagerender();
+            }
+        }
