16f08dc903d724e6362a9d71f23e27bb8240151f
DariusIII <dkrisan@gmail.com>
2025-04-11 17:02:10 +0200

    CS fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -112,8 +112,8 @@ class LoginController extends Controller
         $meta_title = 'Login';
         $meta_keywords = 'Login';
         $meta_description = 'Login';
-        //$content = app('smarty.view')->fetch($theme.'/login.tpl');
-        app('smarty.view')->assign(compact(/*'content',*/ 'meta_title', 'meta_keywords', 'meta_description'));
+        // $content = app('smarty.view')->fetch($theme.'/login.tpl');
+        app('smarty.view')->assign(compact(/* 'content', */ 'meta_title', 'meta_keywords', 'meta_description'));
 
         return app('smarty.view')->display($theme.'/login.tpl');
     }
