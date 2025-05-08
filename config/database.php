6b35c7f67feb02195a05a7acf267a2d5b877ab17
DariusIII <dkrisan@gmail.com>
2025-05-08 13:19:36 +0200

    Update .env.example and database config for new predis version

--- config/database.php
+++ config/database.php
@@ -76,6 +76,7 @@ return [
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME', 'default'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
