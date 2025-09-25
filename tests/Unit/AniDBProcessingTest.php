2de4bc1af8cc84b34aedcd1e702b2ea9178c2b4c
DariusIII <dkrisan@gmail.com>
2025-09-26 00:42:42 +0200

    Fix failing test

--- tests/Unit/AniDBProcessingTest.php
+++ tests/Unit/AniDBProcessingTest.php
@@ -46,7 +46,7 @@ final class AniDBProcessingTest extends TestCase
     public function test_movie_token_maps_to_episode_one(): void
     {
         $status = null;
-        $res = $this->invokeExtract('Spirited Away - Movie [BD 1080p]');
+        $res = $this->invokeExtract('Spirited Away - Movie [BD 1080p]', $status);
         $this->assertSame(1, $res['epno']);
         $this->assertSame('Spirited Away', $res['title']);
         $this->assertNull($status);
@@ -55,7 +55,7 @@ final class AniDBProcessingTest extends TestCase
     public function test_complete_series_maps_to_episode_zero(): void
     {
         $status = null;
-        $res = $this->invokeExtract('Great Show - Complete Series [BD 1080p]');
+        $res = $this->invokeExtract('Great Show - Complete Series [BD 1080p]', $status);
         $this->assertSame(0, $res['epno']);
         $this->assertSame('Great Show', $res['title']);
         $this->assertNull($status);
@@ -64,7 +64,7 @@ final class AniDBProcessingTest extends TestCase
     public function test_underscore_and_periods_are_normalized(): void
     {
         $status = null;
-        $res = $this->invokeExtract('[Group] My_Show.Name - 07 [720p]');
+        $res = $this->invokeExtract('[Group] My_Show.Name - 07 [720p]', $status);
         $this->assertSame('My Show Name', $res['title']);
         $this->assertSame(7, $res['epno']);
         $this->assertNull($status);
@@ -73,7 +73,7 @@ final class AniDBProcessingTest extends TestCase
     public function test_failed_extraction_sets_status_and_returns_empty(): void
     {
         $status = null;
-        $res = $this->invokeExtract('ThisWillNotMatchPattern');
+        $res = $this->invokeExtract('ThisWillNotMatchPattern', $status);
         $this->assertSame([], $res);
         $this->assertIsInt($status);
         $this->assertLessThan(0, $status, 'Status should be negative on failure (PROC_EXTFAIL)');
