83ba1bd4ea6560f5206042529ce3837ba4f02eea
DariusIII <dkrisan@gmail.com>
2025-10-16 21:24:20 +0200

    CS fixes

--- database/migrations/2025_10_16_120000_update_dark_mode_to_theme_preference.php
+++ database/migrations/2025_10_16_120000_update_dark_mode_to_theme_preference.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -47,4 +47,3 @@ return new class extends Migration
         });
     }
 };
-
