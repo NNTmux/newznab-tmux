2b1b7c4992e9f32e7bc1245200822fce7828bd68
DariusIII <dkrisan@gmail.com>
2025-12-29 23:09:44 +0100

    Fix missing error pages

--- resources/views/errors/402.blade.php
+++ resources/views/errors/402.blade.php
@@ -1,4 +1,4 @@
-@extends('errors::minimal')
+@extends('errors.minimal')
 
 @section('title', __('Payment Required'))
 @section('code', '402')
--- resources/views/errors/maintenance.blade.php
+++ resources/views/errors/maintenance.blade.php
@@ -1,4 +1,4 @@
-@extends('errors::minimal')
+@extends('errors.minimal')
 
 @section('title', __('Maintenance'))
 @section('code', '503')
