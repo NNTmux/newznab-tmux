523cf6cece2e2ffc884e9f1b29df27758d6f7be0
DariusIII <dkrisan@gmail.com>
2026-02-18 11:16:26 +0100

    Update forum policy

--- app/Policies/ForumPolicy.php
+++ app/Policies/ForumPolicy.php
@@ -14,71 +14,71 @@ class ForumPolicy extends \TeamTeaTime\Forum\Policies\ForumPolicy
 
     public function createCategories(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function manageCategories(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function moveCategories(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function renameCategories(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function viewTrashedThreads(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function viewTrashedPosts(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function deleteCategories(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function deletePosts(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function deleteThreads(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function restorePosts(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function restoreThreads(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function purgePosts(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function purgeThreads(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 
     public function deleteThrashedPosts(mixed $user): bool
     {
-        return $user->hasRole('Admin');
+        return $user?->hasRole('Admin') ?? false;
     }
 }
