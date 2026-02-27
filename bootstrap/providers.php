52d5db4f02733b8717760fbb73c8cf8f886a880d
DariusIII <dkrisan@gmail.com>
2026-02-27 16:46:00 +0100

    Add back laravel horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,10 +5,11 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CategorizationServiceProvider::class,
     App\Providers\ForumServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\ProcessingServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
+    App\Providers\SearchServiceProvider::class,
     App\Providers\TvProcessingServiceProvider::class,
     App\Providers\UserServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
-    App\Providers\SearchServiceProvider::class,
 ];
--- resources/views/partials/admin-menu.blade.php
+++ resources/views/partials/admin-menu.blade.php
@@ -300,6 +300,9 @@
             <a href="{{ url('/admin/site-stats') }}" class="block py-2 px-3 text-gray-400 dark:text-gray-500 hover:text-white dark:hover:text-white hover:bg-gray-800 dark:hover:bg-gray-800 rounded transition">
                 <i class="fas fa-chart-bar mr-2 text-purple-400"></i>Statistics
             </a>
+            <a href="{{ url(config('horizon.path', 'horizon')) }}" class="block py-2 px-3 text-gray-400 dark:text-gray-500 hover:text-white dark:hover:text-white hover:bg-gray-800 dark:hover:bg-gray-800 rounded transition">
+                <i class="fas fa-stream mr-2 text-cyan-400"></i>Horizon
+            </a>
         </div>
     </div>
 
